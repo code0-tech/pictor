f26f0172725fa7a5cf8d46fbf4fbf71e21ad04fc
Nico Sammito <nico@sammito.de>
2023-12-07 07:02:40 +0100

    changing text for message

--- src/components/input/Input.stories.tsx
+++ src/components/input/Input.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Input from "./Input";
-import {IconAB, IconMail} from "@tabler/icons-react";
+import {IconMail} from "@tabler/icons-react";
 
 export default {
     title: "Input",
@@ -18,7 +18,7 @@ export const Mail = () => <Input>
 
 export const Disabled = () => <Input disabled>
     <Input.Control placeholder={"Your Mail"}>
-        <Input.Control.Icon><p>sdsd</p></Input.Control.Icon>
+        <Input.Control.Icon><IconMail/></Input.Control.Icon>
         <Input.Control.Message>test</Input.Control.Message>
     </Input.Control>
     <Input.Desc>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Input.Desc>
@@ -26,7 +26,7 @@ export const Disabled = () => <Input disabled>
 
 export const NotValid = () => <Input valid={false}>
     <Input.Control placeholder={"Your E-Mail"}>
-        <Input.Control.Icon><p>sdsd</p></Input.Control.Icon>
+        <Input.Control.Icon><IconMail/></Input.Control.Icon>
         <Input.Control.Message>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
             invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</Input.Control.Message>
     </Input.Control>
@@ -35,8 +35,9 @@ export const NotValid = () => <Input valid={false}>
 
 export const Valid = () => <Input valid>
     <Input.Control placeholder={"Your E-Mail"}>
-        <Input.Control.Icon><p>sdsd</p></Input.Control.Icon>
-        <Input.Control.Message>test</Input.Control.Message>
+        <Input.Control.Icon><IconMail/></Input.Control.Icon>
+        <Input.Control.Message>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</Input.Control.Message>
     </Input.Control>
     <Input.Desc>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Input.Desc>
 </Input>

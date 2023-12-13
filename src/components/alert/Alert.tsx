c2d79c61bb0e5b3354ffe6bb26a2d4e013910f0d
nicosammito <nico@sammito.de>
2023-12-13 10:33:50 +0100

    Alert component

---
+++ src/components/alert/Alert.tsx
@@ -0,0 +1,63 @@
+import React, {ReactElement, ReactNode} from "react";
+import {IconAlertCircle, IconCircleCheck, IconCircleX, IconInfoCircle, IconX} from "@tabler/icons-react";
+import "./Alert.style.scss"
+
+export interface AlertType {
+
+    children?: ReactNode | ReactNode[]
+    title: ReactNode
+    //defaults to info
+    variant?: "primary" | "secondary" | "success" | "warning" | "error"
+    //defaults to true
+    icon?: boolean
+    //defaults to false
+    dismissible?: boolean
+    onClose?: (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
+}
+
+const IconVariants = {
+    "primary": <IconInfoCircle/>,
+    "secondary": <IconInfoCircle/>,
+    "success": <IconCircleCheck/>,
+    "warning": <IconAlertCircle/>,
+    "error": <IconCircleX/>
+
+}
+
+const Alert: React.FC<AlertType> = (props) => {
+
+    const {variant = "primary", dismissible = false, icon = true, title, onClose = (event) => {}, children} = props
+
+    return <div className={`alert alert--${variant}`}>
+        <div className={"alert__header"}>
+            <div className={"alert__header-wrapper"}>
+                {icon ? <AlertIcon variant={variant}/> : null}
+                <span className={"alert__heading"}>{title}</span>
+            </div>
+            {dismissible ? <span className={"alert__dismissible"} onClick={event => onClose(event)}><IconX/></span> : null}
+        </div>
+
+        {children ? <div className={"alert__content"}>
+            {children}
+        </div> : null}
+    </div>
+
+}
+
+export interface AlertHeadingType {
+    children: ReactNode
+}
+
+export interface AlertIconType {
+    variant: "primary" | "secondary" | "success" | "warning" | "error"
+}
+
+const AlertIcon: React.FC<AlertIconType> = ({variant}) => {
+    return <span className={"alert__icon"}>
+        {IconVariants[variant]}
+    </span>
+}
+
+
+
+export default Alert

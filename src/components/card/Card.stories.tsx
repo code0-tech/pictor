fcf46e58d75c127102ede560dd66ba670a591d9b
nicosammito <nico@sammito.de>
2023-12-19 08:21:21 +0100

    example card with image header footer and item with dropdown

--- src/components/card/Card.stories.tsx
+++ src/components/card/Card.stories.tsx
@@ -2,6 +2,10 @@ import {Meta} from "@storybook/react";
 import React from "react";
 import Card from "./Card";
 import ListGroup from "../list-group/ListGroup";
+import {CardFooter} from "./CardFooter";
+import ButtonGroup from "../button-group/ButtonGroup";
+import Button from "../button/Button";
+import Dropdown from "../dropdown/Dropdown";
 
 const meta: Meta = {
     title: "Card",
@@ -17,5 +21,27 @@ export const Test = () => {
             <Card.Title>Nico Sammito</Card.Title>
             <Card.Subtitle>Co-Founder</Card.Subtitle>
         </Card.Header>
+        <ListGroup>
+            <ListGroup.Item>
+                <Dropdown>
+                    <Dropdown.Trigger>
+                        Test
+                    </Dropdown.Trigger>
+                    <Dropdown.Menu>
+                        sd
+                    </Dropdown.Menu>
+                </Dropdown>
+            </ListGroup.Item>
+        </ListGroup>
+        <CardFooter>
+            <ButtonGroup>
+                <Button>
+                    Button
+                </Button>
+                <Button>
+                    Button
+                </Button>
+            </ButtonGroup>
+        </CardFooter>
     </Card>
 }

70827f7675f59871290f6d6ab465ce7a9d79b3e8
Raphael Goetz <raphael.goetz.7@gmail.com>
2024-02-16 22:16:55 +0100

    feat: added variants to Button.tsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -9,7 +9,9 @@ import {getChild, getContent} from "../../utils/utils";
 export interface ButtonType extends DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
     children: ReactNode | ReactNode[]
     //defaults to primary
-    variant?: "primary" | "secondary" | "info" | "success" | "warning" | "error",
+    color?: "primary" | "secondary" | "info" | "success" | "warning" | "error",
+    //default to normal
+    variant?: "none" | "normal" | "outlined" | "filled",
     //defaults to false
     active?: boolean
     //defaults to false
@@ -22,13 +24,20 @@ export interface ButtonIconType {
 
 const Button: React.FC<ButtonType> = (props) => {
 
-    const {children, variant = "primary", active = false, disabled = false, ...args} = props
+    const {children, variant = "normal", color = "primary", active = false, disabled = false, ...args} = props
     const icon = getChild(children, ButtonIcon)
     const content = getContent(children, ButtonIcon)
 
+    const getVariant = (): string => {
+        if (variant == "none") return "button__withoutAll";
+        if (variant == "outlined") return "button__withoutBorder";
+        if (variant == "filled") return "button__withoutBackground";
+        return "";
+    }
 
-    return <a {...args} className={`button button--${variant} ${active ? "button--active" : ""} ${disabled ? "button--disabled" : ""}`}
-        aria-disabled={disabled ? "true" : "false"}>
+    return <a {...args}
+              className={`button button--${color} ${active ? "button--active" : ""} ${disabled ? "button--disabled" : ""} ${getVariant()}`}
+              aria-disabled={disabled ? "true" : "false"}>
         {icon}
         {content ? <span className={"button__content"}>{content}</span> : null}
     </a>

875995a1a73c7cc91e1f29c43c5f49e2ed505595
Dario <derioreal63@gmail.com>
2024-02-17 22:22:56 +0100

    init MultiSelect

---
+++ src/components/MultiSelect/MultiSelect.stories.tsx
@@ -0,0 +1,25 @@
+import MultiSelect from "./MultiSelect";
+import {Meta, StoryObj} from "@storybook/react";
+import React from "react";
+
+
+const meta: Meta = {
+    title: "MultiSelect",
+    component: MultiSelect,
+}
+
+
+export default meta;
+
+type MenuStory = StoryObj<typeof MultiSelect>
+
+export const BasicPill: MenuStory = {
+    render: (args) => {
+
+        return <MultiSelect placeholder={"Select your items"}>
+            <MultiSelect.Option key={"1"}>Test (Key=1)</MultiSelect.Option>
+            <MultiSelect.Option key={"2"}>Test 2 (Key=2)</MultiSelect.Option>
+        </MultiSelect>
+
+    }
+}

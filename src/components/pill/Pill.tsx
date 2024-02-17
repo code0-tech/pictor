1fc39c0ce4dc244745f31556b120296de256ca57
Dario <derioreal63@gmail.com>
2024-02-18 00:53:40 +0100

    add href to pill

--- src/components/pill/Pill.tsx
+++ src/components/pill/Pill.tsx
@@ -11,6 +11,7 @@ export interface PillType {
     variant?: "primary" | "secondary" | "info" | "success" | "warning" | "error",
     //Defaults to md
     size?: "xs" | "sm" | "md" | "lg" | "xl",
+    href?: string,
     onRemoveButtonClick?: () => void,
 }
 
@@ -19,13 +20,23 @@ export interface PillContentType {
 }
 
 const Pill: React.FC<PillType> = (props) => {
-    const {children, removeButton = false, onRemoveButtonClick, variant = "primary", size = "md"} = props;
+    const {children, href, removeButton = false, onRemoveButtonClick, variant = "primary", size = "md"} = props;
 
     const content: string = getChild(children, PillContent, true)?.props.children
 
-    return <span className={`pill-root pill-variant-${variant} pill-size-${size}`}>
-            <span className={"pill-label"}>{content}</span>
-        {removeButton && <button title={"Pill Title"} onClick={onRemoveButtonClick} className={"pill-remove-button"}><IconX color={"#999999"} className={"pill-remove-icon"}/></button>}
+    return <span
+        className={`pill-root ${removeButton ? "rm-button-active" : ""} pill-variant-${variant} pill-size-${size} ${href && "pill-href"}`}
+        onClick={(event) => {
+            console.log(event.target)
+            if (!["pill-root", "pill-label"].includes((event.target as HTMLDivElement).id)) return
+            if (href) {
+                window.location.assign(href)
+            }
+        }} id={"pill-root"}>
+            <span id={"pill-label"} className={"pill-label"}>{content}</span>
+        {removeButton &&
+            <button title={"Pill Title"} onClick={onRemoveButtonClick} className={"pill-remove-button"}><IconX
+                color={"#999999"} className={"pill-remove-icon"}/></button>}
         </span>
 
 }

58a181cc87bb876f6865fd48e1cb5bbaa46781df
Dario <derioreal63@gmail.com>
2024-02-18 17:26:40 +0100

    add Click Event

--- src/components/pill/Pill.tsx
+++ src/components/pill/Pill.tsx
@@ -1,17 +1,16 @@
-import React from "react"
+import React, {AnchorHTMLAttributes, DetailedHTMLProps, MouseEventHandler} from "react"
 import "./Pill.style.scss"
 import {getChild} from "../../utils/utils";
 import {Button} from "../../index";
 import {IconX} from "@tabler/icons-react";
 
-export interface PillType {
+export interface PillType extends DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
     children: string,
     removeButton?: boolean,
     //defaults to primary
     color?: "primary" | "secondary" | "info" | "success" | "warning" | "error",
     //Defaults to md
     size?: "xs" | "sm" | "md" | "lg" | "xl",
-    onRemoveButtonClick?: () => void,
 }
 
 export interface PillContentType {
@@ -19,14 +18,14 @@ export interface PillContentType {
 }
 
 const Pill: React.FC<PillType> = (props) => {
-    const {children, removeButton = false, onRemoveButtonClick, color = "primary", size = "md"} = props;
+    const {children, removeButton = false, onClick, color = "primary", size = "md"} = props;
 
     return <span
         className={`pill pill--${color} pill--${size}`}
         id={"pill-root"}>
             <span id={`pill__label`} className={`pill__label ${removeButton ? "pill__label--active" : ""}`}>{children}</span>
         {removeButton &&
-            <Button variant={color} size={size} onClick={onRemoveButtonClick}>
+            <Button variant={color} size={size} onClick={onClick}>
                 <Button.Icon><IconX color={"#999999"}/></Button.Icon>
             </Button>
         }

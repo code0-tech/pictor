ee7494d8011d6d9192a6d76cd8d741612f545696
nicosammito <nico@sammito.de>
2024-02-18 21:25:19 +0100

    added offset 8 = 0.5rem

--- src/components/popover/InternalPopover.tsx
+++ src/components/popover/InternalPopover.tsx
@@ -10,7 +10,7 @@ interface InternalPopoverType extends Omit<AriaPopoverProps, 'popoverRef'> {
 
 export const InternalPopover: React.FC<InternalPopoverType> = (props) => {
 
-    const {children, state, offset = 0, ...args} = props
+    const {children, state, offset = 8, ...args} = props
     const popoverRef = React.useRef(null);
     const {popoverProps} = usePopover({
         ...args,

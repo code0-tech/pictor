d4aee5a32528e3d28d130c6c48a22640480ab0bd
Dario <derioreal63@gmail.com>
2024-02-18 13:44:19 +0100

    rename variant to color and remove Content Component

--- src/components/pill/Pill.tsx
+++ src/components/pill/Pill.tsx
@@ -5,10 +5,10 @@ import {Button} from "../../index";
 import {IconX} from "@tabler/icons-react";
 
 export interface PillType {
-    children: React.ReactElement<PillContentType> | React.ReactElement<PillContentType>[],
+    children: string,
     removeButton?: boolean,
     //defaults to primary
-    variant?: "primary" | "secondary" | "info" | "success" | "warning" | "error",
+    color?: "primary" | "secondary" | "info" | "success" | "warning" | "error",
     //Defaults to md
     size?: "xs" | "sm" | "md" | "lg" | "xl",
     href?: string,
@@ -20,12 +20,10 @@ export interface PillContentType {
 }
 
 const Pill: React.FC<PillType> = (props) => {
-    const {children, href, removeButton = false, onRemoveButtonClick, variant = "primary", size = "md"} = props;
-
-    const content: string = getChild(children, PillContent, true)?.props.children
+    const {children, href, removeButton = false, onRemoveButtonClick, color = "primary", size = "md"} = props;
 
     return <span
-        className={`pill-root ${removeButton ? "rm-button-active" : ""} pill-variant-${variant} pill-size-${size} ${href && "pill-href"}`}
+        className={`pill-root ${removeButton ? "rm-button-active" : ""} pill-color-${color} pill-size-${size} ${href && "pill-href"}`}
         onClick={(event) => {
             console.log(event.target)
             if (!["pill-root", "pill-label"].includes((event.target as HTMLDivElement).id)) return
@@ -33,7 +31,7 @@ const Pill: React.FC<PillType> = (props) => {
                 window.location.assign(href)
             }
         }} id={"pill-root"}>
-            <span id={"pill-label"} className={"pill-label"}>{content}</span>
+            <span id={"pill-label"} className={"pill-label"}>{children}</span>
         {removeButton &&
             <button title={"Pill Title"} onClick={onRemoveButtonClick} className={"pill-remove-button"}><IconX
                 color={"#999999"} className={"pill-remove-icon"}/></button>}
@@ -41,13 +39,5 @@ const Pill: React.FC<PillType> = (props) => {
 
 }
 
-const PillContent: React.FC<PillContentType> = (props) => {
 
-    const {children} = props;
-
-    return <>{children}</>
-}
-
-export default Object.assign(Pill, {
-    Content: PillContent,
-})
+export default Object.assign(Pill)

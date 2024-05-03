fdc2ecda32d83a20cec6f0ea4a49c98e10f6f201
nicosammito <nico@sammito.de>
2024-05-03 16:26:54 +0200

    implementation of simple flex container

---
+++ src/components/flex/Flex.tsx
@@ -0,0 +1,20 @@
+import {Code0Component} from "../../utils/types";
+import React from "react";
+import {mergeCode0Props} from "../../utils/utils";
+import "./Flex.style.scss";
+
+interface FlexType extends Code0Component<HTMLDivElement> {
+    children: React.ReactNode | React.ReactNode[]
+}
+
+const Flex: React.FC<FlexType> = props => {
+
+    const {children, ...rest} = props
+
+    return <div {...mergeCode0Props("flex", rest)}>
+        {children}
+    </div>
+
+}
+
+export default Flex

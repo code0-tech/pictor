4364ddd8dc94dd33ee7edf7ff848d2df8f0137fd
nicosammito <nico@sammito.de>
2024-05-18 14:57:55 +0200

    adding accessibilities but just removing them from the test-runner

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,9 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",

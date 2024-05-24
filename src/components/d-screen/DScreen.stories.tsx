8dc7521ce990cb442a0797894b41012c2df7dbe5
nicosammito <nico@sammito.de>
2024-05-24 14:29:34 +0200

    first implementation of screen component with sidebars, content and items

--- src/components/d-screen/DScreen.stories.tsx
+++ src/components/d-screen/DScreen.stories.tsx
@@ -1,20 +1,100 @@
 import {Meta} from "@storybook/react";
 import React from "react";
 import DScreen from "./DScreen";
+import Badge from "../badge/Badge";
+import {IconBrandAdobe, IconDatabase, IconHierarchy3, IconSettings, IconTicket} from "@tabler/icons-react";
+import Text from "../Text/Text";
 
 const meta: Meta = {
     title: "Dashboard Screen",
     parameters: {
         visualTest: {
             selector: 'body'
-        }
-    }
+        },
+        layout: "fullscreen"
+    },
+    component: DScreen
 }
 
 export const DashboardScreenExample = () => {
-    return <DScreen>
+    return <div style={{width: "100vw", height: "100vh", position: "relative"}}>
+        <DScreen>
+            <DScreen.VBar.Top>
+                <Badge>Home</Badge>
+                <Text size={"sm"} mx={0.5}>/</Text>
+                <Badge>Organisations</Badge>
+                <Text size={"sm"} mx={0.5}>/</Text>
+                <Badge>Code0</Badge>
+                <Text size={"sm"} mx={0.5}>/</Text>
+                <Badge>Projects</Badge>
+                <Text size={"sm"} mx={0.5}>/</Text>
+                <Badge>Sagittarius</Badge>
+            </DScreen.VBar.Top>
+            <DScreen.VBar.Bottom>
+                <Badge>Test</Badge>
+            </DScreen.VBar.Bottom>
+            <DScreen.HBar.Left justify={"space-between"}>
+                <div style={{display: "flex", flexDirection: "column", gap: ".5rem"}}>
+                    <DScreen.Item>
+                        <IconHierarchy3 size={12}/>
+                        Flows
+                    </DScreen.Item>
+                    <DScreen.Item>
+                        <IconTicket size={12}/>
+                        Issues
+                    </DScreen.Item>
+                    <DScreen.Item>
+                        <IconDatabase size={12}/>
+                        Database
+                    </DScreen.Item>
+                </div>
+                <div style={{display: "flex", flexDirection: "column", gap: ".25rem"}}>
+                    <DScreen.Item>
+                        <IconSettings size={12}/>
+                        Settings
+                    </DScreen.Item>
+                </div>
+            </DScreen.HBar.Left>
+            <DScreen.HBar.Right>
+                <DScreen.Item>
+                    Docs
+                </DScreen.Item>
+            </DScreen.HBar.Right>
+            <DScreen.Content>
+                <DScreen>
+                    <DScreen.HBar.Left>
+                        <Text size={"md"} hierarchy={"primary"}>
+                            Sagittarius
+                        </Text>
+                        <Text size={"xs"}>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                        </Text>
+                    </DScreen.HBar.Left>
+                    <DScreen.Content>
+                        <DScreen>
+                            <DScreen.VBar.Top justify={"flex-end"}>
+                                <DScreen.Item>
+                                    Test Run
+                                </DScreen.Item>
+                            </DScreen.VBar.Top>
+                            <DScreen.VBar.Bottom>
+                                <DScreen.Item>
+                                    Logs
+                                </DScreen.Item>
+                            </DScreen.VBar.Bottom>
+                            <DScreen.Content>
+                                <Text size={"md"}>
+                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                </Text>
+                            </DScreen.Content>
+                        </DScreen>
+                    </DScreen.Content>
+                </DScreen>
 
-    </DScreen>
+            </DScreen.Content>
+        </DScreen>
+    </div>
 }
 
 export default meta
--- src/components/d-screen/DScreen.tsx
+++ src/components/d-screen/DScreen.tsx
@@ -1,14 +1,129 @@
 import React from "react";
 import {Code0Component} from "../../utils/types";
 import "./DScreen.style.scss"
+import {getChild, mergeCode0Props} from "../../utils/utils";
 
 export interface DScreenType extends Code0Component<HTMLDivElement> {
-
+    children: React.ReactElement<Omit<VBar, "type"> & Content>[]
 }
 
-const DScreen: React.FC = () => {
-    return <></>
+/**
+ *
+ * @constructor
+ *
+ * @example
+ *
+ * <DScreen>
+ *     <DScreen.VBar>
+ *     </DScreen.VBar>
+ *     <DScreen.VContent>
+ *         <DScreen.HBar type="left">
+ *         </DScreen.HBar>
+ *         <DScreen.HContent>
+ *         </DScreen.HContent>
+ *         <DScreen.HBar type="right">
+ *         </DScreen.HBar>
+ *     </DScreen.VContent>
+ *     <DScreen.VBar>
+ *     </DScreen.VBar>
+ * </DScreen>
+ *
+ * @example
+ *
+ * <DScreen>
+ *     <DScreen.VBar.Top>
+ *      <DScreen.VBar.Bottom>
+ *     <DScreen.HBar.Left>
+ *     <DScreen.HBar.Right>
+ *     <DScreen.Content>
+ *
+ */
+const DScreen: React.FC<DScreenType> = (props) => {
+    const {children} = props
+    const vBarTop = getChild(children, VBarTop, false)
+    const vBarBottom = getChild(children, VBarBottom, false)
+    const hBarLeft = getChild(children, HBarLeft, false)
+    const hBarRight = getChild(children, HBarRight, false)
+    const content = getChild(children, Content, true)
+    return <div className={"d-screen"}>
+        {vBarTop ? vBarTop : null}
+        {content ? (
+            <div className={"d-screen__v-content"}>
+                {hBarLeft ? hBarLeft : null}
+                <div className={"d-screen__h-content"}>
+                    {content}
+                </div>
+                {hBarRight ? hBarRight : null}
+            </div>
+        ) : null}
+        {vBarBottom ? vBarBottom : null}
+    </div>
 }
 
-export default Object.assign(DScreen)
+export interface VBar extends Code0Component<HTMLDivElement> {
+    type: 'top' | 'bottom'
+    children: React.ReactNode[] | React.ReactNode
+}
+
+const VBar: React.FC<VBar> = (props) => {
+    const {type, children, ...rest} = props
+    return <div {...mergeCode0Props(`d-screen__v-bar d-screen__v-bar--${type}`, rest)}>
+        {children}
+    </div>
+}
+
+const VBarTop: React.FC<Omit<VBar, "type">> = (props) => <VBar type={"top"} {...props}/>
+const VBarBottom: React.FC<Omit<VBar, "type">> = (props) => <VBar type={"bottom"} {...props}/>
+
+export interface HBar extends Code0Component<HTMLDivElement> {
+    type: 'left' | 'right'
+    children: React.ReactNode[] | React.ReactNode
+}
+
+const HBar: React.FC<HBar> = (props) => {
+    const {type, children, ...rest} = props
+    return <div {...mergeCode0Props(`d-screen__h-bar d-screen__h-bar--${type}`, rest)}>
+        {children}
+    </div>
+}
+
+const HBarLeft: React.FC<Omit<HBar, "type">> = (props) => <HBar type={"left"} {...props}/>
+const HBarRight: React.FC<Omit<HBar, "type">> = (props) => <HBar type={"right"} {...props}/>
+
+export interface Content extends Code0Component<HTMLDivElement> {
+    children: React.ReactNode[] | React.ReactNode
+}
+
+const Content: React.FC<Content> = (props) => {
+    const {children, ...rest} = props
+    return <div {...mergeCode0Props(`d-screen__content`, rest)}>
+        {children}
+    </div>
+}
+
+export interface Item extends Code0Component<HTMLSpanElement> {
+    children: React.ReactNode[] | React.ReactNode
+    //defaults to false
+    active?: boolean
+}
+
+const Item: React.FC<Item> = (props) => {
+    const {children, active, ...rest} = props
+    return <span {...mergeCode0Props(`d-screen__item ${active ? "d-screen__item--active" : ""}`, rest)}>
+        {children}
+    </span>
+}
+
+export default Object.assign(DScreen, {
+    VBar: {
+        Top: VBarTop,
+        Bottom: VBarBottom
+    },
+    HBar: {
+        Left: HBarLeft,
+        Right: HBarRight
+    },
+    Content,
+    Item
+})
 

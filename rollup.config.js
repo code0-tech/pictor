08ad309e65386ac079dba660454239052b9347cf
nicosammito <nico@sammito.de>
2024-05-25 20:31:35 +0200

    cleanup package & add possibility for tree shaking and added support for client and server components (React 18) functionality

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,72 @@
 import resolve from "@rollup/plugin-node-resolve";
+import external from 'rollup-plugin-peer-deps-external';
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-
+import terser from "@rollup/plugin-terser"
 import postcss from "rollup-plugin-postcss";
-import visualizer from 'rollup-plugin-visualizer';
 import dts from "rollup-plugin-dts";
-
-const packageJson = require("./package.json");
-const extensions = ['.js', '.ts', '.jsx', '.tsx'];
-const excludeExtensions = [
-    'test.js',
-    'test.ts',
-    'test.jsx',
-    'test.tsx',
-    'stories.js',
-    'stories.ts',
-    'stories.jsx',
-    'stories.tsx',
-];
+import preserveDirectives from "rollup-plugin-preserve-directives";
 
 export default [{
-    input: "src/index.ts",
-    output: [
-        {
-            file: packageJson.main,
-            format: "cjs",
-            sourcemap: false,
-        },
-        {
-            file: packageJson.module,
-            format: "esm",
-            sourcemap: false,
-        },
-    ],
+    input: "./src/index.ts",
+    output: {
+        dir: "dist/esm",
+        format: "esm",
+        sourcemap: true,
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name.includes('node_modules')) {
+                return chunkInfo.name.replace('node_modules', 'external') + '.js';
+            }
+
+            return '[name].js';
+        }
+    },
     plugins: [
+        external(),
         resolve(),
         commonjs(),
         typescript({
             tsconfig: "./tsconfig.json",
             declaration: true,
-            declarationDir: 'dist',
+            declarationDir: 'dist/esm',
             exclude: ["**/__stories__", "**/*.stories.tsx"]
         }),
         postcss(),
-        visualizer({
-            filename: 'bundle-analysis.html',
-            open: false,
+        terser(),
+        preserveDirectives(),
+    ],
+    external: ["react", "react-dom"],
+}, {
+    input: "./src/index.ts",
+    output: {
+        dir: "dist/cjs",
+        format: "cjs",
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        sourcemap: true,
+        entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name.includes('node_modules')) {
+                return chunkInfo.name.replace('node_modules', 'external') + '.js';
+            }
+
+            return '[name].js';
+        }
+    },
+    plugins: [
+        external(),
+        resolve(),
+        commonjs(),
+        typescript({
+            tsconfig: "./tsconfig.json",
+            declaration: true,
+            declarationDir: 'dist/cjs',
+            exclude: ["**/__stories__", "**/*.stories.tsx"]
         }),
+        postcss(),
+        terser(),
+        preserveDirectives(),
     ],
     external: ["react", "react-dom"],
 }, {

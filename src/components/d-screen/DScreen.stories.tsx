9199fe7e43fe5e22ef37b3d782b293a0f6feec16
nicosammito <nico@sammito.de>
2024-05-27 00:03:31 +0200

    reimplemented responsive behaviour and collapse functionality. Also introduced new collapse item

--- src/components/d-screen/DScreen.stories.tsx
+++ src/components/d-screen/DScreen.stories.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import DScreen from "./DScreen";
 import Badge from "../badge/Badge";
 import {
+    IconApi,
     IconDatabase,
     IconHierarchy3,
     IconLayoutNavbarCollapse,
@@ -29,84 +30,82 @@ export const DashboardScreenExample = () => {
     return <DFullScreen>
         <DScreen>
             <DScreen.VBar.Top>
-                <Badge>Home</Badge>
-                <Text size={"sm"} mx={0.5}>/</Text>
-                <Badge>Organisations</Badge>
-                <Text size={"sm"} mx={0.5}>/</Text>
-                <Badge>Code0</Badge>
-                <Text size={"sm"} mx={0.5}>/</Text>
-                <Badge>Projects</Badge>
-                <Text size={"sm"} mx={0.5}>/</Text>
-                <Badge>Sagittarius</Badge>
+                <DScreen.BarContent mediaMaxWidth={800}>
+                    <Badge>Home</Badge>
+                    <Text size={"sm"} mx={0.5}>/</Text>
+                    <Badge>...</Badge>
+                    <Text size={"sm"} mx={0.5}>/</Text>
+                    <Badge>Sagittarius</Badge>
+                </DScreen.BarContent>
+                <DScreen.BarContent mediaMinWidth={800}>
+                    <Badge>Home</Badge>
+                    <Text size={"sm"} mx={0.5}>/</Text>
+                    <Badge>Organisations</Badge>
+                    <Text size={"sm"} mx={0.5}>/</Text>
+                    <Badge>Code0</Badge>
+                    <Text size={"sm"} mx={0.5}>/</Text>
+                    <Badge>Projects</Badge>
+                    <Text size={"sm"} mx={0.5}>/</Text>
+                    <Badge>Sagittarius</Badge>
+                </DScreen.BarContent>
             </DScreen.VBar.Top>
             <DScreen.VBar.Bottom>
-                <Badge>Test</Badge>
+                <DScreen.BarContent>
+                    <Badge>Test</Badge>
+                </DScreen.BarContent>
             </DScreen.VBar.Bottom>
-            <DScreen.HBar.Left justify={"space-between"}>
-                <div style={{display: "flex", flexDirection: "column", gap: ".5rem"}}>
-                    <DScreen.Item>
-                        <IconHierarchy3 size={12}/>
-                        Flows
-                    </DScreen.Item>
-                    <DScreen.Item>
-                        <IconTicket size={12}/>
-                        Issues
-                    </DScreen.Item>
-                    <DScreen.Item>
-                        <IconDatabase size={12}/>
-                        Database
-                    </DScreen.Item>
-                </div>
-                <div style={{display: "flex", flexDirection: "column", gap: ".5rem"}}>
-                    <DScreen.Item>
-                        <IconSettings size={12}/>
-                        Settings
-                    </DScreen.Item>
-                </div>
+            <DScreen.HBar.Left>
+                <DScreen.BarContent justify={"space-between"}>
+                    <div style={{display: "flex", flexDirection: "column", gap: ".5rem"}}>
+                        <DScreen.Item>
+                            <IconHierarchy3 size={12}/>
+                            Flows
+                        </DScreen.Item>
+                        <DScreen.Item>
+                            <IconTicket size={12}/>
+                            Issues
+                        </DScreen.Item>
+                        <DScreen.Item>
+                            <IconDatabase size={12}/>
+                            Database
+                        </DScreen.Item>
+                    </div>
+                    <div style={{display: "flex", flexDirection: "column", gap: ".5rem"}}>
+                        <DScreen.Item>
+                            <IconSettings size={12}/>
+                            Settings
+                        </DScreen.Item>
+                    </div>
+                </DScreen.BarContent>
             </DScreen.HBar.Left>
             <DScreen.HBar.Right>
-                <DScreen.Item>
-                    Docs
-                </DScreen.Item>
+                <DScreen.BarContent>
+                    <DScreen.Item>
+                        Docs
+                    </DScreen.Item>
+                </DScreen.BarContent>
             </DScreen.HBar.Right>
             <DScreen.Content>
                 <DScreen>
-                    <DScreen.HBar.Left defaultSize={300} maxSizes={[0, 300]}>
-                        {(size, collapsed, collapse) => {
+                    <DScreen.HBar.Left>
+                        {(collapsed, collapse) => {
                             return <>
-                                {size > 0 ? (
-                                    <>
+                                <DScreen.CollapsableItem pos={"absolute"} right={"-17px"} top={"10%"} onClick={collapse}>
+                                    {collapsed ? <IconLayoutSidebarRightCollapse/> : <IconLayoutSidebarLeftCollapse/>}
+                                </DScreen.CollapsableItem>
+                                {!collapsed ? (
+                                    <DScreen.BarContent>
                                         <Text size={"md"} hierarchy={"primary"}>
                                             Sagittarius 200
                                         </Text>
                                         <Text size={"xs"}>
                                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
                                         </Text>
-                                        <Button pos={"absolute"} bottom={"0"} mb={"0"}
-                                                color={collapsed ? "info" : "primary"} onClick={collapse}>
-                                            <Button.Icon>
-                                                {
-                                                    !collapsed ? <IconLayoutSidebarLeftCollapse/> :
-                                                        <IconLayoutSidebarRightCollapse/>
-                                                }
-                                            </Button.Icon>
-                                            {!collapsed ? "Collapse" : null}
-                                        </Button>
-                                    </>
+                                    </DScreen.BarContent>
                                 ) : (
-                                    <>
-                                        <Button
-                                            pos={"relative"}><Button.Icon><IconLayoutNavbarCollapse/></Button.Icon></Button>
-                                        <Button pos={"absolute"} bottom={"0"} mb={"0"}
-                                                color={collapsed ? "info" : "primary"} onClick={collapse}>
-                                            <Button.Icon>
-                                                {
-                                                    !collapsed ? <IconLayoutSidebarLeftCollapse/> :
-                                                        <IconLayoutSidebarRightCollapse/>
-                                                }
-                                            </Button.Icon>
-                                        </Button>
-                                    </>
+                                    <DScreen.BarContent>
+                                        <Button color={"info"}><Button.Icon><IconApi/></Button.Icon></Button>
+                                    </DScreen.BarContent>
                                 )}
                             </>
                         }}
@@ -114,14 +113,18 @@ export const DashboardScreenExample = () => {
                     <DScreen.Content>
                         <DScreen>
                             <DScreen.VBar.Top justify={"flex-end"}>
-                                <DScreen.Item>
-                                    Test Run
-                                </DScreen.Item>
+                                <DScreen.BarContent>
+                                    <DScreen.Item>
+                                        Test Run
+                                    </DScreen.Item>
+                                </DScreen.BarContent>
                             </DScreen.VBar.Top>
                             <DScreen.VBar.Bottom>
-                                <DScreen.Item>
-                                    Logs
-                                </DScreen.Item>
+                                <DScreen.BarContent>
+                                    <DScreen.Item>
+                                        Logs
+                                    </DScreen.Item>
+                                </DScreen.BarContent>
                             </DScreen.VBar.Bottom>
                             <DScreen.Content>
                                 <Text size={"md"}>
--- src/components/d-screen/DScreen.tsx
+++ src/components/d-screen/DScreen.tsx
@@ -1,4 +1,6 @@
-import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
+"use client"
+
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import {Code0Component} from "../../utils/types";
 import "./DScreen.style.scss"
 import {getChild, mergeCode0Props} from "../../utils/utils";
@@ -7,37 +9,6 @@ export interface DScreenType extends Code0Component<HTMLDivElement> {
     children: React.ReactElement<Omit<VBar, "type"> & Content>[]
 }
 
-/**
- *
- * @constructor
- *
- * @example
- *
- * <DScreen>
- *     <DScreen.VBar>
- *     </DScreen.VBar>
- *     <DScreen.VContent>
- *         <DScreen.HBar type="left">
- *         </DScreen.HBar>
- *         <DScreen.HContent>
- *         </DScreen.HContent>
- *         <DScreen.HBar type="right">
- *         </DScreen.HBar>
- *     </DScreen.VContent>
- *     <DScreen.VBar>
- *     </DScreen.VBar>
- * </DScreen>
- *
- * @example
- *
- * <DScreen>
- *     <DScreen.VBar.Top>
- *      <DScreen.VBar.Bottom>
- *     <DScreen.HBar.Left>
- *     <DScreen.HBar.Right>
- *     <DScreen.Content>
- *
- */
 const DScreen: React.FC<DScreenType> = (props) => {
     const {children} = props
     const vBarTop = getChild(children, VBarTop, false)
@@ -61,9 +32,7 @@ const DScreen: React.FC<DScreenType> = (props) => {
 }
 
 interface BarProps extends Omit<Code0Component<HTMLDivElement>, "children"> {
-    children: React.ReactNode[] | React.ReactNode | ((size: number, collapsed: boolean, collapse: () => void) => React.ReactNode | React.ReactNode[])
-    defaultSize?: number
-    maxSizes?: number[]
+    children: React.ReactElement<BarContentProps>[] | React.ReactElement<BarContentProps> | ((collapsed: boolean, collapse: () => void) => React.ReactElement<BarContentProps>[] | React.ReactElement<BarContentProps>)
 }
 
 export interface VBar extends BarProps {
@@ -76,58 +45,78 @@ export interface HBar extends BarProps {
 
 const VBar: React.FC<VBar> = (props) => {
     const {type, children, ...rest} = props
-    return <div {...mergeCode0Props(`d-screen__v-bar d-screen__v-bar--${type}`, rest)}>
-        <div {...mergeCode0Props(`d-screen__v-bar-content`, rest)}>
-            {typeof children === "function" ? children(1, false, () => {}) : children}
-        </div>
+    const [collapsed, setCollapsed] = useState(false)
+    const barRef = useRef<HTMLDivElement | null>(null)
+
+    const collapse = () => {
+        setCollapsed(prevState => !prevState)
+    }
+
+    const child = typeof children === "function" ? useMemo(() => children(collapsed, collapse), [collapsed]) : children
+
+    return <div ref={barRef} {...mergeCode0Props(`d-screen__v-bar d-screen__v-bar--${type}`, rest)}>
+        {child}
     </div>
 }
 const HBar: React.FC<HBar> = (props) => {
 
-    const {type, children, maxSizes, defaultSize = 0, ...rest} = props
-
-    const [size, setSize] = useState<number>(defaultSize)
+    const {type, children, ...rest} = props
     const [collapsed, setCollapsed] = useState(false)
     const barRef = useRef<HTMLDivElement | null>(null)
 
-    const callback = () => {
-        if (!barRef.current || collapsed) return
-        const width = window.innerWidth
-        const matchingSize = maxSizes?.sort((a, b) => b - a).find((size) => size <= width) ?? 0
-        setSize(matchingSize)
-    }
-
     const collapse = () => {
-        setCollapsed(prevState => {
-            const newValue = !prevState
-            const minSize = maxSizes?.sort((a, b) => a - b)[0] ?? 0
-            newValue && setSize(minSize)
-            return newValue
-        })
+        setCollapsed(prevState => !prevState)
     }
 
-
-    useEffect(() => {
-        if (!barRef.current) return
-        window.addEventListener("resize", callback)
-        callback()
-        return () => {
-            window.removeEventListener("resize", callback)
-        }
-    }, [collapsed])
-
-    const child = typeof children === "function" ? useMemo(() => children(size, collapsed, collapse), [size, collapsed]) : children
+    const child = typeof children === "function" ? useMemo(() => children(collapsed, collapse), [collapsed]) : children
 
     return <div ref={barRef} {...mergeCode0Props(`d-screen__h-bar d-screen__h-bar--${type}`, rest)}>
-        <div {...mergeCode0Props(`d-screen__h-bar-content`, rest)}>
-            {child}
-        </div>
+        {child}
     </div>
 }
 const VBarTop: React.FC<Omit<VBar, "type">> = (props) => <VBar type={"top"} {...props}/>
 const VBarBottom: React.FC<Omit<VBar, "type">> = (props) => <VBar type={"bottom"} {...props}/>
 const HBarLeft: React.FC<Omit<HBar, "type">> = (props) => <HBar type={"left"} {...props}/>
 const HBarRight: React.FC<Omit<HBar, "type">> = (props) => <HBar type={"right"} {...props}/>
+
+export interface BarContentProps extends Code0Component<HTMLDivElement> {
+    children: React.ReactNode | React.ReactNode[]
+    mediaMinHeight?: number
+    mediaMinWidth?: number
+    mediaMaxHeight?: number
+    mediaMaxWidth?: number
+}
+
+const BarContent: React.FC<BarContentProps> = props => {
+    const {
+        children,
+        mediaMinHeight = 0,
+        mediaMinWidth = 0,
+        mediaMaxHeight = 0,
+        mediaMaxWidth = 0,
+        ...rest} = props
+    const contentRef = useRef<HTMLDivElement | null>(null)
+
+    const resizeCallback = () => {
+        if (!contentRef.current) return
+        if ((window.innerWidth >= mediaMinWidth && window.innerWidth < (mediaMaxWidth == 0 ? window.innerWidth + 1 : mediaMaxWidth))
+            && (window.innerHeight >= mediaMinHeight && window.innerHeight < (mediaMaxHeight == 0 ? window.innerHeight + 1 : mediaMaxHeight))) {
+            contentRef.current.style.display = "flex"
+        } else {
+            contentRef.current.style.display = "none"
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', resizeCallback)
+        resizeCallback()
+        return () => window.removeEventListener('resize', resizeCallback)
+    }, [mediaMinHeight, mediaMinWidth, mediaMaxHeight, mediaMaxWidth])
+
+    return <div ref={contentRef} {...mergeCode0Props(`d-screen__bar-content`, rest)}>
+        {props.children}
+    </div>
+}
 
 export interface Content extends Code0Component<HTMLDivElement> {
     children: React.ReactNode[] | React.ReactNode
@@ -153,6 +142,17 @@ const Item: React.FC<Item> = (props) => {
     </span>
 }
 
+export interface CollapsableItemProps extends Code0Component<HTMLDivElement> {
+    children: React.ReactNode[] | React.ReactNode
+}
+
+const CollapsableItem: React.FC<CollapsableItemProps> = props => {
+    const {children, ...rest} = props
+    return <div {...mergeCode0Props(`d-screen__collapsable-item`, rest)}>
+        {children}
+    </div>
+}
+
 export default Object.assign(DScreen, {
     VBar: {
         Top: VBarTop,
@@ -163,6 +163,8 @@ export default Object.assign(DScreen, {
         Right: HBarRight
     },
     Content,
-    Item
+    Item,
+    BarContent,
+    CollapsableItem
 })
 

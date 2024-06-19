74087267ab9827907538bcb4ab1d679ad33759ff
nicosammito <nico@sammito.de>
2024-06-19 11:36:01 +0200

    removed icon from button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,7 +2,7 @@ import "./Button.style.scss"
 import React, {
     ReactNode
 } from "react";
-import {getChild, getContent, mergeCode0Props} from "../../utils/utils";
+import {mergeCode0Props} from "../../utils/utils";
 import {Code0Component, Color} from "../../utils/types"
 
 export interface ButtonType extends Code0Component<HTMLAnchorElement> {
@@ -17,31 +17,16 @@ export interface ButtonType extends Code0Component<HTMLAnchorElement> {
     disabled?: boolean
 }
 
-export interface ButtonIconType extends Code0Component<HTMLSpanElement>{
-    children: ReactNode
-}
-
 const Button: React.FC<ButtonType> = (props) => {
 
     const {children, variant = "normal", color = "primary", active = false, disabled = false, ...args} = props
-    const icon = getChild(children, ButtonIcon)
-    const content = getContent(children, ButtonIcon)
 
     return <a {...mergeCode0Props(`button button--${color} ${active ? "button--active" : ""} ${disabled ? "button--disabled" : ""} button--${variant}`, args)}
               aria-disabled={disabled ? "true" : "false"}>
-        {icon}
-        {content ? <span className={"button__content"}>{content}</span> : null}
+        {children}
     </a>
 }
 
-const ButtonIcon: React.FC<ButtonIconType> = (props) => {
 
-    const {children, ...args} = props
-    return <span {...mergeCode0Props("button__icon", args)}>
-        {children}
-    </span>
-}
 
-export default Object.assign(Button, {
-    Icon: ButtonIcon
-});
+export default Object.assign(Button);

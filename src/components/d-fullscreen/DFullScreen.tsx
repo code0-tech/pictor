e30c6f07c1a22604e944c4530a9471f2f74366a4
nicosammito <nico@sammito.de>
2024-07-16 16:39:33 +0200

    2: fixing scroll behavior on ios that interferes with vertical resizing

--- src/components/d-fullscreen/DFullScreen.tsx
+++ src/components/d-fullscreen/DFullScreen.tsx
@@ -19,7 +19,8 @@ const DFullScreen: React.FC<DFullScreenProps> = props => {
     }, [])
 
     return <div style={{
-        position: "fixed",
+        position: "relative",
+        overflow: "hidden",
         ...(dimensions[0] ? {width: dimensions[0]} : {}),
         ...(dimensions[1] ? {height: dimensions[1]} : {})
     }}>

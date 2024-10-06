83884694234d592cc4c6e16d7e494bb2cf66ff8f
nicosammito <nico@sammito.de>
2024-10-06 19:22:35 +0200

    outsource DScreen components into separate files

--- src/components/d-screen/DScreen.stories.tsx
+++ src/components/d-screen/DScreen.stories.tsx
@@ -12,6 +12,8 @@ import {
 import Text from "../Text/Text";
 import DFullScreen from "../d-fullscreen/DFullScreen";
 import DFolder, {useFolderControls} from "../d-folder/DFolder";
+import DScreenBar from "./DScreenBar";
+import DScreenButton from "./DScreenButton";
 
 const meta: Meta = {
     title: "Dashboard Screen",
@@ -30,123 +32,94 @@ export const DashboardScreenExample = () => {
 
     return <DFullScreen>
         <DScreen>
-            <DScreen.VBar.Top>
-                <DScreen.BarContent align={"center"} mediaMaxWidth={800}>
-                    <Badge>Home</Badge>
-                    <Text size={"sm"} mx={0.5}>/</Text>
-                    <Badge>...</Badge>
-                    <Text size={"sm"} mx={0.5}>/</Text>
-                    <Badge>Sagittarius</Badge>
-                </DScreen.BarContent>
-                <DScreen.BarContent align={"center"} mediaMinWidth={800}>
-                    <Badge>Home</Badge>
-                    <Text size={"sm"} mx={0.5}>/</Text>
-                    <Badge>Organisations</Badge>
-                    <Text size={"sm"} mx={0.5}>/</Text>
-                    <Badge>Code0</Badge>
-                    <Text size={"sm"} mx={0.5}>/</Text>
-                    <Badge>Projects</Badge>
-                    <Text size={"sm"} mx={0.5}>/</Text>
-                    <Badge>Sagittarius</Badge>
-                </DScreen.BarContent>
-            </DScreen.VBar.Top>
-            <DScreen.VBar.Bottom>
-                <DScreen.BarContent>
-                    <Badge>Test</Badge>
-                </DScreen.BarContent>
-            </DScreen.VBar.Bottom>
-            <DScreen.HBar.Left resizeable>
-                <DScreen.BarContent justify={"space-between"}>
-                    <div style={{display: "flex", flexDirection: "column", gap: ".5rem", overflow: "hidden"}}>
-                        <DScreen.Item>
-                            <IconHierarchy3 size={12}/>
-                            Flows
-                        </DScreen.Item>
-                        <DScreen.Item>
-                            <IconTicket size={12}/>
-                            Issues
-                        </DScreen.Item>
-                        <DScreen.Item>
-                            <IconDatabase size={12}/>
-                            Database
-                        </DScreen.Item>
-                    </div>
-                    <div style={{display: "flex", flexDirection: "column", gap: ".5rem", overflow: "hidden"}}>
-                        <DScreen.Item>
-                            <IconSettings size={12}/>
-                            Settings
-                        </DScreen.Item>
-                    </div>
-                </DScreen.BarContent>
-            </DScreen.HBar.Left>
-            <DScreen.HBar.Right resizeable>
-                <DScreen.BarContent>
-                    <DScreen.Item>
-                        Docs
-                    </DScreen.Item>
-                </DScreen.BarContent>
-            </DScreen.HBar.Right>
-            <DScreen.Content>
-                <DScreen>
-                    <DScreen.HBar.Left resizeable>
-                        <DScreen.BarContent p={"0"}>
-                            <DScreen>
-                                <DScreen.VBar.Top>
-                                    <DScreen.BarContent>
-                                        <DScreen.Item onClick={openAll}>
-                                            Open All
-                                        </DScreen.Item>
-                                        <DScreen.Item onClick={closeAll}>
-                                            Close All
-                                        </DScreen.Item>
-                                    </DScreen.BarContent>
-                                </DScreen.VBar.Top>
-                                <DScreen.Content p={0.5}>
-                                    {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(() => {
-                                        return <DFolder controls={controls} name={"Google Cloud Flows"} defaultOpen>
-                                            <DFolder controls={controls} name={"Google Cloud Flows"}>
-                                                <DFolder.Item icon={<IconBrandAdobe size={12}/>}
-                                                              name={"Google Cloud Flows"}/>
-                                                <DFolder.Item active name={"Google Cloud Flows"}/>
-                                            </DFolder>
-                                        </DFolder>
-                                    })}
-                                </DScreen.Content>
-                            </DScreen>
-                        </DScreen.BarContent>
-                    </DScreen.HBar.Left>
-                    <DScreen.Content>
-                        <DScreen>
-                            <DScreen.VBar.Top resizeable justify={"flex-end"}>
-                                <DScreen.BarContent>
-                                    <DScreen.Item>
-                                        Test Run
-                                    </DScreen.Item>
-                                </DScreen.BarContent>
-                            </DScreen.VBar.Top>
-                            <DScreen.VBar.Bottom resizeable>
-                                <DScreen.BarContent>
-                                    <DScreen.Item>
-                                        Logs
-                                    </DScreen.Item>
-                                </DScreen.BarContent>
-                            </DScreen.VBar.Bottom>
-                            <DScreen.Content>
-                                <Text size={"md"}>
-                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                                </Text>
-                            </DScreen.Content>
-                        </DScreen>
-                    </DScreen.Content>
-                </DScreen>
+            <DScreenBar type={"top"} align={"center"}>
+                <Text>Home</Text>
+                <Text mx={0.5}>/</Text>
+                <Text>Organisations</Text>
+                <Text mx={0.5}>/</Text>
+                <Text>Code0</Text>
+                <Text mx={0.5}>/</Text>
+                <Text>Projects</Text>
+                <Text mx={0.5}>/</Text>
+                <Text>Sagittarius</Text>
+            </DScreenBar>
+            <DScreenBar type={"bottom"}>
+                <Text>Test</Text>
+            </DScreenBar>
+            <DScreenBar type={"left"} justify={"space-between"} resizeable>
+                <div style={{display: "flex", flexDirection: "column", gap: ".5rem", overflow: "hidden"}}>
+                    <DScreenButton color={"info"}>
+                        <IconHierarchy3 size={12}/>
+                        Flows
+                    </DScreenButton>
+                    <DScreenButton>
+                        <IconTicket size={12}/>
+                        Issues
+                    </DScreenButton>
+                    <DScreenButton>
+                        <IconDatabase size={12}/>
+                        Database
+                    </DScreenButton>
+                </div>
+                <div style={{display: "flex", flexDirection: "column", gap: ".5rem", overflow: "hidden"}}>
+                    <DScreenButton>
+                        <IconSettings size={12}/>
+                        Settings
+                    </DScreenButton>
+                </div>
+            </DScreenBar>
+            <DScreenBar type={"right"} resizeable>
+                <DScreenButton>
+                    Docs
+                </DScreenButton>
+            </DScreenBar>
 
-            </DScreen.Content>
+            <DScreen>
+                <DScreenBar type={"left"} p={"0"} resizeable>
+                    <DScreen>
+                        <DScreenBar type={"top"}>
+                            <DScreenButton onClick={openAll}>
+                                Open All
+                            </DScreenButton>
+                            <DScreenButton onClick={closeAll}>
+                                Close All
+                            </DScreenButton>
+                        </DScreenBar>
+                        <div style={{padding: ".5rem"}}>
+                            {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(() => {
+                                return <DFolder controls={controls} name={"Google Cloud Flows"} defaultOpen>
+                                    <DFolder controls={controls} name={"Google Cloud Flows"}>
+                                        <DFolder.Item icon={<IconBrandAdobe size={12}/>}
+                                                      name={"Google Cloud Flows"}/>
+                                        <DFolder.Item active name={"Google Cloud Flows"}/>
+                                    </DFolder>
+                                </DFolder>
+                            })}
+                        </div>
+                    </DScreen>
+                </DScreenBar>
+                <DScreen>
+                    <DScreenBar type={"top"} resizeable justify={"flex-end"}>
+                        <DScreenButton>
+                            Test Run
+                        </DScreenButton>
+                    </DScreenBar>
+                    <DScreenBar type={"bottom"} resizeable>
+                        <DScreenButton>
+                            Logs
+                        </DScreenButton>
+                    </DScreenBar>
+                    <Text>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                    </Text>
+                </DScreen>
+            </DScreen>
         </DScreen>
     </DFullScreen>
 }
 
-export const DashboardResizeLabelTest = () => {
+/*export const DashboardResizeLabelTest = () => {
     return <DFullScreen>
         <DScreen>
             <DScreen.VBar.Top>
@@ -175,13 +148,13 @@ export const DashboardResizeLabelTest = () => {
                 </DScreen.BarContent>
             </DScreen.VBar.Bottom>
             <DScreen.Content>
-                <Text size={"md"}>
+                <Text >
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
                     tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
                 </Text>
             </DScreen.Content>
         </DScreen>
     </DFullScreen>
-}
+}*/
 
 export default meta

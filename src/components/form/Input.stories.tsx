43f4bf3e752e41d364e9d14c9a9c0c3c7328492f
nicosammito <nico@sammito.de>
2024-10-08 20:10:24 +0200

    implemented useForm hook and test component

--- src/components/form/Input.stories.tsx
+++ src/components/form/Input.stories.tsx
@@ -1,112 +1,29 @@
-import React from "react";
+import React, {forwardRef} from "react";
 import useForm from "./useForm";
-import Input from "./Input";
-import Card from "../card/Card";
-import Button from "../button/Button";
-import {IconEye, IconKey, IconLogin, IconMail, IconMinus, IconPlus, IconX} from "@tabler/icons-react";
-import ButtonGroup from "../button-group/ButtonGroup";
-import Text from "../Text/Text";
-import PasswordInput from "./PasswordInput";
 
+const TestC: React.FC<any> = forwardRef((props, ref) => {
+    console.log(props)
+    return <input type={"text"} {...props} ref={ref}></input>
+})
 export default {
     title: "Input",
-    component: Input
+    component: TestC
 }
 
-export const Login = () => {
+export const Test = () => {
 
     const [inputs, validate] = useForm({
         initialValues: {
-            email: null,
-            password: null,
-            checkbox: true
+            test: "test@"
         },
         validate: {
-            email: (value) => {
-                if (!value) return "Email is required"
-                return null
-            },
-            password: (value) => {
-                if (!value) return "Password is required"
-                return null
-            }
-        },
-        onSubmit: (values) => {
-            console.log(values)
+            test: (value) => !value.includes("@") ? "Hier ist was falsch" : null
         }
     })
 
-    return <Card maw={300}>
-        <Text size={"xl"} display={"block"} hierarchy={"primary"}>Login</Text>
-        <br/>
-        <Text size={"sm"} display={"block"}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat, sed diam voluptua.
-        </Text>
-        <br/>
-        <form>
-            <Input
-                placeholder={"Email"}
-                label={"Email"}
-                type={"email"}
-                description={"Your Email address for login"}
-                left={<IconMail size={13}/>}
-                right={<Button><IconX size={13}/></Button>}
-                {...inputs.email}
-            />
-            <br/>
-            <PasswordInput
-                placeholder={"Password"}
-                label={"Password"}
-                description={"Your password for login"}
-                left={<IconKey size={13}/>}
-                {...inputs.password}
-            />
-            <br/>
-            <div style={{
-                display: "flex",
-                justifyContent:"end"
-            }}>
-                <Button color={"info"} onClick={validate}>
-                    <IconLogin size={13}/>
-                    Login
-                </Button>
-
-            </div>
-
-        </form>
-    </Card>
-
-}
-
-export const WebsiteInput = () => {
-
-    return <Card maw={300}>
-        <Input
-            label={"website"}
-            description={"Your website for linking"}
-            type={"text"}
-            placeholder={"code0.tech"}
-            left={"https://"}
-            leftType={"placeholder"}
-            right={<Button><IconX size={13}/></Button>}
-        />
-    </Card>
-
-}
-
-export const NumberInput = () => {
-
-    return <Card maw={300}>
-        <Input
-            label={"number"}
-            description={"Increase and decrease your number"}
-            type={"number"}
-            placeholder={"code0.tech"}
-            left={<Button color={"secondary"}><IconMinus size={13}/></Button>}
-            leftType={"action"}
-            right={<Button color={"secondary"}><IconPlus size={13}/></Button>}
-        />
-    </Card>
+    return <>
+        <TestC {...inputs.test}/>
+        <button onClick={validate}></button>
+    </>
 
 }
--- src/components/form/useForm.ts
+++ src/components/form/useForm.ts
@@ -1,85 +1,69 @@
 "use client"
 
-import {RefObject, useCallback, useRef, useState} from "react";
+import {RefObject, useCallback, useMemo, useRef, useState} from "react";
 
 export type Validations<Values> = Partial<{
     [Key in keyof Values]: (value: Values[Key]) => string | null;
 }>
 
-export interface FormValidationProps<Values> {
+interface FormValidationProps<Values> {
     initialValues: Values
-    validate?: Validations<Values>,
-    onSubmit?: (values: Values) => void
+    validate?: Validations<Values>
 }
 
-export interface ValidationProps<Value> {
-    defaultValue?: Value
-    valid?: boolean
-    notValidMessage?: string | null
-    //ref?: RefObject<HTMLInputElement>,
-    required?: boolean
+interface ValidationProps<Value> {
+    value: Value
+    valid: boolean
+    notValidMessage: string | null
+    ref: RefObject<HTMLInputElement>
 }
 
-export type ValidationsProps<Values> = Partial<{
+type ValidationsProps<Values> = Partial<{
     [Key in keyof Values]: ValidationProps<Values[Key]>
 }>
 
-export type FormValidationReturn<Values> = [ValidationsProps<Values>, () => void]
-
+type FormValidationReturn<Values> = [ValidationsProps<Values>, () => void]
 
 const useForm = <Values extends Record<string, any> = Record<string, any>>(props: FormValidationProps<Values>): FormValidationReturn<Values> => {
 
-    const {initialValues, validate, onSubmit = () => {}} = props
+    const {initialValues, validate} = props
 
-    const refs = Object.entries(initialValues).map(() =>  useRef<HTMLInputElement | null>(null))
-    const [inputProps, setInputProps] = useState<ValidationsProps<Values>>({})
+    const [valuesState, setValuesStore] = useState<Values>(initialValues)
+    const inputProps = useMemo<ValidationsProps<Values>>(() => {
+
+        let inputProps: ValidationsProps<Values> = {}
+
+        Object.entries(valuesState).map(([k, v]) => ({
+            name: k,
+            initialValue: v,
+            function: !!validate && !!validate[k] ? validate[k] : (value: typeof v) => null
+        })).forEach(item => {
+            Object.assign(inputProps, {
+                [item.name]: {
+                    defaultValue: item.initialValue,
+                    notValidMessage: item.function(item.initialValue),
+                    valid: !item.function(item.initialValue),
+                    ref: useRef<HTMLInputElement>(null)
+                }
+            })
+        })
+
+        return inputProps
+
+    }, [valuesState])
 
     const validateFunction = useCallback(() => {
 
-        let submittable = true
-        let inputProps: ValidationsProps<Values> = {}
-        let submitProps: Object = {}
+        let values: { [index: string]: any } = {...valuesState}
 
-        Object.entries(initialValues).map(([k, v], index) => {
-
-            const inputRef: RefObject<HTMLInputElement> = refs[index]
-            const type = inputRef.current?.type ?? "text"
-            const currentValue = (type == "checkbox" || type == "radio") ? inputRef.current?.checked : inputRef.current?.value
-
-            return {
-                name: k,
-                value: currentValue as typeof v || undefined,
-                function: !!validate && !!validate[k] ? validate[k] : (value: typeof currentValue) => null
-            }
-
-        }).forEach((item, index) => {
-
-            const message = item.value !== null ? item.function(item.value) : null
-            submittable = submittable ? message === null ? true : !message : false
-
-            Object.assign(inputProps, {
-                [item.name]: {
-                    defaultValue: item.value,
-                    notValidMessage: message,
-                    valid: message === null ? true : !message,
-                    ref: refs[index],
-                    ...(!!validate ? {required: true} : {})
-                }
-            })
-
-            Object.assign(submitProps, {
-                [item.name]: item.value
-            })
-
+        Object.entries(inputProps as ValidationsProps<Values>).forEach(([key, value]) => {
+            const inputRef: RefObject<HTMLInputElement> = value.ref
+            values[key] = inputRef.current?.value
         })
 
-        setInputProps(() => inputProps)
+        setValuesStore(values as Values)
 
-        if (submittable) onSubmit(submitProps as Values)
-
-
-
-    }, [])
+    }, [inputProps])
 
     return [
         inputProps,

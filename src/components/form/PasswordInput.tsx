551f581553cd1bf8bf06a27f863a0794d470f686
nicosammito <nico@sammito.de>
2024-10-27 17:28:35 +0100

    adding possibility to control type and add right elements

--- src/components/form/PasswordInput.tsx
+++ src/components/form/PasswordInput.tsx
@@ -1,10 +1,9 @@
 import React, {RefObject} from "react";
 import Input, {InputProps} from "./Input";
-import ButtonGroup from "../button-group/ButtonGroup";
 import {IconEye, IconX} from "@tabler/icons-react";
 import Button from "../button/Button";
 
-interface PasswordInputProps extends Omit<InputProps<string | null>, "wrapperComponent" | "right" | "rightType" | "type"> {
+interface PasswordInputProps extends Omit<InputProps<string | null>, "wrapperComponent" | "type"> {
     clearable?: boolean,
     visible?: boolean
 }
@@ -15,6 +14,7 @@ const PasswordInput: React.ForwardRefExoticComponent<PasswordInputProps> = React
     const {
         clearable = true,
         visible = true,
+        right,
         ...rest
     } = props
 
@@ -27,10 +27,10 @@ const PasswordInput: React.ForwardRefExoticComponent<PasswordInputProps> = React
         else if (ref.current && ref.current.type == "text") ref.current.type = "password"
     }
 
-    const rightAction = clearable && visible ? <ButtonGroup>
-        <Button onClick={toVisible}><IconEye size={13}/></Button>
-        <Button onClick={toClearable}><IconX size={13}/></Button>
-    </ButtonGroup> : clearable || visible ? <Button onClick={clearable ? toClearable : toVisible}>{clearable ? <IconX size={13}/> : <IconEye size={13}/>}</Button> : <></>
+    const rightAction = [right]
+    visible && rightAction.push(<Button onClick={toVisible}><IconEye size={13}/></Button>)
+    clearable && rightAction.push(<Button onClick={toClearable}><IconX size={13}/></Button>)
+
 
     return <Input
         right={rightAction}

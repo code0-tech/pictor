f2effb6d7c6221c57734fec665c011014c5e89cf
nicosammito <nico@sammito.de>
2025-02-13 16:56:41 +0100

    feat: adding all necessary events

--- src/components/d-split-screen/DSplitter.tsx
+++ src/components/d-split-screen/DSplitter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {DSplitPaneHandle} from "./DSplitPane";
 
 import "./DSplitter.style.scss"
@@ -12,9 +12,58 @@ export interface DSplitterProps {
 
 const DSplitter: React.FC<DSplitterProps> = (props) => {
 
-    const {firstPane, secondPane, direction = "horizontal"} = props
+    const {firstPane, secondPane, direction} = props
 
-    return <div className={`d-splitter d-splitter--${direction}`}/>
+    const ref = React.useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+
+        const disableSelect = (event: MouseEvent) => event.preventDefault();
+
+        const onCursorMove = (event: MouseEvent | TouchEvent) => {
+            //TODO
+            //calculate and set new size
+        }
+
+        const onCursorUp = (event: MouseEvent | TouchEvent) => {
+            //TODO
+            //disable child splitters if they are in contact with current
+            //level splitters
+        }
+
+        const onCursorDown = (event: MouseEvent | TouchEvent) => {
+            //TODO
+
+            //check if mouse is in resize area
+
+            //deselect text
+            const selection = document.getSelection()
+            if (selection) selection.removeAllRanges()
+
+            window.addEventListener("touchcancel", onCursorUp)
+            window.addEventListener("touchend", onCursorUp)
+            window.addEventListener("mouseup", onCursorUp)
+
+            window.addEventListener('selectstart', disableSelect);
+
+            window.addEventListener("touchmove", onCursorMove)
+            window.removeEventListener("mousemove", onCursorMove)
+        }
+
+        window.addEventListener("touchstart", onCursorDown)
+        window.addEventListener("mousedown", onCursorDown)
+        //styling for resize areas
+
+        return () => {
+            window.removeEventListener("touchstart", onCursorDown)
+            window.removeEventListener("mousedown", onCursorDown)
+            //styling for resize areas
+        }
+
+
+    }, [firstPane, secondPane, ref]);
+
+    return <div ref={ref} className={`d-splitter d-splitter--${direction}`}/>
 }
 
 export default DSplitter

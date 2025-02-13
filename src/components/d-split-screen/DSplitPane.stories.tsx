bce04fc35bc6b770f8b46325673cdc3bdaad1828
nicosammito <nico@sammito.de>
2025-02-13 14:40:20 +0100

    feat: new pane which just handles the content

---
+++ src/components/d-split-screen/DSplitPane.stories.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect} from "react";
+import {Meta} from "@storybook/react";
+import DFullScreen from "../d-fullscreen/DFullScreen";
+import DSplitScreen from "./DSplitScreen";
+import DSplitPane, {DSplitPaneHandle} from "./DSplitPane";
+
+const meta: Meta = {
+    title: "DSplitPane",
+    parameters: {
+        visualTest: {
+            selector: 'body'
+        },
+        layout: "fullscreen"
+    },
+}
+
+export const test = () => {
+
+
+    return <DFullScreen>
+        <DSplitScreen>
+            <DSplitPane>
+                <DSplitScreen direction={"vertical"}>
+                    <DSplitPane>
+                        s
+                    </DSplitPane>
+                    <DSplitPane>
+                        s
+                    </DSplitPane>
+                </DSplitScreen>
+            </DSplitPane>
+            <DSplitPane>
+                s
+            </DSplitPane>
+        </DSplitScreen>
+    </DFullScreen>
+
+}
+
+export default meta

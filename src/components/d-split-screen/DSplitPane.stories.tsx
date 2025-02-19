ab9dba9fd13487d18061db4138b0f7dd111b06ef
nicosammito <nico@sammito.de>
2025-02-19 15:06:30 +0100

    feat: new more realistic testing environment

--- src/components/d-split-screen/DSplitPane.stories.tsx
+++ src/components/d-split-screen/DSplitPane.stories.tsx
@@ -1,8 +1,12 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {Meta} from "@storybook/react";
 import DFullScreen from "../d-fullscreen/DFullScreen";
 import DSplitScreen from "./DSplitScreen";
-import DSplitPane, {DSplitPaneHandle} from "./DSplitPane";
+import DSplitPane from "./DSplitPane";
+import DFolder, {useFolderControls} from "../d-folder/DFolder";
+import DScreenButton from "../d-screen/DScreenButton";
+import {IconBrandAdobe, IconDatabase, IconHierarchy3, IconSettings, IconTicket} from "@tabler/icons-react";
+import {ZoomPanPinchExample} from "../d-zoom-pan-pinch/DZoomPanPinch.stories";
 
 const meta: Meta = {
     title: "DSplitPane",
@@ -19,10 +23,11 @@ export const test = () => {
 
     return <DFullScreen>
         <DSplitScreen direction={"horizontal"}>
-            <DSplitPane>
+            <DSplitPane miw={"fit-content"}>
                 <DSplitScreen direction={"vertical"}>
                     <DSplitPane>
-                        s
+                        Lorem Ipsum dolar set amit
+                        Lorem Ipsum dolar set amit
                     </DSplitPane>
                     <DSplitPane>
                         s
@@ -39,5 +44,60 @@ export const test = () => {
     </DFullScreen>
 
 }
+export const Dashboard = () => {
+
+    const [controls, openAll, closeAll] = useFolderControls()
+
+    return <DFullScreen>
+        <DSplitScreen>
+            <DSplitPane maw={"fit-content"} p={0.5} justify={"space-between"} style={{writingMode: "vertical-rl", display: "flex"}}>
+                <div style={{display: "flex", gap: ".5rem", overflow: "hidden"}}>
+                    <DScreenButton color={"secondary"}>
+                        <IconHierarchy3 size={12}/>
+                        Flows
+                    </DScreenButton>
+                    <DScreenButton color={"warning"}>
+                        <IconTicket size={12}/>
+                        Issues
+                    </DScreenButton>
+                    <DScreenButton color={"info"}>
+                        <IconDatabase size={12}/>
+                        Database
+                    </DScreenButton>
+                </div>
+                <div style={{display: "flex", gap: ".5rem", overflow: "hidden"}}>
+                    <DScreenButton color={"primary"}>
+                        <IconSettings size={12}/>
+                        Settings
+                    </DScreenButton>
+                </div>
+            </DSplitPane>
+            <DSplitPane maw={"fit-content"}>
+                <div style={{padding: ".5rem"}}>
+                    <DScreenButton onClick={openAll}>
+                        Open All
+                    </DScreenButton>
+                    <DScreenButton onClick={closeAll}>
+                        Close All
+                    </DScreenButton>
+                    {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => {
+                        return <DFolder controls={controls} name={"Google Cloud Flows"} defaultOpen>
+                            <DFolder controls={controls} name={"Google Cloud Flows"}>
+                                <DFolder.Item icon={<IconBrandAdobe size={12}/>}
+                                              name={"Google Cloud Flows"}/>
+                                <DFolder.Item active={index === 1} name={"Google Cloud Flows"}/>
+                            </DFolder>
+                        </DFolder>
+                    })}
+                </div>
+            </DSplitPane>
+            <DSplitPane>
+                <ZoomPanPinchExample/>
+            </DSplitPane>
+        </DSplitScreen>
+    </DFullScreen>
+
+}
+
 
 export default meta

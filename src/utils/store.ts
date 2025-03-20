67b36209df7303176e65816828850909d6eb8e99
nicosammito <nico@sammito.de>
2025-03-20 22:28:29 +0100

    fix: switch to state based store

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -4,7 +4,7 @@ import React from "react";
  * using a React ref as a store in combination with a map
  * to be able to access and modify the store easily
  */
-export type Store<T> = React.MutableRefObject<Map<number, T>>
+export type Store<T> = [T[], React.Dispatch<React.SetStateAction<T[]>>]
 
 export class Service<T> {
 
@@ -14,10 +14,49 @@ export class Service<T> {
         this.store = store
     }
 
+    public delete(index: number) {
+        this.store[1](prevState => {
+            const newState = prevState.filter((value, index1) => index1 !== index);
+            return [
+                ...newState,
+            ]
+        })
+    }
+
+    public add(value: T) {
+        this.store[1](prevState => {
+            return [
+                ...prevState,
+                value
+            ]
+        })
+    }
+
+    public set(index: number, value: T) {
+        this.store[1](prevState => {
+            prevState[index] = value
+            return [
+                ...prevState
+            ]
+        })
+    }
+
+    public has(index: number) {
+        return !!this.store[0][index]
+    }
+
+    public get(index: number) {
+        return this.store[0][index]
+    }
+
+    public values() {
+        return this.store[0]
+    }
+
 }
 
 // @ts-ignore
-export const createStore = <K, T extends Service<K>>(service: typeof T, callback?: (store: Store<K>) => T): T => {
-    const store = React.useRef<Map<number, K>>(new Map())
-    return (callback ? callback(store) : new service(store)) as T
+export const createService = <K, T extends Service<K>>(service: typeof T, callback?: (store: Store<K>) => T, initial?: K[]): [K[] | undefined, T] => {
+    const store = React.useState<K[]>(initial ?? [])
+    return [store[0], (callback ? callback(store) : new service(store)) as T]
 }

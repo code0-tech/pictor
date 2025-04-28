8aec1155fdaa2fd1ece9c181579d05d5f72bc452
nicosammito <nico@sammito.de>
2025-04-28 18:00:10 +0200

    feat: adding reference object and also detailed object defintion

---
+++ src/components/d-flow/data-type/DFlowDataType.view.ts
@@ -0,0 +1,143 @@
+import {Translation} from "../../../utils/translation";
+import {DFlowDataTypeService} from "./DFlowDataType.service";
+
+export interface RawRefObject {
+    type: string
+    primaryLevel: number
+    secondaryLevel: number
+    territoryLevel?: number
+}
+
+export type RawObject = {[key: string]: RawDataTypes}
+
+export type RawDataTypes = number | boolean | string | RawObject | Array<RawDataTypes> | RawRefObject
+
+export enum EDataType {
+    PRIMITIVE, //number, boolean, text
+    TYPE,
+    OBJECT,
+    DATATYPE,
+    ARRAY,
+    GENERIC,
+    NODE,
+    ERROR
+}
+
+export enum EDataTypeRuleType {
+    REGEX,
+    NUMBER_RANGE,
+    ITEM_OF_COLLECTION,
+    CONTAINS_TYPE,
+    CONTAINS_KEY,
+    LOCK_KEY,
+    //etc
+}
+
+export interface DataTypeRuleObject {
+    type: EDataTypeRuleType
+    config: object
+
+}
+
+export interface DataTypeObject {
+    data_type_id: string
+    type: EDataType
+    name?: Translation[]
+    rules?: DataTypeRuleObject[]
+    inputTypes?: string[] // data type id
+    returnType?: string // data type id
+    parent?: string // data type id
+}
+
+/*
+    @todo is DataType castable to another DataType
+ */
+export class DataType {
+
+    private readonly _service: DFlowDataTypeService
+    private readonly _id: string
+    private readonly _type: EDataType
+    private readonly _name?: Translation[]
+    private readonly _rules?: DataTypeRuleObject[]
+    private readonly _inputTypes?: string[] // are only in use if Type is NODE
+    private readonly _returnType?: string // are only in use if Type is NODE
+    private readonly _parent?: string
+
+    constructor(dataType: DataTypeObject, service: DFlowDataTypeService) {
+        this._id = dataType.data_type_id
+        this._type = dataType.type
+        this._name = dataType.name
+        this._rules = dataType.rules
+        this._inputTypes = dataType.inputTypes
+        this._returnType = dataType.returnType
+        this._parent = dataType.parent
+        this._service = service
+    }
+
+    /**
+     * @todo check if EDataType is equal
+     */
+    public validateDataType(dataType: DataType): boolean {
+
+        //all rules need to match
+        const isDeepEqual = (object1: {[index: string]:any}, object2: {[index: string]:any}) => {
+            const objKeys1 = Object.keys(object1)
+            const objKeys2 = Object.keys(object2)
+
+            if (objKeys1.length !== objKeys2.length) return false
+
+            for (const key of objKeys1) {
+                const value1 = object1[key]
+                const value2 = object2[key]
+                const isObjects = isObject(value1) && isObject(value2)
+
+                if ((isObjects && !isDeepEqual(value1, value2)) || (!isObjects && value1 !== value2)) return false
+            }
+            return true
+        }
+
+        const isObject = (object: object) => {
+            return object != null && typeof object === "object";
+        }
+
+        const arraysEqual = (a1: [], a2: []): boolean =>
+            a1.length === a2.length && a1.every((o: any, idx: any) => isDeepEqual(o, a2[idx]))
+
+        //check input types
+        if (this._inputTypes?.length !== dataType._inputTypes?.length) return false
+
+        const notMatchingInputTypes = this._inputTypes?.map((id, index) => {
+            return this._service.getDataType(id)?.validateDataType(this._service.getDataType(dataType._inputTypes!![index] as string) as DataType)
+        }).includes(false)
+        if (notMatchingInputTypes === false) return false
+
+        //check return type
+        if ((this._returnType && !dataType._returnType) || !this._returnType && dataType._returnType)
+            return false
+
+        if ((this._returnType && dataType._returnType) && !(this._service.getDataType(this._returnType as string)?.validateDataType(this._service.getDataType(dataType._returnType as string) as DataType)))
+            return false
+
+        if (this.allRules && !dataType.allRules) return false
+        if (!this.allRules && dataType.allRules) return false
+
+        return arraysEqual(this.allRules as [], dataType.allRules as [])
+    }
+
+    public validateValue(value: string): boolean {
+        return false
+    }
+
+
+    get rules(): DataTypeRuleObject[] | undefined {
+        return this._rules;
+    }
+
+    get allRules(): DataTypeRuleObject[] {
+        return [...(this._rules || []), ...(this._service.getDataType(this._parent as string)?.rules || [])]
+    }
+
+    get id(): string {
+        return this._id;
+    }
+}

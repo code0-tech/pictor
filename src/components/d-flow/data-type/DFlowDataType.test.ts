f05db18e848fbb504feabf168515b4b9caf00f4d
nicosammito <nico@sammito.de>
2025-04-29 19:05:59 +0200

    test: adding tests to check DataTypes against each other

---
+++ src/components/d-flow/data-type/DFlowDataType.test.ts
@@ -0,0 +1,53 @@
+import {describe, expect, test} from '@jest/globals'
+import {DataType, EDataType, EDataTypeRuleType} from "./DFlowDataType.view";
+import {dataTypes} from "./DFlowDataType.data";
+import {
+    createNonReactiveArrayService,
+    NonReactiveArrayService,
+    NonReactiveArrayStore
+} from "../../../utils/nonReactiveArrayStore";
+import {DFlowDataTypeService} from "./DFlowDataType.service";
+
+class NonReactiveDataTypeService extends NonReactiveArrayService<DataType> implements DFlowDataTypeService{
+
+    constructor(store: NonReactiveArrayStore<DataType>) {
+        super(store);
+    }
+
+    public getDataType = (id: string): DataType | undefined => {
+        return this.values().find(value => value.id === id)
+    }
+
+
+}
+
+describe('data type validation against data type', () => {
+    const [_, service] = createNonReactiveArrayService<DataType, NonReactiveDataTypeService>(NonReactiveDataTypeService);
+
+    [...dataTypes, {
+        data_type_id: "NUMBER_2",
+        type: EDataType.PRIMITIVE,
+        rules: [{
+            type: EDataTypeRuleType.REGEX,
+            config: {pattern: "/^(?:-(?:[1-9](?:\d{0,2}(?:,\d{3})+|\d*))|(?:0|(?:[1-9](?:\d{0,2}(?:,\d{3})+|\d*))))(?:.\d+|)$/"}
+        }]
+    }].forEach((dataType) => {
+        service.add(new DataType(dataType, service))
+    })
+
+    test('NUMBER match', () => {
+        expect(service.getDataType("NUMBER")?.validateDataType(service.getDataType("NUMBER") as DataType)).toBe(true)
+    })
+
+    test('HTTP_METHOD match', () => {
+        expect(service.getDataType("HTTP_METHOD")?.validateDataType(service.getDataType("HTTP_METHOD") as DataType)).toBe(true)
+    })
+
+    test('different data type id match', () => {
+        expect(service.getDataType("NUMBER")?.validateDataType(service.getDataType("NUMBER_2") as DataType)).toBe(true)
+    })
+
+    test('NUMBER with TEXT not match', () => {
+        expect(service.getDataType("NUMBER")?.validateDataType(service.getDataType("TEXT") as DataType)).toBe(false)
+    })
+})

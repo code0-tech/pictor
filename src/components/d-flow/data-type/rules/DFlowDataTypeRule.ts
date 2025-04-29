d083800b375be6e487b1d91d363a5de3800ff6fa
nicosammito <nico@sammito.de>
2025-04-29 16:48:29 +0200

    feat: mapping rule type to rule validator

--- src/components/d-flow/data-type/rules/DFlowDataTypeRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeRule.ts
@@ -1,9 +1,24 @@
-import {EDataType, RawDataTypes} from "../DFlowDataType.view";
+import {EDataType, EDataTypeRuleType, RawDataTypes} from "../DFlowDataType.view";
+import {DFlowDataTypeRegexRule} from "./DFlowDataTypeRegexRule";
+import {DFlowDataTypeRangeRule} from "./DFlowDataTypeNumberRangeRule";
+import {DFlowDataTypeItemOfCollectionRule} from "./DFlowDataTypeItemOfCollectionRule";
+import {DFlowDataTypeService} from "../DFlowDataType.service";
+import {DFlowDataTypeContainsTypeRule} from "./DFlowDataTypeContainsTypeRule";
+import {DFlowDataTypeContainsKeyRule} from "./DFlowDataTypeContainsKeyRule";
 
 export interface DFlowDataTypeRule {
-    validate(value: RawDataTypes, config: object): boolean
+    validate(value: RawDataTypes, config: object, service: DFlowDataTypeService): boolean
 }
 
 export const staticImplements = <T>(...types: EDataType[]) => {
     return <U extends T>(constructor: U) => constructor
 }
+
+export const RuleMap = new Map<EDataTypeRuleType, DFlowDataTypeRule>([
+    [EDataTypeRuleType.REGEX, DFlowDataTypeRegexRule],
+    [EDataTypeRuleType.NUMBER_RANGE, DFlowDataTypeRangeRule],
+    [EDataTypeRuleType.ITEM_OF_COLLECTION, DFlowDataTypeItemOfCollectionRule],
+    [EDataTypeRuleType.CONTAINS_TYPE, DFlowDataTypeContainsTypeRule],
+    [EDataTypeRuleType.CONTAINS_KEY, DFlowDataTypeContainsKeyRule]
+
+])

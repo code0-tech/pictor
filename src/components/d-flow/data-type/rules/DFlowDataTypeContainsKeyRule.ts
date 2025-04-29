5c1af9a7bb9cebe8fe40c721c591016a1e15fc94
nicosammito <nico@sammito.de>
2025-04-29 16:47:39 +0200

    feat: adding contains key validator

--- src/components/d-flow/data-type/rules/DFlowDataTypeContainsKeyRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeContainsKeyRule.ts
@@ -1,4 +1,20 @@
+import {DFlowDataTypeRule, staticImplements} from "./DFlowDataTypeRule";
+import {EDataType, RawDataTypes} from "../DFlowDataType.view";
+import {DFlowDataTypeService} from "../DFlowDataType.service";
+
 export interface DFlowDataTypeContainsKeyRuleConfig {
     key: string
     type: string
+}
+
+/**
+ * @todo check if value is of type RawObject
+ */
+@staticImplements<DFlowDataTypeRule>(EDataType.OBJECT)
+export class DFlowDataTypeContainsKeyRule {
+    public static validate(value: RawDataTypes, config: DFlowDataTypeContainsKeyRuleConfig, service: DFlowDataTypeService): boolean {
+        if (!(typeof value === "object")) return false
+        if (!service.getDataType(config.type)) return false
+        return (config.key in value) && (!!service.getDataType(config.type)?.validateValue(value))
+    }
 }

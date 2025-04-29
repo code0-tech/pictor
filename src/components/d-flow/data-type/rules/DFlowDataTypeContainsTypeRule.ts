a8e9af54a150ac9041b0c69f8622dd6433273b8d
nicosammito <nico@sammito.de>
2025-04-29 16:47:47 +0200

    feat: adding contains type validator

--- src/components/d-flow/data-type/rules/DFlowDataTypeContainsTypeRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeContainsTypeRule.ts
@@ -1,3 +1,16 @@
+import {DFlowDataTypeRule, staticImplements} from "./DFlowDataTypeRule";
+import {EDataType, RawDataTypes} from "../DFlowDataType.view";
+import {DFlowDataTypeService} from "../DFlowDataType.service";
+
 export interface DFlowDataTypeContainsTypeRuleConfig {
     type: string
+}
+
+@staticImplements<DFlowDataTypeRule>(EDataType.ARRAY)
+export class DFlowDataTypeContainsTypeRule {
+    public static validate(value: RawDataTypes, config: DFlowDataTypeContainsTypeRuleConfig, service: DFlowDataTypeService): boolean {
+        if (!(Array.isArray(value))) return false
+        if (!service.getDataType(config.type)) return false
+        return value.every(value1 => service.getDataType(config.type)?.validateValue(value))
+    }
 }

f7d056ee15150131ab0f3d6405c180b86e4fde2c
nicosammito <nico@sammito.de>
2025-05-04 15:17:11 +0200

    feat: new FlowType object

---
+++ src/components/d-flow/type/DFlowType.view.ts
@@ -0,0 +1,20 @@
+import {Translation} from "../../../utils/translation";
+import {DataTypeObject} from "../data-type/DFlowDataType.view";
+
+export interface FlowType {
+    flow_type_id: string
+    name: Translation[]
+    settings: FlowTypeSetting[]
+    input_type?: DataTypeObject // data type
+    return_type?: DataTypeObject
+}
+
+export interface FlowTypeSetting {
+    flow_definition_settings_id: string
+    key: string
+    name: Translation[]
+    unique: boolean
+    description: Translation[]
+    type: string // data type id
+    default_value?: object
+}

834f4232565abc9ccce2fc480ef7824de4235cdd
nicosammito <nico@sammito.de>
2025-05-05 15:22:56 +0200

    fix: check if value is of type Object

--- src/components/d-flow/data-type/rules/DFlowDataTypeContainsKeyRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeContainsKeyRule.ts
@@ -1,5 +1,5 @@
 import {DFlowDataTypeRule, staticImplements} from "./DFlowDataTypeRule";
-import {EDataType, Values} from "../DFlowDataType.view";
+import {EDataType, isObject, Values} from "../DFlowDataType.view";
 import {DFlowDataTypeService} from "../DFlowDataType.service";
 
 export interface DFlowDataTypeContainsKeyRuleConfig {
@@ -7,13 +7,11 @@ export interface DFlowDataTypeContainsKeyRuleConfig {
     type: string
 }
 
-/**
- * @todo check if value is of type RawObject
- */
+
 @staticImplements<DFlowDataTypeRule>(EDataType.OBJECT)
 export class DFlowDataTypeContainsKeyRule {
     public static validate(value: Values, config: DFlowDataTypeContainsKeyRuleConfig, service: DFlowDataTypeService): boolean {
-        if (!(typeof value === "object")) return false
+        if (!(isObject(value))) return false
         if (!service.getDataType(config.type)) return false
         return (config.key in value) && (!!service.getDataType(config.type)?.validateValue(value))
     }

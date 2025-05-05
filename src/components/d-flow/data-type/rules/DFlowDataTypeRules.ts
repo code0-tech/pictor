06b4c884b9d2374237c0ca90fc00839ad98b748e
nicosammito <nico@sammito.de>
2025-05-05 16:21:04 +0200

    fix: circular imports

---
+++ src/components/d-flow/data-type/rules/DFlowDataTypeRules.ts
@@ -0,0 +1,25 @@
+import {EDataTypeRuleType} from "../DFlowDataType.view";
+import {DFlowDataTypeRegexRule, DFlowDataTypeRegexRuleConfig} from "./DFlowDataTypeRegexRule";
+import {DFlowDataTypeNumberRangeRuleConfig, DFlowDataTypeRangeRule} from "./DFlowDataTypeNumberRangeRule";
+import {
+    DFlowDataTypeItemOfCollectionRule,
+    DFlowDataTypeItemOfCollectionRuleConfig
+} from "./DFlowDataTypeItemOfCollectionRule";
+import {DFlowDataTypeContainsTypeRule, DFlowDataTypeContainsTypeRuleConfig} from "./DFlowDataTypeContainsTypeRule";
+import {DFlowDataTypeContainsKeyRule, DFlowDataTypeContainsKeyRuleConfig} from "./DFlowDataTypeContainsKeyRule";
+import {DFlowDataTypeRule} from "./DFlowDataTypeRule";
+
+export const RuleMap = new Map<EDataTypeRuleType, DFlowDataTypeRule>([
+    [EDataTypeRuleType.REGEX, DFlowDataTypeRegexRule],
+    [EDataTypeRuleType.NUMBER_RANGE, DFlowDataTypeRangeRule],
+    [EDataTypeRuleType.ITEM_OF_COLLECTION, DFlowDataTypeItemOfCollectionRule],
+    [EDataTypeRuleType.CONTAINS_TYPE, DFlowDataTypeContainsTypeRule],
+    [EDataTypeRuleType.CONTAINS_KEY, DFlowDataTypeContainsKeyRule]
+
+])
+
+export type CombinesRuleConfig = DFlowDataTypeRegexRuleConfig
+    | DFlowDataTypeNumberRangeRuleConfig
+    | DFlowDataTypeItemOfCollectionRuleConfig
+    | DFlowDataTypeContainsTypeRuleConfig
+    | DFlowDataTypeContainsKeyRuleConfig

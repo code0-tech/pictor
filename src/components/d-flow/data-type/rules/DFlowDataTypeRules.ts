060bba4024868fa90929bc6d9944b91291b006f6
nicosammito <nico@sammito.de>
2025-05-05 22:00:12 +0200

    feat: new return type rule

--- src/components/d-flow/data-type/rules/DFlowDataTypeRules.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeRules.ts
@@ -8,13 +8,15 @@ import {
 import {DFlowDataTypeContainsTypeRule, DFlowDataTypeContainsTypeRuleConfig} from "./DFlowDataTypeContainsTypeRule";
 import {DFlowDataTypeContainsKeyRule, DFlowDataTypeContainsKeyRuleConfig} from "./DFlowDataTypeContainsKeyRule";
 import {DFlowDataTypeRule} from "./DFlowDataTypeRule";
+import {DFlowDataTypeReturnTypeRule, DFlowDataTypeReturnTypeRuleConfig} from "./DFlowDataTypeReturnTypeRule";
 
 export const RuleMap = new Map<EDataTypeRuleType, DFlowDataTypeRule>([
     [EDataTypeRuleType.REGEX, DFlowDataTypeRegexRule],
     [EDataTypeRuleType.NUMBER_RANGE, DFlowDataTypeRangeRule],
     [EDataTypeRuleType.ITEM_OF_COLLECTION, DFlowDataTypeItemOfCollectionRule],
     [EDataTypeRuleType.CONTAINS_TYPE, DFlowDataTypeContainsTypeRule],
-    [EDataTypeRuleType.CONTAINS_KEY, DFlowDataTypeContainsKeyRule]
+    [EDataTypeRuleType.CONTAINS_KEY, DFlowDataTypeContainsKeyRule],
+    [EDataTypeRuleType.RETURNS_TYPE, DFlowDataTypeReturnTypeRule]
 
 ])
 
@@ -23,3 +25,4 @@ export type CombinesRuleConfig = DFlowDataTypeRegexRuleConfig
     | DFlowDataTypeItemOfCollectionRuleConfig
     | DFlowDataTypeContainsTypeRuleConfig
     | DFlowDataTypeContainsKeyRuleConfig
+    | DFlowDataTypeReturnTypeRuleConfig

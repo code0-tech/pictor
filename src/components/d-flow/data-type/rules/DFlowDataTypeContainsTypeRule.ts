6e92c9ce4a4e2562a989f249f8cdcd8b20420700
nicosammito <nico@sammito.de>
2025-05-05 21:59:43 +0200

    fix: check every value inside array against data type

--- src/components/d-flow/data-type/rules/DFlowDataTypeContainsTypeRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeContainsTypeRule.ts
@@ -11,6 +11,6 @@ export class DFlowDataTypeContainsTypeRule {
     public static validate(value: Values, config: DFlowDataTypeContainsTypeRuleConfig, service: DFlowDataTypeService): boolean {
         if (!(Array.isArray(value))) return false
         if (!service.getDataType(config.type)) return false
-        return value.every(value1 => service.getDataType(config.type)?.validateValue(value))
+        return value.every(value1 => service.getDataType(config.type)?.validateValue(value1))
     }
 }

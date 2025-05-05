09ab9836a677710bc9cd90e969a4c1fd38e1c638
nicosammito <nico@sammito.de>
2025-05-05 16:21:04 +0200

    fix: circular imports

--- src/components/d-flow/data-type/rules/DFlowDataTypeRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeRule.ts
@@ -1,13 +1,5 @@
-import {EDataType, EDataTypeRuleType, Values} from "../DFlowDataType.view";
-import {DFlowDataTypeRegexRule, DFlowDataTypeRegexRuleConfig} from "./DFlowDataTypeRegexRule";
-import {DFlowDataTypeNumberRangeRuleConfig, DFlowDataTypeRangeRule} from "./DFlowDataTypeNumberRangeRule";
-import {
-    DFlowDataTypeItemOfCollectionRule,
-    DFlowDataTypeItemOfCollectionRuleConfig
-} from "./DFlowDataTypeItemOfCollectionRule";
+import {EDataType, Values} from "../DFlowDataType.view";
 import {DFlowDataTypeService} from "../DFlowDataType.service";
-import {DFlowDataTypeContainsTypeRule, DFlowDataTypeContainsTypeRuleConfig} from "./DFlowDataTypeContainsTypeRule";
-import {DFlowDataTypeContainsKeyRule, DFlowDataTypeContainsKeyRuleConfig} from "./DFlowDataTypeContainsKeyRule";
 
 export interface DFlowDataTypeRule {
     validate(value: Values, config: object, service: DFlowDataTypeService): boolean
@@ -16,18 +8,3 @@ export interface DFlowDataTypeRule {
 export const staticImplements = <T>(...types: EDataType[]) => {
     return <U extends T>(constructor: U) => constructor
 }
-
-export const RuleMap = new Map<EDataTypeRuleType, DFlowDataTypeRule>([
-    [EDataTypeRuleType.REGEX, DFlowDataTypeRegexRule],
-    [EDataTypeRuleType.NUMBER_RANGE, DFlowDataTypeRangeRule],
-    [EDataTypeRuleType.ITEM_OF_COLLECTION, DFlowDataTypeItemOfCollectionRule],
-    [EDataTypeRuleType.CONTAINS_TYPE, DFlowDataTypeContainsTypeRule],
-    [EDataTypeRuleType.CONTAINS_KEY, DFlowDataTypeContainsKeyRule]
-
-])
-
-export type CombinesRuleConfig = DFlowDataTypeRegexRuleConfig
-    | DFlowDataTypeNumberRangeRuleConfig
-    | DFlowDataTypeItemOfCollectionRuleConfig
-    | DFlowDataTypeContainsTypeRuleConfig
-    | DFlowDataTypeContainsKeyRuleConfig

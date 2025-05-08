d5baee519f465f861a5091f873d219f53ce1ea11
nicosammito <nico@sammito.de>
2025-05-08 16:28:39 +0200

    feat: changed name from Values to Value

--- src/components/d-flow/data-type/rules/DFlowDataTypeContainsTypeRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeContainsTypeRule.ts
@@ -8,7 +8,7 @@ export interface DFlowDataTypeContainsTypeRuleConfig {
 
 @staticImplements<DFlowDataTypeRule>(EDataType.ARRAY)
 export class DFlowDataTypeContainsTypeRule {
-    public static validate(value: Values, config: DFlowDataTypeContainsTypeRuleConfig, service: DFlowDataTypeService): boolean {
+    public static validate(value: Value, config: DFlowDataTypeContainsTypeRuleConfig, service: DFlowDataTypeService): boolean {
         if (!(Array.isArray(value))) return false
         if (!service.getDataType(config.type)) return false
         return value.every(value1 => service.getDataType(config.type)?.validateValue(value1))
--- src/components/d-flow/data-type/rules/DFlowDataTypeRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeRule.ts
@@ -1,8 +1,8 @@
-import {EDataType, Values} from "../DFlowDataType.view";
+import {EDataType, Value} from "../DFlowDataType.view";
 import {DFlowDataTypeService} from "../DFlowDataType.service";
 
 export interface DFlowDataTypeRule {
-    validate(value: Values, config: object, service: DFlowDataTypeService): boolean
+    validate(value: Value, config: object, service: DFlowDataTypeService): boolean
 }
 
 export const staticImplements = <T>(...types: EDataType[]) => {

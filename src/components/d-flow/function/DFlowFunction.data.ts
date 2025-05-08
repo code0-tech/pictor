bc1bc21f74feddd5557f044b42608d5fed6f8bbb
nicosammito <nico@sammito.de>
2025-05-08 16:22:37 +0200

    feat: sample data for functions

--- src/components/d-flow/function/DFlowFunction.data.ts
+++ src/components/d-flow/function/DFlowFunction.data.ts
@@ -1,16 +1,38 @@
 import {FunctionDefinition} from "./DFlowFunction.view";
 
-const functionData: FunctionDefinition = {
-    function_id: "123456789",
-    runtime_function: "standard::math::add",
-    return_type: "NUMBER",
+const functionData: FunctionDefinition[] = [{
+    function_id: "",
+    runtime_function: "std::array::concat",
+    return_type: "ARRAY<K>",
     parameters: [{
-        parameter_id: "123456789_1",
-        runtime_parameter_id: "standard::math::add__firstValue",
-        type: "NUMBER"
+        parameter_id: "",
+        runtime_parameter_id: "first",
+        type: "ARRAY<K>",
     }, {
-        parameter_id: "123456789_2",
-        runtime_parameter_id: "standard::math::add__secondValue",
-        type: "NUMBER"
-    }]
-}
+        parameter_id: "",
+        runtime_parameter_id: "second",
+        type: "ARRAY<K>",
+    }],
+    generic_keys: ["K"]
+}, {
+    function_id: "",
+    runtime_function: "std::array::map",
+    return_type: "ARRAY",
+    parameters: [{
+        parameter_id: "",
+        runtime_parameter_id: "array",
+        type: "ARRAY",
+    }, {
+        parameter_id: "test",
+        runtime_parameter_id: "node",
+        type: "MAP_GENERIC_INPUT_NODE",
+    }],
+    generic_keys: ["K", "T"],
+    generic_mapper: [
+        {
+            generic_source: "T",
+            generic_target: "V",
+            parameter_id: "test"
+        }
+    ]
+}]

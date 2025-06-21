2301705c9e38764cabb9d69d2989f033bac82889
nicosammito <nico@sammito.de>
2025-06-21 17:10:52 +0200

    feat: adding ExampleDialog to Dashboard

--- src/components/d-resizable/DResizable.stories.tsx
+++ src/components/d-resizable/DResizable.stories.tsx
@@ -3,12 +3,12 @@ import {DResizableHandle, DResizablePanel, DResizablePanelGroup} from "./DResiza
 import React from "react";
 import DFullScreen from "../d-fullscreen/DFullScreen";
 import {ZoomPanPinchExample} from "../d-zoom-pan-pinch/DZoomPanPinch.stories";
-import DFolder, {useFolderControls} from "../d-folder/DFolder";
+import DFolder from "../d-folder/DFolder";
 import Button from "../button/Button";
 import {IconBrandAdobe, IconDatabase, IconHierarchy3, IconSettings, IconTicket} from "@tabler/icons-react";
 import Flex from "../flex/Flex";
-import Text from "../text/Text";
 import {MenuExample} from "../menu /Menu.stories";
+import {ExampleDialog} from "../dialog/Dialog.stories";
 
 const meta: Meta = {
     title: "Dashboard Resizable",
@@ -22,54 +22,6 @@ const meta: Meta = {
 }
 
 export default meta
-
-export const test = () => {
-
-    const [controls, openAll, closeAll] = useFolderControls()
-
-    return <DFullScreen>
-        <DResizablePanelGroup direction={"horizontal"}>
-            <DResizablePanel w={"fit-content"}>
-                <Flex justify={"space-between"} style={{flexDirection: "column"}} h={"100%"}>
-                    <Flex style={{flexDirection: "column", gap: ".5rem"}} h={"100%"}>
-                        <Button style={{aspectRatio: "50/50", width: "40px"}} variant={"outlined"} color={"secondary"}>
-                            <IconHierarchy3 size={12}/>
-                        </Button>
-                        <Button style={{aspectRatio: "50/50", width: "40px"}} variant={"outlined"} color={"warning"}>
-                            <IconTicket size={12}/>
-                        </Button>
-                        <Button style={{aspectRatio: "50/50", width: "40px"}} variant={"outlined"} color={"info"}>
-                            <IconDatabase size={12}/>
-                        </Button>
-                    </Flex>
-                    <div style={{display: "flex", gap: ".5rem", overflow: "hidden"}}>
-                        <Button style={{aspectRatio: "50/50", width: "40px"}} variant={"outlined"} color={"primary"}>
-                            <IconSettings size={12}/>
-                        </Button>
-                    </div>
-                </Flex>
-            </DResizablePanel>
-            <DResizableHandle/>
-            <DResizablePanel>
-                <div style={{padding: ".5rem"}}>
-                    {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => {
-                        return <DFolder controls={controls} name={"Google Cloud Flows"} defaultOpen>
-                            <DFolder controls={controls} name={"Google Cloud Flows"}>
-                                <DFolder.Item icon={<IconBrandAdobe size={12}/>}
-                                              name={"Google Cloud Flows"}/>
-                                <DFolder.Item active={index === 1} name={"Google Cloud Flows"}/>
-                            </DFolder>
-                        </DFolder>
-                    })}
-                </div>
-            </DResizablePanel>
-            <DResizableHandle/>
-            <DResizablePanel>
-                <ZoomPanPinchExample/>
-            </DResizablePanel>
-        </DResizablePanelGroup>
-    </DFullScreen>
-}
 
 export const Dashboard = () => {
 
@@ -87,7 +39,7 @@ export const Dashboard = () => {
                         <IconDatabase size={12}/>
                     </Button>
                 </Flex>
-                <div style={{display: "flex", gap: ".5rem", overflow: "hidden"}}>
+                <div>
                     <Button style={{aspectRatio: "50/50", width: "40px"}} variant={"outlined"} color={"primary"}>
                         <IconSettings size={12}/>
                     </Button>
@@ -105,10 +57,10 @@ export const Dashboard = () => {
                 })}
             </div>
             <Flex style={{position: "relative", flex: 1, flexDirection: "column", width: "100%", height: "100%", boxSizing: "border-box"}}>
-                <div>
+                <Flex>
                     <MenuExample/>
-                    <br/>
-                </div>
+                    <ExampleDialog/>
+                </Flex>
                 <div style={{position: "relative", overflow: "auto", flex: "1 1 auto", boxSizing: "border-box"}}>
                     <DResizablePanelGroup direction={"horizontal"} autoSaveId={"1"}>
                         <DResizablePanel collapsible collapsedSize={0} minSize={10}>

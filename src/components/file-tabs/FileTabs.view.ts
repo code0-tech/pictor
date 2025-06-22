5f486e4003b993fe28d7e088084c8ef9429662a5
nicosammito <nico@sammito.de>
2025-06-22 04:36:21 +0200

    feat: adding independent id, active and lastActive

--- src/components/file-tabs/FileTabs.view.ts
+++ src/components/file-tabs/FileTabs.view.ts
@@ -1,7 +1,10 @@
 import React from "react";
 
 export interface FileTabsView {
+    id?: string;
     closeable: boolean
     children: React.ReactNode
     content: React.ReactNode
+    active: boolean,
+    lastActive?: Date
 }

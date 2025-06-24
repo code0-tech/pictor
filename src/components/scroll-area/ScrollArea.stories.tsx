50e57f8a89d5e7d8123ce9bb3223ebfb2f6b823d
nicosammito <nico@sammito.de>
2025-06-24 20:23:20 +0200

    feat: new ScrollArea component

---
+++ src/components/scroll-area/ScrollArea.stories.tsx
@@ -0,0 +1,26 @@
+import {Meta} from "@storybook/react";
+import {ScrollArea, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport} from "./ScrollArea";
+import React from "react";
+import Button from "../button/Button";
+import Flex from "../flex/Flex";
+
+export default {
+    title: "Scroll Area",
+} as Meta
+
+export const HorizontalScrollArea = () => {
+    return <ScrollArea w={"200px"} type={"always"}>
+        <ScrollAreaViewport>
+            <Flex>
+                {Array.from({length: 10}, (_, i) => {
+                    return <Button>
+                        Test
+                    </Button>
+                })}
+            </Flex>
+        </ScrollAreaViewport>
+        <ScrollAreaScrollbar orientation={"horizontal"}>
+            <ScrollAreaThumb/>
+        </ScrollAreaScrollbar>
+    </ScrollArea>
+}

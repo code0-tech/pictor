fb664b3682cc56098274dd77181ad83f5481875c
nicosammito <nico@sammito.de>
2025-07-01 17:20:36 +0200

    feat: new Menu component based on radix

--- src/components/menu /Menu.stories.tsx
+++ src/components/menu /Menu.stories.tsx
@@ -1,36 +1,61 @@
 import {Meta} from "@storybook/react";
 import React from "react";
-import Menu, {MenuBody, MenuItem, MenuSeparator, MenuTrigger, MenuTriggerArrow} from "./Menu";
-import Button from "../button/Button";
 import {
-    IconCheck,
-    IconStar,
-    IconUserCircle,
-} from "@tabler/icons-react";
+    Menu,
+    MenuContent, MenuGroup,
+    MenuItem, MenuLabel,
+    MenuPortal,
+    MenuSeparator,
+    MenuSub,
+    MenuSubContent,
+    MenuSubTrigger,
+    MenuTrigger
+} from "./Menu";
+import Button from "../button/Button";
+import {IconChevronRight, IconStar, IconUserCircle} from "@tabler/icons-react";
+import Flex from "../flex/Flex";
 
 const meta: Meta = {
-    title: "Menu",
-    component: Menu
+    title: "Menu"
 }
 
 export default meta
 
-export const MenuExample = () => {
+export const ExampleMenu = () => {
 
-    return <Menu placement={"bottom-start"}>
-        <MenuTrigger>
-            <Button variant="outlined" color="primary">
-                Menu
-                <MenuTriggerArrow/>
-            </Button>
+    return <Menu>
+        <MenuTrigger asChild>
+            <Button>Open Menu</Button>
         </MenuTrigger>
-        <MenuBody gutter={8}>
-            <MenuItem><IconUserCircle size={16}/> New Liquid <br/> Glass Design</MenuItem>
-            <MenuSeparator/>
-            <MenuItem><IconStar size={16}/>UI Designer</MenuItem>
-            <MenuSeparator/>
-            <MenuItem><IconCheck size={16}/>Dribbble</MenuItem>
-        </MenuBody>
+        <MenuPortal >
+            <MenuContent side={"bottom"} align={"start"} sideOffset={5.6}>
+                <MenuLabel>Label</MenuLabel>
+                <MenuGroup>
+                    <MenuItem> New Liquid <br/> Glass Design</MenuItem>
+                    <MenuItem><IconUserCircle size={16}/> New Liquid <br/> Glass Design</MenuItem>
+                    <MenuItem><IconUserCircle size={16}/> New Liquid <br/> Glass Design</MenuItem>
+                </MenuGroup>
+
+                <MenuSeparator/>
+                <MenuItem disabled><IconStar size={16}/>UI Designer</MenuItem>
+                <MenuSeparator/>
+                <MenuSub>
+                    <MenuSubTrigger>
+                        <Flex justify={"space-between"} align={"center"} w={"100%"}>
+                            Dribbble <IconChevronRight size={16}/>
+                        </Flex>
+                    </MenuSubTrigger>
+                    <MenuPortal>
+                        <MenuSubContent>
+                            <MenuItem><IconUserCircle size={16}/> New Liquid <br/> Glass Design</MenuItem>
+                            <MenuSeparator/>
+                            <MenuItem disabled><IconStar size={16}/>UI Designer</MenuItem>
+                        </MenuSubContent>
+                    </MenuPortal>
+
+                </MenuSub>
+            </MenuContent>
+        </MenuPortal>
     </Menu>
 
 }
--- src/components/menu /Menu.tsx
+++ src/components/menu /Menu.tsx
@@ -1,40 +1,81 @@
 import React from "react";
 import "./Menu.style.scss"
 
-import {
-    //props
-    MenuProviderProps as AKMenuProviderProps,
-    MenuButtonProps as AKMenuButtonProps,
-    MenuButtonArrowProps as AKMenuButtonArrowProps,
-    MenuProps as AKMenuProps,
-    MenuItemProps as AKMenuItemProps,
-    MenuSeparatorProps as AKMenuSeparatorProps,
-    //components
-    MenuProvider as AKMenuProvider,
-    MenuButton as AKMenuButton,
-    MenuButtonArrow as AKMenuButtonArrow,
-    Menu as AKMenu,
-    MenuItem as AKMenuItem,
-    MenuSeparator as AKMenuSeparator,
-} from "@ariakit/react"
 import {Code0ComponentProps} from "../../utils/types";
 import {mergeCode0Props} from "../../utils/utils";
+import {
+    DropdownMenu, DropdownMenuArrow,
+    DropdownMenuArrowProps, DropdownMenuContent,
+    DropdownMenuContentProps, DropdownMenuGroup,
+    DropdownMenuGroupProps, DropdownMenuItem,
+    DropdownMenuItemProps, DropdownMenuLabel,
+    DropdownMenuLabelProps, DropdownMenuPortal,
+    DropdownMenuPortalProps,
+    DropdownMenuProps, DropdownMenuSeparator,
+    DropdownMenuSeparatorProps, DropdownMenuSub, DropdownMenuSubContent,
+    DropdownMenuSubContentProps,
+    DropdownMenuSubProps, DropdownMenuSubTrigger,
+    DropdownMenuSubTriggerProps, DropdownMenuTrigger,
+    DropdownMenuTriggerProps
+} from "@radix-ui/react-dropdown-menu";
 
-type MenuProps = AKMenuProviderProps
-type MenuTriggerProps = Code0ComponentProps & AKMenuButtonProps & { children: React.ReactElement }
-type MenuTriggerArrowProps = Code0ComponentProps & AKMenuButtonArrowProps
-type MenuBodyProps = Code0ComponentProps & AKMenuProps
-type MenuItemProps = Code0ComponentProps & AKMenuItemProps
-type MenuSeparatorProps = Code0ComponentProps & AKMenuSeparatorProps
+export type MenuProps = Code0ComponentProps & DropdownMenuProps
+export type MenuTriggerProps = Code0ComponentProps & DropdownMenuTriggerProps
+export type MenuPortalProps = Code0ComponentProps & DropdownMenuPortalProps
+export type MenuContentProps = Code0ComponentProps & DropdownMenuContentProps
+export type MenuLabelProps = Code0ComponentProps & DropdownMenuLabelProps
+export type MenuItemProps = Code0ComponentProps & DropdownMenuItemProps
+export type MenuGroupProps = Code0ComponentProps & DropdownMenuGroupProps
+export type MenuSubProps = Code0ComponentProps & DropdownMenuSubProps
+export type MenuSubTriggerProps = Code0ComponentProps & DropdownMenuSubTriggerProps
+export type MenuSubContentProps = Code0ComponentProps & DropdownMenuSubContentProps
+export type MenuSeparatorProps = Code0ComponentProps & DropdownMenuSeparatorProps
+export type MenuArrowProps = Code0ComponentProps & DropdownMenuArrowProps
 
-const Menu: React.FC<MenuProps> = (props) => <AKMenuProvider {...props} />
-export const MenuTrigger: React.FC<MenuTriggerProps> = (firstProps) => <AKMenuButton
-    render={(props) => {
-        return React.cloneElement(firstProps.children, props);
-    }}/>
-export const MenuTriggerArrow: React.FC<MenuTriggerArrowProps> = (props) => <AKMenuButtonArrow {...props} />
-export const MenuBody: React.FC<MenuBodyProps> = (props) => <AKMenu {...mergeCode0Props("menu", props)}/>
-export const MenuItem: React.FC<MenuItemProps> = (props) => <AKMenuItem {...mergeCode0Props("menu__item", props)}/>
-export const MenuSeparator: React.FC<MenuSeparatorProps> = (props) => <AKMenuSeparator {...mergeCode0Props("menu__separator", props)}/>
+export const Menu: React.FC<MenuProps> = (props) => {
+    return <DropdownMenu {...mergeCode0Props("menu", props) as MenuProps}/>
+}
 
-export default Menu
+export const MenuTrigger: React.FC<MenuTriggerProps> = (props) => {
+    return <DropdownMenuTrigger {...mergeCode0Props("menu__trigger", props) as MenuTriggerProps}/>
+}
+
+export const MenuPortal: React.FC<MenuPortalProps> = (props) => {
+    return <DropdownMenuPortal {...mergeCode0Props("menu__portal", props) as MenuPortalProps}/>
+}
+
+export const MenuContent: React.FC<MenuContentProps> = (props) => {
+    return <DropdownMenuContent align={props.align} {...mergeCode0Props("menu__content", props) as MenuContentProps}/>
+}
+
+export const MenuLabel: React.FC<MenuLabelProps> = (props) => {
+    return <DropdownMenuLabel {...mergeCode0Props("menu__label", props) as MenuLabelProps}/>
+}
+
+export const MenuItem: React.FC<MenuItemProps> = (props) => {
+    return <DropdownMenuItem {...mergeCode0Props("menu__item", props) as MenuItemProps}/>
+}
+
+export const MenuGroup: React.FC<MenuGroupProps> = (props) => {
+    return <DropdownMenuGroup {...mergeCode0Props("menu__group", props) as MenuGroupProps}/>
+}
+
+export const MenuSub: React.FC<MenuSubProps> = (props) => {
+    return <DropdownMenuSub {...mergeCode0Props("menu__sub", props) as MenuSubProps}/>
+}
+
+export const MenuSubTrigger: React.FC<MenuSubTriggerProps> = (props) => {
+    return <DropdownMenuSubTrigger {...mergeCode0Props("menu__sub-trigger", props) as MenuSubTriggerProps}/>
+}
+
+export const MenuSubContent: React.FC<MenuSubContentProps> = (props) => {
+    return <DropdownMenuSubContent {...mergeCode0Props("menu__sub-content", props) as MenuSubContentProps}/>
+}
+
+export const MenuSeparator: React.FC<MenuSeparatorProps> = (props) => {
+    return <DropdownMenuSeparator {...mergeCode0Props("menu__separator", props) as MenuSeparatorProps}/>
+}
+
+export const MenuArrow: React.FC<MenuArrowProps> = (props) => {
+    return <DropdownMenuArrow {...mergeCode0Props("menu__arrow", props) as MenuArrowProps}/>
+}

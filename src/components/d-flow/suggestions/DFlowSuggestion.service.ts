39a14e93d5e8d0492bfba14d1fb0573a1609285c
nicosammito <nico@sammito.de>
2025-07-06 20:56:12 +0200

    feat: add DFlowSuggestionService for managing DFlow suggestions

---
+++ src/components/d-flow/suggestions/DFlowSuggestion.service.ts
@@ -0,0 +1,21 @@
+import {ReactiveArrayService, ReactiveArrayStore} from "../../../utils/reactiveArrayService";
+import {DFlowSuggestion} from "./DFlowSuggestion.view";
+
+export class DFlowSuggestionService extends ReactiveArrayService<DFlowSuggestion> {
+
+    constructor(store: ReactiveArrayStore<DFlowSuggestion>) {
+        super(store);
+    }
+
+    //get all suggestions with matching hash
+    public getSuggestionsByHash(hash: string): DFlowSuggestion[] {
+        return this.values().filter(suggestion => suggestion.hash === hash);
+    }
+
+
+    //add suggestion with hash
+    public addSuggestion(suggestion: DFlowSuggestion): void {
+        this.add(suggestion)
+    }
+
+}

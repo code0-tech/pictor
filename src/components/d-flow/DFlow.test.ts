eefbbd9cf260a61755808016d1b1660c1538f9b6
nicosammito <nico@sammito.de>
2025-07-13 00:42:51 +0200

    feat: add unit tests for DFlowNonReactiveService class

--- src/components/d-flow/DFlow.test.ts
+++ src/components/d-flow/DFlow.test.ts
@@ -1,5 +1,6 @@
 import {Flow} from "./DFlow.view";
 import {NonReactiveArrayService, NonReactiveArrayStore} from "../../utils/nonReactiveArrayService";
+import {describe, test} from "@jest/globals";
 
 export class DFlowNonReactiveService extends NonReactiveArrayService<Flow> {
 
@@ -8,3 +9,11 @@ export class DFlowNonReactiveService extends NonReactiveArrayService<Flow> {
     }
 
 }
+
+describe("DFlowNonReactiveService", () => {
+
+    test("should create an instance of DFlowNonReactiveService", () => {
+
+    })
+
+})

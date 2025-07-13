c178b0f08d3067a6a1b7d369cc6b1912da7333d2
nicosammito <nico@sammito.de>
2025-07-13 20:07:14 +0200

    feat: enhance DFlowSuggestionMenu with number input and improved suggestion display

--- src/components/d-flow/suggestions/DFlowSuggestionMenu.tsx
+++ src/components/d-flow/suggestions/DFlowSuggestionMenu.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import {Menu, MenuContent, MenuItem, MenuPortal, MenuProps, MenuTrigger} from "../../menu/Menu";
-import {DFlowSuggestion} from "./DFlowSuggestion.view";
+import {Menu, MenuContent, MenuItem, MenuLabel, MenuPortal, MenuProps, MenuTrigger} from "../../menu/Menu";
+import {DFlowSuggestion, DFlowSuggestionType} from "./DFlowSuggestion.view";
+import NumberInput from "../../form/NumberInput";
+import {IconAtom, IconCirclesRelation, IconCornerDownLeft, IconFileFunctionFilled} from "@tabler/icons-react";
+import Text from "../../text/Text";
+import {ScrollArea, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport} from "../../scroll-area/ScrollArea";
 
 export interface DFlowSuggestionMenuProps extends MenuProps {
     suggestions?: DFlowSuggestion[]
@@ -11,12 +15,59 @@ export const DFlowSuggestionMenu: React.FC<DFlowSuggestionMenuProps> = (props) =
     const {suggestions, ...rest} = props
 
     return <Menu {...rest}>
-        <MenuTrigger>Test</MenuTrigger>
+        <MenuTrigger asChild>
+            <NumberInput title={"number"}
+                         description={"Increase and decrease your number"}
+                         placeholder={"code0.tech"}/>
+        </MenuTrigger>
         <MenuPortal>
-            <MenuContent>
-                {suggestions?.map(suggestion => {
-                    return <MenuItem>{JSON.stringify(suggestion.value)}</MenuItem>
-                })}
+            <MenuContent sticky={"always"} align={"center"} pos={"relative"} sideOffset={8}>
+                <ScrollArea h={"calc(var(--radix-popper-available-height) - 100px)"}>
+                    <ScrollAreaViewport>
+                        {suggestions?.map(suggestion => {
+                            switch (suggestion.type) {
+                                case DFlowSuggestionType.FUNCTION:
+                                    return <MenuItem>
+                                        <IconFileFunctionFilled color={"#70ffb2"} size={16}/>
+                                        <div>
+                                            <Text display={"flex"}
+                                                  style={{gap: ".5rem"}}>{suggestion.displayText.map(text =>
+                                                <span>{text}</span>)}</Text>
+                                            <Text hierarchy={"tertiary"} size={"xs"}>FUNCTION</Text>
+                                        </div>
+                                    </MenuItem>
+                                case DFlowSuggestionType.FUNCTION_COMBINATION:
+                                    return <MenuItem>{JSON.stringify(suggestion.value)}</MenuItem>
+                                case DFlowSuggestionType.REF_OBJECT:
+                                    return <MenuItem>
+                                        <IconCirclesRelation color={"#FFBE0B"} size={16}/>
+                                        <div>
+                                            <Text display={"flex"}
+                                                  style={{gap: ".5rem"}}>{suggestion.displayText.map(text =>
+                                                <span>{text}</span>)}</Text>
+                                            <Text hierarchy={"tertiary"} size={"xs"}>VARIABLE</Text>
+                                        </div>
+                                    </MenuItem>
+                                case DFlowSuggestionType.VALUE:
+                                    return <MenuItem>
+                                        <IconAtom color={"#D90429"} size={16}/>
+                                        <div>
+                                            <Text display={"flex"}
+                                                  style={{gap: ".5rem"}}>{suggestion.displayText.map(text =>
+                                                <span>{text}</span>)}</Text>
+                                            <Text hierarchy={"tertiary"} size={"xs"}>VALUE</Text>
+                                        </div>
+                                    </MenuItem>
+                                default:
+                                    return <MenuItem>{JSON.stringify(suggestion.value)}</MenuItem>
+                            }
+                        })}
+                    </ScrollAreaViewport>
+                    <ScrollAreaScrollbar orientation={"vertical"}>
+                        <ScrollAreaThumb/>
+                    </ScrollAreaScrollbar>
+                </ScrollArea>
+                <MenuLabel>Press <IconCornerDownLeft size={12}/> to insert</MenuLabel>
             </MenuContent>
         </MenuPortal>
     </Menu>

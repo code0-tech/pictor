cf3e20a331f9eb73a607caab4212319bfa4fffb4
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-07-14 19:50:01 +0200

    feat: init Command

---
+++ src/components/command/Command.stories.tsx
@@ -0,0 +1,18 @@
+import {Meta} from "@storybook/react"
+import React from "react"
+import {Command} from "./Command"
+
+export default {
+    title: "Command",
+} as Meta
+
+
+export const ExampleCommand = () => {
+
+    return (
+        <Command>
+
+        </Command>
+    )
+
+}
---
+++ src/components/command/Command.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import {Command as CommandPrimitive} from "cmdk"
+import {mergeCode0Props} from "../../utils/utils"
+import {Code0ComponentProps} from "../../utils/types"
+import Dialog from "../dialog/Dialog"
+
+export type CommandProps = Code0ComponentProps & React.ComponentProps<typeof CommandPrimitive>
+export type CommandDialogProps = Code0ComponentProps & React.ComponentProps<typeof Dialog>
+export type CommandListProps = Code0ComponentProps & React.ComponentProps<typeof CommandPrimitive.List>
+export type CommandInputProps = Code0ComponentProps & React.ComponentProps<typeof CommandPrimitive.Input>
+export type CommandEmptyProps = Code0ComponentProps & React.ComponentProps<typeof CommandPrimitive.Empty>
+export type CommandGroupProps = Code0ComponentProps & React.ComponentProps<typeof CommandPrimitive.Group>
+export type CommandItemProps = Code0ComponentProps & React.ComponentProps<typeof CommandPrimitive.Item>
+export type CommandSeparatorProps = Code0ComponentProps & React.ComponentProps<typeof CommandPrimitive.Separator>
+export type CommandShortcutProps = Code0ComponentProps & React.ComponentProps<"span">
+
+export const Command: React.FC<CommandProps> = (props) => {
+    return <CommandPrimitive {...mergeCode0Props("command", props) as CommandProps}/>
+}
+
+//export const CommandDialog: React.FC<CommandProps> = (props) => {}
+
+export const CommandList: React.FC<CommandListProps> = (props) => {
+    return <CommandPrimitive.List {...mergeCode0Props("command__list", props) as CommandListProps}/>
+}
+
+export const CommandInput: React.FC<CommandInputProps> = (props) => {
+    return <CommandPrimitive.Input {...mergeCode0Props("command__input", props) as CommandInputProps}/>
+}
+
+export const CommandEmpty: React.FC<CommandEmptyProps> = (props) => {
+    return <CommandPrimitive.Empty {...mergeCode0Props("command__empty", props) as CommandEmptyProps}/>
+}
+
+export const CommandGroup: React.FC<CommandGroupProps> = (props) => {
+    return <CommandPrimitive.Group {...mergeCode0Props("command__group", props) as CommandGroupProps}/>
+}
+
+export const CommandItem: React.FC<CommandItemProps> = (props) => {
+    return <CommandPrimitive.Item {...mergeCode0Props("command__item", props) as CommandItemProps}/>
+}
+
+export const CommandSeparator: React.FC<CommandSeparatorProps> = (props) => {
+    return <CommandPrimitive.Separator {...mergeCode0Props("command__separator", props) as CommandSeparatorProps}/>
+}
+
+//export const CommandShortcut: React.FC<CommandShortcutProps> = (props) => {}
+

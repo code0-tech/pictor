3acf289f1fe11261ca4f3f4fb8b761e856eb1340
nicosammito <nico@sammito.de>
2025-07-16 08:23:11 +0200

    feat: refactor DFlowSuggestionMenu to extract footer and improve suggestion mapping

--- src/components/d-flow/suggestions/DFlowSuggestionMenu.tsx
+++ src/components/d-flow/suggestions/DFlowSuggestionMenu.tsx
@@ -1,130 +1,82 @@
 "use client";
 
 import React from "react";
-import {MenuContent, MenuContentProps, MenuItem, MenuLabel} from "../../menu/Menu";
+import {MenuLabel} from "../../menu/Menu";
 import {DFlowSuggestion, DFlowSuggestionType} from "./DFlowSuggestion.view";
 import {
     IconArrowsShuffle,
     IconBulb,
+    IconCircleDot,
     IconCirclesRelation,
     IconCornerDownLeft,
-    IconFileFunctionFilled, IconCircleDot
+    IconFileFunctionFilled
 } from "@tabler/icons-react";
 import Text from "../../text/Text";
-import {ScrollArea, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport} from "../../scroll-area/ScrollArea";
 import Flex from "../../flex/Flex";
 import {Tooltip, TooltipContent, TooltipPortal, TooltipTrigger} from "../../tooltip/Tooltip";
+import {InputSuggestion} from "../../form/InputSuggestion";
 
-export interface DFlowSuggestionMenuProps extends MenuContentProps {
-    suggestions?: DFlowSuggestion[]
-    onSuggestionSelect?: (suggestion: DFlowSuggestion) => void
+export const DFlowSuggestionMenuFooter: React.FC = () => {
+    return <MenuLabel>
+        <Flex align={"center"} style={{gap: ".35rem"}}>Press <IconCornerDownLeft size={12}/> to insert</Flex>
+        <Flex ml={1} align={"center"} justify={"center"}>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <IconBulb size={12}/>
+                </TooltipTrigger>
+                <TooltipPortal>
+                    <TooltipContent align={"center"} side={"right"} sideOffset={8}>
+                        <Flex pt={0.35} pb={0.35} style={{flexDirection: "column", gap: ".35rem"}}>
+                            <Flex align={"center"} style={{gap: ".35rem"}}>
+                                <IconFileFunctionFilled color={"#70ffb2"} size={16}/>
+                                <Text hierarchy={"tertiary"} size={"xs"}>FUNCTION</Text>
+                            </Flex>
+                            <Flex align={"center"} style={{gap: ".35rem"}}>
+                                <IconArrowsShuffle color={"#29BF12"} size={16}/>
+                                <Text hierarchy={"tertiary"} size={"xs"}>FUNCTION COMBINATION</Text>
+                            </Flex>
+                            <Flex align={"center"} style={{gap: ".35rem"}}>
+                                <IconCirclesRelation color={"#FFBE0B"} size={16}/>
+                                <Text hierarchy={"tertiary"} size={"xs"}>VARIABLE</Text>
+                            </Flex>
+                            <Flex align={"center"} style={{gap: ".35rem"}}>
+                                <IconCircleDot color={"#D90429"} size={16}/>
+                                <Text hierarchy={"tertiary"} size={"xs"}>VALUE</Text>
+                            </Flex>
+                        </Flex>
+
+                    </TooltipContent>
+                </TooltipPortal>
+            </Tooltip>
+        </Flex></MenuLabel>
 }
 
-export interface DFlowSuggestionMenuRef {
-    focusFirstItem: () => void
-    focusLastItem: () => void
+export const toInputSuggestions = (suggestions: DFlowSuggestion[]): InputSuggestion[] => {
+
+    return suggestions.map(suggestion => {
+
+        const iconMap: Record<DFlowSuggestionType, React.ReactNode> = {
+            [DFlowSuggestionType.FUNCTION]: <IconFileFunctionFilled color="#70ffb2" size={16}/>,
+            [DFlowSuggestionType.FUNCTION_COMBINATION]: <IconFileFunctionFilled color="#70ffb2" size={16}/>,
+            [DFlowSuggestionType.REF_OBJECT]: <IconCirclesRelation color="#FFBE0B" size={16}/>,
+            [DFlowSuggestionType.VALUE]: <IconCircleDot color="#D90429" size={16}/>,
+        }
+
+        const children: React.ReactNode = <>
+            {iconMap[suggestion.type]}
+            <div>
+                <Text display="flex" style={{gap: ".5rem"}}>
+                    {suggestion.displayText.map((text, idx) => (
+                        <span key={idx}>{text}</span>
+                    ))}
+                </Text>
+            </div>
+        </>
+
+        return {
+            children,
+            ref: suggestion,
+            value: suggestion.value
+        };
+    })
 }
-
-export const DFlowSuggestionMenu: React.FC<DFlowSuggestionMenuProps> = React.forwardRef<DFlowSuggestionMenuRef, DFlowSuggestionMenuProps>(
-    ({suggestions, onSuggestionSelect = () => {}, ...rest}, ref) => {
-
-        const itemRefs = React.useRef<(HTMLDivElement | null)[]>([])
-        const localRef = React.useRef<HTMLDivElement>(null);
-
-        React.useImperativeHandle(ref, () => {
-            const element = localRef.current!;
-            return Object.assign(element, {
-                focusFirstItem: () => itemRefs.current[0]?.focus(),
-                focusLastItem: () => itemRefs.current.at(-1)?.focus(),
-            });
-        }, []);
-
-        // @ts-ignore
-        return <MenuContent onOpenAutoFocus={(event) => event.preventDefault()} ref={localRef} onInteractOutside={(event) => {
-            if (event.target instanceof HTMLInputElement) event.preventDefault()
-        }} onCloseAutoFocus={(event) => {
-            event.preventDefault()
-        }} align={"start"} sideOffset={8} {...rest} >
-            <ScrollArea h={`${(suggestions?.length ?? 1) * 28}px`} mah={"calc(var(--radix-popper-available-height) - 3rem)"}>
-                <ScrollAreaViewport>
-                    {suggestions?.map((suggestion, i) => {
-                        switch (suggestion.type) {
-                            case DFlowSuggestionType.FUNCTION:
-                                // @ts-ignore
-                                return <MenuItem onSelect={() => onSuggestionSelect(suggestion)} ref={el => itemRefs.current[i] = el}>
-                                    <IconFileFunctionFilled color={"#70ffb2"} size={16}/>
-                                    <div>
-                                        <Text display={"flex"}
-                                              style={{gap: ".5rem"}}>{suggestion.displayText.map(text =>
-                                            <span>{text}</span>)}</Text>
-                                    </div>
-                                </MenuItem>
-                            case DFlowSuggestionType.FUNCTION_COMBINATION:
-                                // @ts-ignore
-                                return <MenuItem onSelect={() => onSuggestionSelect(suggestion)} ref={el => itemRefs.current[i] = el}>
-                                    {JSON.stringify(suggestion.value)}
-                                </MenuItem>
-                            case DFlowSuggestionType.REF_OBJECT:
-                                // @ts-ignore
-                                return <MenuItem onSelect={() => onSuggestionSelect(suggestion)} ref={el => itemRefs.current[i] = el}>
-                                    <IconCirclesRelation color={"#FFBE0B"} size={16}/>
-                                    <div>
-                                        <Text display={"flex"}
-                                              style={{gap: ".5rem"}}>{suggestion.displayText.map(text =>
-                                            <span>{text}</span>)}</Text>
-                                    </div>
-                                </MenuItem>
-                            case DFlowSuggestionType.VALUE:
-                                // @ts-ignore
-                                return <MenuItem onSelect={() => onSuggestionSelect(suggestion)} ref={el => itemRefs.current[i] = el}>
-                                    <IconCircleDot color={"#D90429"} size={16}/>
-                                    <div>
-                                        <Text display={"flex"}
-                                              style={{gap: ".5rem"}}>{suggestion.displayText.map(text =>
-                                            <span>{text}</span>)}</Text>
-                                    </div>
-                                </MenuItem>
-                            default:
-                                return <MenuItem onSelect={() => onSuggestionSelect(suggestion)}>{JSON.stringify(suggestion.value)}</MenuItem>
-                        }
-                    })}
-                </ScrollAreaViewport>
-                <ScrollAreaScrollbar orientation={"vertical"}>
-                    <ScrollAreaThumb/>
-                </ScrollAreaScrollbar>
-            </ScrollArea>
-            <MenuLabel>
-                <Flex align={"center"} style={{gap: ".35rem"}}>Press <IconCornerDownLeft size={12}/> to insert</Flex>
-                <Flex ml={1} align={"center"} justify={"center"}>
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <IconBulb size={12}/>
-                        </TooltipTrigger>
-                        <TooltipPortal>
-                            <TooltipContent align={"center"} side={"right"} sideOffset={8}>
-                                <Flex pt={0.35} pb={0.35} style={{flexDirection: "column", gap: ".35rem"}}>
-                                    <Flex align={"center"} style={{gap: ".35rem"}}>
-                                        <IconFileFunctionFilled color={"#70ffb2"} size={16}/>
-                                        <Text hierarchy={"tertiary"} size={"xs"}>FUNCTION</Text>
-                                    </Flex>
-                                    <Flex align={"center"} style={{gap: ".35rem"}}>
-                                        <IconArrowsShuffle color={"#29BF12"} size={16}/>
-                                        <Text hierarchy={"tertiary"} size={"xs"}>FUNCTION COMBINATION</Text>
-                                    </Flex>
-                                    <Flex align={"center"} style={{gap: ".35rem"}}>
-                                        <IconCirclesRelation color={"#FFBE0B"} size={16}/>
-                                        <Text hierarchy={"tertiary"} size={"xs"}>VARIABLE</Text>
-                                    </Flex>
-                                    <Flex align={"center"} style={{gap: ".35rem"}}>
-                                        <IconCircleDot color={"#D90429"} size={16}/>
-                                        <Text hierarchy={"tertiary"} size={"xs"}>VALUE</Text>
-                                    </Flex>
-                                </Flex>
-
-                            </TooltipContent>
-                        </TooltipPortal>
-                    </Tooltip>
-                </Flex></MenuLabel>
-        </MenuContent>
-    }
-)

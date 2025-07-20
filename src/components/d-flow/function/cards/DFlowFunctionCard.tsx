5ea6e73ad57380a3112b03160b928cc4cfbcc1b0
nicosammito <nico@sammito.de>
2025-07-20 14:42:24 +0200

    feat: enhance DFlowFunctionCard to support dynamic parameter handles

--- src/components/d-flow/function/cards/DFlowFunctionCard.tsx
+++ src/components/d-flow/function/cards/DFlowFunctionCard.tsx
@@ -1,27 +1,41 @@
 import {Code0Component} from "../../../../utils/types";
 import {Handle, Node, NodeProps, Position} from "@xyflow/react";
-import {NodeFunctionObject} from "../../DFlow.view";
+import {NodeFunctionObject, NodeParameterObject} from "../../DFlow.view";
 import React, {memo} from "react";
 import Card from "../../../card/Card";
 
-type CodeZeroComponentProps = Code0Component<HTMLDivElement>
+type CodeZeroComponentProps = Code0Component<HTMLDivElement>;
 
 // @ts-ignore
-export interface DFlowFunctionCardProps extends NodeProps<Node<CodeZeroComponentProps & NodeFunctionObject>> {
-
-}
+export interface DFlowFunctionCardProps extends NodeProps<Node<CodeZeroComponentProps & NodeFunctionObject>> {}
 
 export const DFlowFunctionCard: React.FC<DFlowFunctionCardProps> = memo((props) => {
+    const {data} = props;
+    const functionData = data as NodeFunctionObject & {isParameter: boolean};
 
-    const {data} = props
+    return (
+        <Card w={300} color={"secondary"} style={{ position: "relative" }}>
+            {/* Oben globaler Eingang */}
+            <Handle isConnectable={false} draggable={false} type="target" position={Position.Top} />
 
-    return <Card w={300} color={"secondary"}>
-        <Handle isConnectable={false} draggable={false} type="target" position={Position.Top}/>
-        My favorite UX feedback from customers is:
-        "How is the app so fast?"
-        Because we’ve built on Next.js and Vercel since day one, our pages load in an instant,
-        which is important when it comes to mission-critical software.
-        <Handle isConnectable={false} type="source" position={Position.Bottom}/>
-    </Card>
+            My favorite UX feedback from customers is:
+            "How is the app so fast?" Because we’ve built on Next.js and Vercel since day one,
+            our pages load in an instant, which is important when it comes to mission-critical software.
 
-})
+            {/* Dynamische Parameter-Eingänge (links) */}
+            {functionData.parameters?.map((param: NodeParameterObject, index: number) => (
+                <Handle
+                    key={param.definition.parameter_id}
+                    type="target"
+                    position={Position.Right}
+                    id={`param-${param.definition.parameter_id}`}
+                    style={{ top: 50 + index * 30 }}
+                    isConnectable={false}
+                />
+            ))}
+
+            {/* Ausgang */}
+            <Handle isConnectable={false} type="source" position={functionData.isParameter ? Position.Left : Position.Bottom} />
+        </Card>
+    );
+});

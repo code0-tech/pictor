b2308f37cb42217bd88b8e57ccb8503947d8c2d1
nicosammito <nico@sammito.de>
2025-07-22 15:01:27 +0200

    feat: remove unnecessary edge position calculation and use parameter  flag instead

--- src/components/d-flow/DFlowEdge.tsx
+++ src/components/d-flow/DFlowEdge.tsx
@@ -5,49 +5,23 @@ import React from "react";
 export interface DFlowEdgeDataProps extends Code0Component<HTMLDivElement> {
     //some data we will use
     color?: string
+    isParameter?: boolean
 }
 
 // @ts-ignore
 export type DFlowEdgeProps = EdgeProps<Edge<DFlowEdgeDataProps>>
 
-const calcEdgePositions = (
-    sourceX: number,
-    sourceY: number,
-    targetX: number,
-    targetY: number
-): { sourcePosition: Position; targetPosition: Position } => {
-    const dx = Math.abs(targetX - sourceX)
-    const dy = Math.abs(targetY - sourceY)
-
-    // Horizontaler Abstand dominiert: Edge geht von Left nach Right
-    if (dx > dy) {
-        return {
-            sourcePosition: sourceX < targetX ? Position.Right : Position.Left,
-            targetPosition: sourceX < targetX ? Position.Left : Position.Right,
-        }
-    }
-    // Vertikaler Abstand dominiert: Edge geht von Bottom nach Top (bzw. oben/unten)
-    return {
-        sourcePosition: sourceY < targetY ? Position.Bottom : Position.Top,
-        targetPosition: sourceY < targetY ? Position.Top : Position.Bottom,
-    }
-}
-
 export const DFlowEdge: React.FC<DFlowEdgeProps> = (props) => {
 
     const {sourceX, sourceY, targetX, targetY, id, data, ...rest} = props
 
-    const {sourcePosition, targetPosition} = calcEdgePositions(
-        sourceX, sourceY, targetX, targetY
-    )
-
     const [edgePath] = getSmoothStepPath({
         sourceX,
         sourceY,
-        sourcePosition,
+        sourcePosition: data?.isParameter ? Position.Left : Position.Top,
         targetX,
         targetY,
-        targetPosition,
+        targetPosition: data?.isParameter ? Position.Right : Position.Bottom,
     })
 
     return <BaseEdge id={id} path={edgePath} style={{ stroke: data?.color}}/>

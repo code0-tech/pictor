8df234a398633563a493f30940b4fcf37f4f5b68
nicosammito <nico@sammito.de>
2025-07-30 20:55:31 +0200

    feat: add flow1 configuration with settings and parameters

--- src/components/d-flow/DFlow.data.ts
+++ src/components/d-flow/DFlow.data.ts
@@ -165,3 +165,41 @@ export const flow: FlowObject = {
         }
     }
 }
+
+export const flow1: FlowObject = {
+    flow_id: "some_database_id",
+    name: "test/test/Flow",
+    type: "some_database_id",
+    settings: [{
+        definition: {
+            setting_id: "rest_setting_2",
+            key: "URL"
+        },
+        value: "/test"
+    }, {
+        definition: {
+            setting_id: "rest_setting_1",
+            key: "HTTP_METHOD"
+        },
+        value: "GET"
+    }],
+    starting_node: {
+        function: {
+            function_id: "std::math::add",
+            runtime_function_id: "std::math::add"
+        },
+        parameters: [{
+            definition: {
+                parameter_id: "std::math::add__firstValue",
+                runtime_parameter_id: "std::math::add__firstValue"
+            },
+            value: 5
+        }, {
+            definition: {
+                parameter_id: "standard::math::add__secondValue",
+                runtime_parameter_id: "standard::math::add__secondValue"
+            },
+            value: 10
+        }]
+    }
+}

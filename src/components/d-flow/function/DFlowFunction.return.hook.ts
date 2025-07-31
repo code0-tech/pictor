e1ac004c49ca7c76581bf2a22fa48db85a2da4b9
nicosammito <nico@sammito.de>
2025-07-31 22:21:31 +0200

    feat: update useReturnType to support reactive data type service

--- src/components/d-flow/function/DFlowFunction.return.hook.ts
+++ src/components/d-flow/function/DFlowFunction.return.hook.ts
@@ -1,13 +1,16 @@
 import {FunctionDefinition} from "./DFlowFunction.view";
 import {Type, Value} from "../data-type/DFlowDataType.view";
-import {DFlowDataTypeService} from "../data-type/DFlowDataType.service";
+import {DFlowDataTypeReactiveService, DFlowDataTypeService} from "../data-type/DFlowDataType.service";
 import {replaceGenericKeysInType, resolveGenericKeys} from "../../../utils/generics";
+import {useService} from "../../../utils/contextStore";
 
 export const useReturnType = (
     func: FunctionDefinition,
     values: Value[],
-    dataTypeService: DFlowDataTypeService
+    dataTypeService?: DFlowDataTypeService
 ): Type | null => {
+
+    dataTypeService = dataTypeService ?? useService(DFlowDataTypeReactiveService)
 
     if (!func.return_type) return null
 

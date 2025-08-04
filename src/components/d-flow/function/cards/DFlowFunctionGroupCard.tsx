296387106a48a21709b5f6da78b278f2525bf454
nicosammito <nico@sammito.de>
2025-08-04 19:53:21 +0200

    feat: simplify DFlowFunctionGroupCard by removing unused props and children

--- src/components/d-flow/function/cards/DFlowFunctionGroupCard.tsx
+++ src/components/d-flow/function/cards/DFlowFunctionGroupCard.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 import {Handle, Node, NodeProps, Position} from "@xyflow/react";
 import {FLOW_EDGE_RAINBOW} from "../../DFlow.edges.hook";
-import "./DFlowFunctionGroupCard.style.scss";
 import Card from "../../../card/Card";
 
 export interface DFlowFunctionGroupCardProps extends NodeProps<Node> {}
 
 export const DFlowFunctionGroupCard: React.FC<DFlowFunctionGroupCardProps> = (
-    {data, className, style, children, ...rest}
+    {data, ...rest}
 ) => {
     const depth = (data as any)?.depth ?? 0;
     const color = FLOW_EDGE_RAINBOW[depth % FLOW_EDGE_RAINBOW.length];
     return (
         <Card
             {...rest}
-            variant={"outlined"}
-            className={`function-group ${className ?? ""}`}
-            style={{...(style || {}), border: `2px solid ${color}`, background: "transparent"}}
         >
             <Handle
                 type="source"
@@ -24,7 +20,6 @@ export const DFlowFunctionGroupCard: React.FC<DFlowFunctionGroupCardProps> = (
                 className="function-group__handle"
                 isConnectable={false}
             />
-            {children}
         </Card>
     );
 };

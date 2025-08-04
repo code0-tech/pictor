57278876c44382f6502440014b07ca77df4a38ed
nicosammito <nico@sammito.de>
2025-08-04 17:00:13 +0200

    feat: enhance useFlowNodes hook with additional services for function and data type handling

--- src/components/d-flow/DFlow.nodes.hook.ts
+++ src/components/d-flow/DFlow.nodes.hook.ts
@@ -2,10 +2,15 @@ import {useService} from "../../utils/contextStore";
 import {DFlowReactiveService} from "./DFlow.service";
 import {isNodeFunctionObject, NodeFunction, NodeFunctionObject} from "./DFlow.view";
 import {Node} from "@xyflow/react";
+import {DFlowFunctionReactiveService} from "./function/DFlowFunction.service";
+import {DFlowDataTypeReactiveService} from "./data-type/DFlowDataType.service";
+import {EDataType} from "./data-type/DFlowDataType.view";
 
 export const useFlowNodes = (flowId: string): Node[] => {
-    const flowService = useService(DFlowReactiveService);
-    const flow = flowService.getById(flowId);
+    const flowService = useService(DFlowReactiveService)
+    const functionService = useService(DFlowFunctionReactiveService)
+    const dataTypeService = useService(DFlowDataTypeReactiveService)
+    const flow = flowService.getById(flowId)
 
     if (!flow) return [];
 
@@ -13,7 +18,8 @@ export const useFlowNodes = (flowId: string): Node[] => {
     let idCounter = 0;
 
     const traverse = (fn: NodeFunction, isParameter = false, parentId?: string) => {
-        const id = `${fn.runtime_id}-${idCounter++}`;
+        const id = `${fn.runtime_id}-${idCounter++}`
+        const definition = functionService.getFunctionDefinition(fn.id);
 
         nodes.push({
             id,
@@ -28,6 +34,9 @@ export const useFlowNodes = (flowId: string): Node[] => {
         });
 
         fn.parameters?.forEach((param, i) => {
+            const paramType = definition?.parameters!!.find(p => p.parameter_id == param.id)?.type
+            const paramDataType = dataTypeService.getDataType(paramType!!)
+            if (paramDataType?.type === EDataType.NODE) // dann soll bitte der type der in nodes gepushed wird nicht "default" sein
             if (param.value && isNodeFunctionObject(param.value as NodeFunctionObject)) {
                 const subNode = new NodeFunction(param.value as NodeFunctionObject);
                 traverse(subNode, true, id); // Parameter: isParameter=true, parentId gesetzt

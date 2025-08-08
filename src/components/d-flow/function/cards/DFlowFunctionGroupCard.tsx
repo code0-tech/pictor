9bae97b869683cf35d87fb2916ef24c628e10d03
nicosammito <nico@sammito.de>
2025-08-08 21:57:05 +0200

    feat: wrap nodes in groups for parameters in DFlowFunctionGroupCard

--- src/components/d-flow/function/cards/DFlowFunctionGroupCard.tsx
+++ src/components/d-flow/function/cards/DFlowFunctionGroupCard.tsx
@@ -8,7 +8,6 @@ export interface DFlowFunctionGroupCardProps extends NodeProps<Node> {}
 export const DFlowFunctionGroupCard: React.FC<DFlowFunctionGroupCardProps> = (
     {data, ...rest}
 ) => {
-    //console.log(data, rest)
     const depth = (data as any)?.depth ?? 0;
     const color = FLOW_EDGE_RAINBOW[depth % FLOW_EDGE_RAINBOW.length];
     return (
@@ -16,8 +15,18 @@ export const DFlowFunctionGroupCard: React.FC<DFlowFunctionGroupCardProps> = (
             <Handle
                 type="source"
                 position={Position.Top}
-                className="function-group__handle"
+                className={"function-card__handle function-card__handle--target"}
                 isConnectable={false}
+                draggable={false}
+                style={{top: "1rem"}}
+            />
+            <Handle
+                type="source"
+                position={Position.Bottom}
+                className={"function-card__handle function-card__handle--target"}
+                isConnectable={false}
+                draggable={false}
+                style={{bottom: "1rem"}}
             />
         </Card>
     );

342153e882eb81d454fec6f40f75e92e2198fc1b
nicosammito <nico@sammito.de>
2025-08-11 15:43:13 +0200

    Revert "feat: remove suggestions from this PR"
    This reverts commit df80505876aa4a3e058daa4d43cb906c42c5d337.

---
+++ src/components/d-flow/suggestions/DFlowSuggestionMenu.stories.tsx
@@ -0,0 +1,52 @@
+import {Meta} from "@storybook/react";
+import React from "react";
+import {DataType} from "../data-type/DFlowDataType.view";
+import {dataTypes} from "../data-type/DFlowDataType.data";
+import {functionData} from "../function/DFlowFunction.data";
+import {FunctionDefinition} from "../function/DFlowFunction.view";
+import {Flow} from "../DFlow.view";
+import {flow} from "../DFlow.data";
+import {ContextStoreProvider} from "../../../utils/contextStore";
+import {createReactiveArrayService} from "../../../utils/reactiveArrayService";
+import {DFlowDataTypeReactiveService} from "../data-type/DFlowDataType.service";
+import {DFlowFunctionReactiveService} from "../function/DFlowFunction.service";
+import {DFlowReactiveService} from "../DFlow.service";
+import {DFlowReactiveSuggestionService} from "./DFlowSuggestion.service";
+import {DFlowSuggestion} from "./DFlowSuggestion.view";
+import {useSuggestions} from "./DFlowSuggestion.hook";
+import TextInput from "../../form/TextInput";
+import {DFlowSuggestionMenuFooter, toInputSuggestions} from "./DFlowSuggestionMenu";
+
+export default {
+    title: "DFlowSuggestionMenu",
+} as Meta
+
+export const Example = () => {
+
+    const dataTypesData: DataType[] = dataTypes.map((dt) => new DataType(dt, null as any))
+    const functionsData: FunctionDefinition[] = functionData.map((fd) => new FunctionDefinition(fd))
+
+    const [dataTypeStore, dataTypeService] = createReactiveArrayService<DataType, DFlowDataTypeReactiveService>(DFlowDataTypeReactiveService, undefined, dataTypesData);
+    const [functionStore, functionService] = createReactiveArrayService<FunctionDefinition, DFlowFunctionReactiveService>(DFlowFunctionReactiveService, undefined, functionsData);
+    const [flowStore, flowService] = createReactiveArrayService<Flow, DFlowReactiveService>(DFlowReactiveService, undefined, [new Flow(flow)]);
+    const [suggestionStore, suggestionService] = createReactiveArrayService<DFlowSuggestion, DFlowReactiveSuggestionService>(DFlowReactiveSuggestionService);
+
+    return <ContextStoreProvider
+        services={[[dataTypeStore, dataTypeService], [functionStore, functionService], [flowStore, flowService], [suggestionStore, suggestionService]]}>
+        {React.useMemo(() => <SuggestionMenu/>, [])}
+    </ContextStoreProvider>
+}
+
+const SuggestionMenu = () => {
+
+    const result = useSuggestions("NUMBER", [], "some_database_id", 0, 0)
+
+    return <div>
+        <TextInput title={"Text"}
+                   clearable
+                   suggestionsFooter={<DFlowSuggestionMenuFooter/>}
+                   suggestions={toInputSuggestions(result)}
+                   description={"Type what ever text you like"}
+                   placeholder={"code0.tech"}/>
+    </div>
+}

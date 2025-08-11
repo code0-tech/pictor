1c2549cce6d31f86b75353c434db27ac07489564
nicosammito <nico@sammito.de>
2025-08-11 12:46:49 +0200

    feat: reorganize imports and comment out function test block in DFlowFunction test

--- src/components/d-flow/function/DFlowFunction.test.ts
+++ src/components/d-flow/function/DFlowFunction.test.ts
@@ -1,16 +1,15 @@
 import {describe, expect, test} from '@jest/globals'
 import {
     createNonReactiveArrayService,
-    NonReactiveArrayStore,
-    NonReactiveArrayService
+    NonReactiveArrayService,
+    NonReactiveArrayStore
 } from "../../../utils/nonReactiveArrayService";
 import {FunctionDefinition} from "./DFlowFunction.view";
 import {DFlowFunctionService} from "./DFlowFunction.service";
 import {functionData} from "./DFlowFunction.data";
+import {NonReactiveDataTypeService} from "../data-type/DFlowDataType.test";
 import {DataType} from "../data-type/DFlowDataType.view";
 import {dataTypes} from "../data-type/DFlowDataType.data";
-import {NonReactiveDataTypeService} from "../data-type/DFlowDataType.test";
-import {useFunctionValidation} from "./DFlowFunction.vaildation.hook";
 import {useReturnType} from "./DFlowFunction.return.hook";
 
 export class DFlowFunctionNonReactiveService extends NonReactiveArrayService<FunctionDefinition> implements DFlowFunctionService {
@@ -26,6 +25,7 @@ export class DFlowFunctionNonReactiveService extends NonReactiveArrayService<Fun
 }
 
 
+/*
 describe('function', () => {
 
     const [__, functionService] = createNonReactiveArrayService<FunctionDefinition, DFlowFunctionNonReactiveService>(DFlowFunctionNonReactiveService);
@@ -56,6 +56,8 @@ describe('function', () => {
         expect(useFunctionValidation(functionService.getFunctionDefinition('std::array::add')!!, [[[1], 2, 3], {type: "NUMBER", primaryLevel: 0, secondaryLevel: 1}], dataTypeService)).toMatchObject([{"message": [{"code": "de_DE", "text": "Parameter #1: Ungültiger Wert. Erwartet: {\"type\":\"ARRAY\",\"generic_mapper\":[{\"types\":[\"D\"],\"generic_target\":\"T\"}]}, Erhalten: [[1],2,3]. [Generic Value: Invalid value]"}], "type": 4}])
     })
 })
+
+ */
 
 describe('return type', () => {
 

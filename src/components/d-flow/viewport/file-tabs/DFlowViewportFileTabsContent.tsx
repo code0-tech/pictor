bd1274e95074f61d20c69b8726a629b59d0831c4
nicosammito <nico@sammito.de>
2025-08-16 01:44:21 +0200

    feat: add DFlowViewportFileTabsContent component for dynamic file tab content rendering

---
+++ src/components/d-flow/viewport/file-tabs/DFlowViewportFileTabsContent.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {NodeFunction} from "../../DFlow.view";
+import TextInput from "../../../form/TextInput";
+import Flex from "../../../flex/Flex";
+import {useService} from "../../../../utils/contextStore";
+import {DFlowFunctionReactiveService} from "../../function/DFlowFunction.service";
+import {useSuggestions} from "../../suggestions/DFlowSuggestion.hook";
+import {DFlowSuggestionMenuFooter} from "../../suggestions/DFlowSuggestionMenuFooter";
+import {toInputSuggestions} from "../../suggestions/DFlowSuggestionMenu.util";
+
+export interface DFlowViewportFileTabsContentProps {
+    functionInstance: NodeFunction
+}
+
+export const DFlowViewportFileTabsContent: React.FC<DFlowViewportFileTabsContentProps> = (props) => {
+
+    const {functionInstance} = props
+    const functionService = useService(DFlowFunctionReactiveService)
+    const definition = functionService.getFunctionDefinition(functionInstance.id)
+    return <Flex style={{gap: ".7rem", flexDirection: "column"}}>
+        {functionInstance.parameters!!.map(parameter => {
+
+            const parameterDefinition = definition?.parameters!!.find(parameterDefinition => parameterDefinition.parameter_id === parameter.id)
+            const result = useSuggestions(parameterDefinition!!.type, [], "some_database_id", 0, 0)
+            return <div>
+                <TextInput title={parameterDefinition!!.parameter_id}
+                           description={JSON.stringify(parameterDefinition!!.type)}
+                           clearable
+                           defaultValue={JSON.stringify(parameter.value)}
+                           suggestionsFooter={<DFlowSuggestionMenuFooter/>}
+                           suggestions={toInputSuggestions(result)}
+
+                />
+            </div>
+        })}
+    </Flex>
+
+}

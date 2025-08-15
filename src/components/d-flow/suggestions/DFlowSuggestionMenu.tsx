367140c32e772c189ab9d987904c5741f24a8bf3
nicosammito <nico@sammito.de>
2025-08-15 23:18:59 +0200

    feat: integrate MenuPortal in DFlowSuggestionMenu for improved rendering and state management

--- src/components/d-flow/suggestions/DFlowSuggestionMenu.tsx
+++ src/components/d-flow/suggestions/DFlowSuggestionMenu.tsx
@@ -1,4 +1,4 @@
-import {Menu, MenuSeparator, MenuTrigger} from "../../menu/Menu";
+import {Menu, MenuPortal, MenuSeparator, MenuTrigger} from "../../menu/Menu";
 import React from "react";
 import {DFlowSuggestion} from "./DFlowSuggestion.view";
 import {DFlowSuggestionMenuFooter} from "./DFlowSuggestionMenuFooter";
@@ -9,6 +9,7 @@ import {
 } from "../../form/InputSuggestion";
 import {toInputSuggestions} from "./DFlowSuggestionMenu.util";
 import {DFlowSuggestionMenuSearchBar} from "./DFlowSuggestionMenuSearchBar";
+import {useReactFlow, useStoreApi} from "@xyflow/react";
 
 export interface DFlowSuggestionMenuProps {
     triggerContent: React.ReactNode
@@ -18,51 +19,59 @@ export interface DFlowSuggestionMenuProps {
 
 export const DFlowSuggestionMenu: React.FC<DFlowSuggestionMenuProps> = (props) => {
 
-    const {
-        suggestions = [], triggerContent, onSuggestionSelect = () => {
-        }
-    } = props
+    const {suggestions = [], triggerContent, onSuggestionSelect = () => {}} = props
 
+    const flowStoreApi = useStoreApi()
     const menuRef = React.useRef<InputSuggestionMenuContentItemsHandle | null>(null); // Ref to suggestion list
     const [stateSuggestions, setStateSuggestions] = React.useState(suggestions)
 
-    return <Menu>
+    return <Menu onOpenChange={event => {
+        setTimeout(() => {
+            flowStoreApi.setState({
+                nodesDraggable: !event,
+                nodesConnectable: !event,
+                elementsSelectable: !event,
+            });
+        }, 250) // Timeout to ensure the menu is fully opened before changing the state
+    }}>
         <MenuTrigger asChild>
             {triggerContent}
         </MenuTrigger>
-        <InputSuggestionMenuContent>
-            <DFlowSuggestionMenuSearchBar onType={event => {
+        <MenuPortal>
+            <InputSuggestionMenuContent>
+                <DFlowSuggestionMenuSearchBar onType={event => {
 
-                if (event.key === "ArrowDown") {
-                    event.preventDefault();
-                    menuRef.current?.focusFirstItem(); // Navigate down
-                } else if (event.key === "ArrowUp") {
-                    event.preventDefault();
-                    menuRef.current?.focusLastItem(); // Navigate up
-                }
+                    if (event.key === "ArrowDown") {
+                        event.preventDefault();
+                        menuRef.current?.focusFirstItem(); // Navigate down
+                    } else if (event.key === "ArrowUp") {
+                        event.preventDefault();
+                        menuRef.current?.focusLastItem(); // Navigate up
+                    }
 
-                // @ts-ignore
-                const searchTerm = event.target.value
-                setStateSuggestions(suggestions.filter(suggestion => {
-                    return suggestion.displayText.some(text => {
-                        return text.includes(searchTerm)
-                    })
-                }))
-                event.preventDefault()
-                return false
-            }}/>
-            <MenuSeparator/>
-            <InputSuggestionMenuContentItems
-                /* @ts-ignore */
-                ref={menuRef}
-                suggestions={toInputSuggestions(stateSuggestions)}
-                onSuggestionSelect={(suggestion) => {
-                    onSuggestionSelect(suggestion.ref as DFlowSuggestion)
-                }}
-            />
-            <MenuSeparator/>
-            <DFlowSuggestionMenuFooter/>
-        </InputSuggestionMenuContent>
+                    // @ts-ignore
+                    const searchTerm = event.target.value
+                    setStateSuggestions(suggestions.filter(suggestion => {
+                        return suggestion.displayText.some(text => {
+                            return text.includes(searchTerm)
+                        })
+                    }))
+                    event.preventDefault()
+                    return false
+                }}/>
+                <MenuSeparator/>
+                <InputSuggestionMenuContentItems
+                    /* @ts-ignore */
+                    ref={menuRef}
+                    suggestions={toInputSuggestions(stateSuggestions)}
+                    onSuggestionSelect={(suggestion) => {
+                        onSuggestionSelect(suggestion.ref as DFlowSuggestion)
+                    }}
+                />
+                <MenuSeparator/>
+                <DFlowSuggestionMenuFooter/>
+            </InputSuggestionMenuContent>
+        </MenuPortal>
     </Menu>
 
 }

fff66e3989bc75276ededc34b1ea87b01d03042c
nicosammito <nico@sammito.de>
2025-08-27 18:12:30 +0200

    feat: update DFlowViewportSuggestionCard to enhance suggestions handling with improved parameter usage

--- src/components/d-flow/viewport/cards/DFlowViewportSuggestionCard.tsx
+++ src/components/d-flow/viewport/cards/DFlowViewportSuggestionCard.tsx
@@ -19,12 +19,13 @@ export type DFlowViewportSuggestionCardProps = NodeProps<Node<DFlowViewportSugge
 
 export const DFlowViewportSuggestionCard: React.FC<DFlowViewportSuggestionCardProps> = memo((props) => {
 
-    const result = useSuggestions(undefined, [], props.data.flowId, 0, 0)
+    const result = useSuggestions(undefined, [], props.data.flowId, 0, [0], 0)
     const flowService = useService(DFlowReactiveService)
 
     return <DFlowSuggestionMenu onSuggestionSelect={suggestion => {
         props.data.parentFunction.nextNode = new NodeFunction(suggestion.value as NodeFunctionObject)
         flowService.update()
-    }} suggestions={result} triggerContent={<Button top={-50} variant={"normal"} color={"secondary"}><IconPlus size={16}/> Add new node</Button>}/>
+    }} suggestions={result} triggerContent={<Button top={-50} variant={"normal"} color={"secondary"}><IconPlus
+        size={16}/> Add new node</Button>}/>
 
 })

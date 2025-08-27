e4f11a95a93fb9b965f02fbc43bea44dbcfb978b
nicosammito <nico@sammito.de>
2025-08-27 18:12:25 +0200

    feat: enhance DFlowViewportFileTabsContent to integrate value handling and update flow service on parameter changes

--- src/components/d-flow/viewport/file-tabs/DFlowViewportFileTabsContent.tsx
+++ src/components/d-flow/viewport/file-tabs/DFlowViewportFileTabsContent.tsx
@@ -7,6 +7,8 @@ import {DFlowFunctionReactiveService} from "../../function/DFlowFunction.service
 import {useSuggestions} from "../../suggestions/DFlowSuggestion.hook";
 import {DFlowSuggestionMenuFooter} from "../../suggestions/DFlowSuggestionMenuFooter";
 import {toInputSuggestions} from "../../suggestions/DFlowSuggestionMenu.util";
+import {Value} from "../../data-type/DFlowDataType.view";
+import {DFlowReactiveService} from "../../DFlow.service";
 
 export interface DFlowViewportFileTabsContentProps {
     functionInstance: NodeFunction
@@ -19,17 +21,30 @@ export const DFlowViewportFileTabsContent: React.FC<DFlowViewportFileTabsContent
 
     const {functionInstance, depthLevel, scopeLevel, nodeLevel} = props
     const functionService = useService(DFlowFunctionReactiveService)
+    const flowService = useService(DFlowReactiveService)
     const definition = functionService.getFunctionDefinition(functionInstance.id)
     return <Flex style={{gap: ".7rem", flexDirection: "column"}}>
         {functionInstance.parameters!!.map(parameter => {
 
             const parameterDefinition = definition?.parameters!!.find(parameterDefinition => parameterDefinition.parameter_id === parameter.id)
             const result = useSuggestions(parameterDefinition!!.type, [], "some_database_id", depthLevel, scopeLevel, nodeLevel)
+
+            const enterValue = (value: Value) => {
+                parameter.value = value
+                flowService.update()
+            }
+
             return <div>
                 <TextInput title={parameterDefinition!!.parameter_id}
                            description={JSON.stringify(parameterDefinition!!.type)}
                            clearable
                            defaultValue={JSON.stringify(parameter.value)}
+                           onSuggestionSelect={(suggestion) => {
+                               enterValue(suggestion.value)
+                           }}
+                           onBlur={event => {
+                               enterValue(JSON.parse(event.target.value))
+                           }}
                            suggestionsFooter={<DFlowSuggestionMenuFooter/>}
                            suggestions={toInputSuggestions(result)}
 

a8cc02b9e8253775694ea70cbde4a5fd45e67266
nicosammito <nico@sammito.de>
2025-08-31 15:26:45 +0200

    feat: enhance DFlowViewportTriggerTabContent with flow type settings and suggestions integration

--- src/components/d-flow/viewport/file-tabs/DFlowViewportTriggerTabContent.tsx
+++ src/components/d-flow/viewport/file-tabs/DFlowViewportTriggerTabContent.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import {Flow} from "../../DFlow.view";
+import {Flow, NodeFunction} from "../../DFlow.view";
 import {useService} from "../../../../utils/contextStore";
 import {DFlowReactiveService} from "../../DFlow.service";
 import TextInput from "../../../form/TextInput";
 import Flex from "../../../flex/Flex";
+import {DFlowTypeReactiveService} from "../../type/DFlowType.service";
+import {FlowTypeSetting} from "../../type/DFlowType.view";
+import {DFlowSuggestion} from "../../suggestions/DFlowSuggestion.view";
+import {useSuggestions} from "../../suggestions/DFlowSuggestion.hook";
+import {DFlowSuggestionMenuFooter} from "../../suggestions/DFlowSuggestionMenuFooter";
+import {toInputSuggestions} from "../../suggestions/DFlowSuggestionMenu.util";
 
 export interface DFlowViewportTriggerTabContentProps {
     instance: Flow
@@ -13,11 +19,40 @@ export const DFlowViewportTriggerTabContent: React.FC<DFlowViewportTriggerTabCon
 
     const {instance} = props
     const flowService = useService(DFlowReactiveService)
+    const flowTypeService = useService(DFlowTypeReactiveService)
+    const definition = flowTypeService.getById(instance.type)
+
+    const flowTypeSettingsDefinition = React.useMemo(() => {
+        const map: Record<string, FlowTypeSetting> = {}
+        definition?.settings.forEach(def => {
+            map[def.flow_definition_settings_id] = def
+        })
+        return map
+    }, [definition?.settings])
+
+    const suggestionsById: Record<string, DFlowSuggestion[]> = {}
+    instance?.settings?.forEach(setting => {
+        const settingDefinition = flowTypeSettingsDefinition[setting.definition.setting_id]
+        suggestionsById[setting.definition.setting_id] = useSuggestions(settingDefinition.type, [], "some_database_id", 0, [0], 0)
+    })
 
     return <Flex style={{gap: ".7rem", flexDirection: "column"}}>
         {instance.settings?.map(setting => {
+
+            const settingsDefinition = flowTypeSettingsDefinition[setting.definition.setting_id]
+            const title = settingsDefinition?.name ? settingsDefinition.name[0]?.text : setting.definition.setting_id
+            const description = settingsDefinition?.description ? settingsDefinition?.description[0]?.text : JSON.stringify(settingsDefinition!!.type)
+            const result = suggestionsById[setting.definition.setting_id]
+            const defaultValue = setting.value instanceof NodeFunction ? JSON.stringify(setting.value.json) : typeof setting.value == "object" || typeof setting.value == "boolean" ? JSON.stringify(setting.value) : setting.value
+
             return <div>
-                <TextInput/>
+                <TextInput title={title}
+                           description={description}
+                           clearable
+                           key={JSON.stringify(setting.value)}
+                           defaultValue={defaultValue}
+                           suggestionsFooter={<DFlowSuggestionMenuFooter/>}
+                           suggestions={toInputSuggestions(result)}/>
             </div>
         })}
     </Flex>

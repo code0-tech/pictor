540bb3aad48e7e5b2aa16f8673da451ef9482f16
nicosammito <nico@sammito.de>
2025-10-08 16:25:53 +0200

    feat: remove unused interfaces and types from DFlowDataType.view.ts for improved clarity

--- src/components/d-flow/data-type/DFlowDataType.view.ts
+++ src/components/d-flow/data-type/DFlowDataType.view.ts
@@ -1,121 +1,13 @@
-import {Translation} from "../../../utils/translation";
+
 import {DFlowDataTypeService} from "./DFlowDataType.service";
-import {CombinesRuleConfig, EDataTypeRuleType, RuleMap} from "./rules/DFlowDataTypeRules";
-import {isNodeFunctionObject, NodeFunction, NodeFunctionObject} from "../DFlow.view";
+import {RuleMap} from "./rules/DFlowDataTypeRules";
+import {isNodeFunctionObject, NodeFunctionObject} from "../DFlow.view";
 import {
     DataType,
     DataTypeIdentifier, DataTypeRuleConnection,
     DataTypeVariant, Maybe,
     TranslationConnection
 } from "@code0-tech/sagittarius-graphql-types";
-
-export enum GenericCombinationStrategy {
-    AND,
-    OR
-}
-
-export interface GenericMapper {
-    types: Type[]
-    generic_target: string
-    generic_combination?: GenericCombinationStrategy[]
-}
-
-export interface GenericType {
-    type: string
-    generic_mapper?: GenericMapper[]
-}
-
-export type Type = GenericType | string
-
-export interface RefPath {
-    path?: string
-    index?: number
-}
-
-/**
- * This interface represents a reference value coming from either
- * the return or from the input of a node.
- * Because references, don't hold the actual value we perform just a {@link DataTypeView#validateDataType}
- * check against the {@link RefObject#type}.
- *
- * Every possible reference can be tracked down via it's depth inside the flow.
- *
- * {@link RefObject#depth} links to the context of the node inside the flow
- * starting at 0.
- *
- * {@link RefObject#nodeLevel} links to the node inside the flow
- * starting at 0.
- *
- * {@link RefObject#tertiaryLevel} links to an {@link DataTypeObject#rule#inputtype} of the node
- * starting at 0.
- */
-export interface RefObject {
-    type: Type
-    depth: number
-    scope: number[]
-    node: number
-    path?: RefPath[]
-}
-
-export const isRefObject = (v: any): v is RefObject =>
-    v && typeof v === 'object' &&
-    typeof v.type === 'string' &&
-    typeof v.depth === 'number' &&
-    typeof v.scope === 'object' &&
-    typeof v.node === 'number' &&
-    Object.keys(v).every(k => ['type', 'depth', 'scope', 'node'].includes(k))
-
-/**
- * This type represents a raw object including the rule
- * that every key has to be of type string and every value must be of type
- * {@link Value}.
- */
-export type Object = { [key: string]: Value }
-
-export const isObject = (v: any): v is Object =>
-    v && typeof v === 'object' && !Array.isArray(v) &&
-    Object.keys(v).every(k => typeof k === 'string') &&
-    Object.values(v).every(isValue)
-
-export type Value =
-    number
-    | boolean
-    | string
-    | Object
-    | Array<Value>
-    | RefObject
-    | NodeFunctionObject
-
-export const isValue = (v: any): boolean =>
-    ['string', 'number', 'boolean'].includes(typeof v) ||
-    Array.isArray(v) && v.every(isValue) ||
-    isRefObject(v) ||
-    isObject(v) ||
-    isNodeFunctionObject(v)
-
-export enum EDataType {
-    PRIMITIVE,
-    TYPE,
-    OBJECT,
-    DATATYPE,
-    ARRAY,
-    NODE,
-    ERROR
-}
-
-export interface DataTypeRuleObject {
-    type: EDataTypeRuleType
-    config: CombinesRuleConfig
-}
-
-export interface DataTypeObject {
-    data_type_id: string
-    type: EDataType
-    name?: Translation[]
-    rules?: DataTypeRuleObject[]
-    parent?: Type // data type id
-    genericKeys?: string[]
-}
 
 /*
     @todo is DataType castable to another DataType

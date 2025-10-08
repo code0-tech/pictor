a65ffcb2fa2b0cb63562186125c99a16a31954b7
nicosammito <nico@sammito.de>
2025-10-08 16:25:03 +0200

    feat: update DFlowService to use FlowView for improved type handling

--- src/components/d-flow/DFlow.service.ts
+++ src/components/d-flow/DFlow.service.ts
@@ -1,17 +1,17 @@
-import {Flow} from "./DFlow.view";
+import {FlowView} from "./DFlow.view";
 import {ReactiveArrayService, ReactiveArrayStore} from "../../utils/reactiveArrayService";
 
 export interface DFlowService {
-    getById(id: string): Flow | undefined;
+    getById(id: string): FlowView | undefined;
 }
 
-export class DFlowReactiveService extends ReactiveArrayService<Flow> implements DFlowService {
+export class DFlowReactiveService extends ReactiveArrayService<FlowView> implements DFlowService {
 
-    constructor(store: ReactiveArrayStore<Flow>) {
+    constructor(store: ReactiveArrayStore<FlowView>) {
         super(store);
     }
 
-    getById(id: string): Flow | undefined {
+    getById(id: string): FlowView | undefined {
         return this.values().find(value => value.id === id);
     }
 }

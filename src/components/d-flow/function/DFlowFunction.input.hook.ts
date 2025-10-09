b6ee98b020939f5aa3ddf88b9886b522af13714e
nicosammito <nico@sammito.de>
2025-10-09 12:08:05 +0200

    feat: update useInputType to use FunctionDefinitionView for improved type consistency

--- src/components/d-flow/function/DFlowFunction.input.hook.ts
+++ src/components/d-flow/function/DFlowFunction.input.hook.ts
@@ -1,11 +1,11 @@
-import {FunctionDefinition} from "./DFlowFunction.view";
+import {FunctionDefinitionView} from "./DFlowFunction.view";
 import {Type, Value} from "../data-type/DFlowDataType.view";
 import {DFlowDataTypeService} from "../data-type/DFlowDataType.service";
 import {replaceGenericKeysInType, resolveGenericKeys} from "../../../utils/generics";
 
 export const useInputType = (
     type: Type,
-    func: FunctionDefinition,
+    func: FunctionDefinitionView,
     values: Value[],
     dataTypeService: DFlowDataTypeService
 ): Type | null => {

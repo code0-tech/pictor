a55d4077c5004851074438eaceaec6115a02ae41
nicosammito <nico@sammito.de>
2025-10-09 12:08:54 +0200

    feat: update DFlowFunctionService to use FunctionDefinitionView for improved type consistency

--- src/components/d-flow/function/DFlowFunction.service.ts
+++ src/components/d-flow/function/DFlowFunction.service.ts
@@ -1,17 +1,17 @@
 import {ReactiveArrayStore, ReactiveArrayService} from "../../../utils/reactiveArrayService";
-import {FunctionDefinition} from "./DFlowFunction.view";
+import {FunctionDefinitionView} from "./DFlowFunction.view";
 
 export interface DFlowFunctionService {
-    getFunctionDefinition(id: string): FunctionDefinition | undefined
+    getFunctionDefinition(id: string): FunctionDefinitionView | undefined
 }
 
-export class DFlowFunctionReactiveService extends ReactiveArrayService<FunctionDefinition> implements DFlowFunctionService {
+export class DFlowFunctionReactiveService extends ReactiveArrayService<FunctionDefinitionView> implements DFlowFunctionService {
 
-    constructor(store: ReactiveArrayStore<FunctionDefinition>) {
+    constructor(store: ReactiveArrayStore<FunctionDefinitionView>) {
         super(store);
     }
 
-    public getFunctionDefinition(id: string): FunctionDefinition | undefined {
+    public getFunctionDefinition(id: string): FunctionDefinitionView | undefined {
         return this.values().find(functionDefinition => functionDefinition.function_id === id)
     }
 

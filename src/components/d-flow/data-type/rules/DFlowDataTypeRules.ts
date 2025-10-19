c715a24919d59de2aa56c127d79897e255942a0b
nicosammito <nico@sammito.de>
2025-10-19 17:40:59 +0200

    feat: remove unused EDataTypeRuleType enum to streamline codebase

--- src/components/d-flow/data-type/rules/DFlowDataTypeRules.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeRules.ts
@@ -9,19 +9,6 @@ import {DFlowDataTypeRule} from "./DFlowDataTypeRule";
 import {DFlowDataTypeReturnTypeRule} from "./DFlowDataTypeReturnTypeRule";
 import {DataTypeRulesVariant} from "@code0-tech/sagittarius-graphql-types";
 
-export enum EDataTypeRuleType {
-    REGEX,
-    NUMBER_RANGE,
-    ITEM_OF_COLLECTION,
-    CONTAINS_TYPE,
-    CONTAINS_KEY,
-    LOCK_KEY,
-    RETURNS_TYPE,
-    INPUT_TYPE,
-    PARENT
-    //etc
-}
-
 export const RuleMap = new Map<DataTypeRulesVariant, DFlowDataTypeRule>([
     [DataTypeRulesVariant.Regex, DFlowDataTypeRegexRule],
     [DataTypeRulesVariant.NumberRange, DFlowDataTypeRangeRule],

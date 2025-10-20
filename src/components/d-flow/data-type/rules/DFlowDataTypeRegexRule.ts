6b2628e5f45a793d0cdc2daaf65c0231887a6f2a
nicosammito <nico@sammito.de>
2025-10-20 17:53:43 +0200

    feat: add validation for pattern in DFlowDataTypeRegexRule

--- src/components/d-flow/data-type/rules/DFlowDataTypeRegexRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeRegexRule.ts
@@ -5,6 +5,7 @@ import {DataTypeRulesRegexConfig, NodeParameterValue} from "@code0-tech/sagittar
 export class DFlowDataTypeRegexRule {
     public static validate(value: NodeParameterValue, config: DataTypeRulesRegexConfig): boolean {
         if (value.__typename != 'LiteralValue') return false
+        if (!config.pattern) return false
         return new RegExp(config.pattern).test(String(value))
     }
 }

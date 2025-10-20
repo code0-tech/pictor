0fc5f80771334d49d2fdba2a41fd67c219a8a515
nicosammito <nico@sammito.de>
2025-10-20 15:21:26 +0200

    feat: enhance type handling and null safety in DFlowDataTypeService methods

--- src/components/d-flow/data-type/DFlowDataType.service.ts
+++ src/components/d-flow/data-type/DFlowDataType.service.ts
@@ -3,8 +3,12 @@ import {DataTypeView} from "./DFlowDataType.view";
 import {resolveType} from "../../../utils/generics";
 import {
     DataTypeIdentifier,
-    DataTypeRule, DataTypeRulesVariant, DataTypeVariant, Maybe,
-    NodeParameterValue, Scalars
+    DataTypeRule,
+    DataTypeRulesVariant,
+    DataTypeVariant,
+    Maybe,
+    NodeParameterValue,
+    Scalars
 } from "@code0-tech/sagittarius-graphql-types";
 
 export interface DFlowDataTypeService {
@@ -14,7 +18,7 @@ export interface DFlowDataTypeService {
 
     getDataTypeFromValue(value: NodeParameterValue): DataTypeView | undefined
 
-    getTypeFromValue(value: NodeParameterValue): DataTypeIdentifier
+    getTypeFromValue(value: NodeParameterValue): Maybe<DataTypeIdentifier> | undefined
 }
 
 export class DFlowDataTypeReactiveService extends ReactiveArrayService<DataTypeView> implements DFlowDataTypeService {
@@ -26,7 +30,7 @@ export class DFlowDataTypeReactiveService extends ReactiveArrayService<DataTypeV
     //TODO: remove string because of sagittarius types update
     public getDataType = (type: DataTypeIdentifier): DataTypeView | undefined => {
         if ((type as DataTypeIdentifier).genericKey) return undefined
-        const id = (type as DataTypeIdentifier).dataType?.identifier ?? (type as DataTypeIdentifier).genericType?.dataType.identifier ?? (type as string)
+        const id = (type as DataTypeIdentifier).dataType?.identifier ?? (type as DataTypeIdentifier).genericType?.dataType?.identifier ?? (type as string)
         return this.values().find(value => value.id === id);
     }
 
@@ -34,10 +38,10 @@ export class DFlowDataTypeReactiveService extends ReactiveArrayService<DataTypeV
 
         if (value.__typename == "LiteralValue") {
             //hardcode primitive types (NUMBER, BOOLEAN, TEXT)
-            if (typeof value.value === "string") return this.getDataType("TEXT")
-            if (typeof value.value === "number") return this.getDataType("NUMBER")
-            if (typeof value.value === "boolean") return this.getDataType("BOOLEAN")
-            if (Array.isArray(value.value) && Array.from(value.value).length <= 0) return this.getDataType("ARRAY")
+            if (typeof value.value === "string") return this.getDataType({dataType: {identifier: "TEXT"}})
+            if (typeof value.value === "number") return this.getDataType({dataType: {identifier: "NUMBER"}})
+            if (typeof value.value === "boolean") return this.getDataType({dataType: {identifier: "BOOLEAN"}})
+            if (Array.isArray(value.value) && Array.from(value.value).length <= 0) return this.getDataType({dataType: {identifier: "ARRAY"}})
         }
 
         //TODO: performance here is bad
@@ -52,18 +56,19 @@ export class DFlowDataTypeReactiveService extends ReactiveArrayService<DataTypeV
 
     }
 
-    public getTypeFromValue = (value: NodeParameterValue): DataTypeIdentifier => {
+    public getTypeFromValue = (value: NodeParameterValue): Maybe<DataTypeIdentifier> | undefined => {
 
         if (value.__typename === "ReferenceValue") return value.dataTypeIdentifier
 
         const dataType = this.getDataTypeFromValue(value)
-        if (!dataType?.genericKeys) return dataType?.id ?? ""
+        if (!dataType?.genericKeys) return {dataType: {id: dataType?.id ?? ""}}
 
         //TODO: missing generic combinations
         const genericMapper: any[] = dataType.genericKeys.map(genericKey => {
 
             const ruleThatIncludesGenericKey: Maybe<DataTypeRule> | undefined = dataType.rules?.nodes?.find((rule: DataTypeRule) => {
-                return "dataTypeIdentifier" in rule.config && rule.config.dataTypeIdentifier.genericKey
+                // @ts-ignore
+                return "dataTypeIdentifier" in (rule?.config ?? {}) && rule?.config?.dataTypeIdentifier?.genericKey
             })
 
             if (ruleThatIncludesGenericKey
@@ -109,16 +114,26 @@ export class DFlowDataTypeReactiveService extends ReactiveArrayService<DataTypeV
             return null
         }).filter(mapper => !!mapper)
 
-        return resolveType({
-            id: (dataType?.id ?? "") as Scalars['DataTypeIdentifierID']['output'],
-            generic_mapper: genericMapper
-        }, this)
+        const resolvedType: DataTypeIdentifier = genericMapper ? {
+            genericType: {
+                dataType: {id: dataType.id as Maybe<Scalars['DataTypeID']['output']>},
+                genericMappers: genericMapper
+            }
+        } : {
+            dataType: {
+                id: dataType.id as Maybe<Scalars['DataTypeID']['output']>
+            }
+        }
+
+        return resolveType(resolvedType, this)
 
     }
 
     public hasDataTypes = (types: DataTypeIdentifier[]): boolean => {
         return types.every(type => {
-            return this.values().find(value => value.id === (typeof type === "string" ? type : type.type))
+            return this.values().find(value => {
+                return value.id === (type.genericType?.dataType?.id ?? type.dataType?.id)
+            })
         })
     }
 

9c62ef5b72632f6934eee68f67eb3d4fc2791f8e
nicosammito <nico@sammito.de>
2025-10-21 16:57:41 +0200

    feat: update DFlowType.view.ts to enhance type safety with optional properties and include flowTypeSettings

--- src/components/d-flow/type/DFlowType.view.ts
+++ src/components/d-flow/type/DFlowType.view.ts
@@ -1,66 +1,88 @@
-import {DataType, FlowType, Maybe, Scalars, TranslationConnection} from "@code0-tech/sagittarius-graphql-types";
+import {
+    DataType,
+    FlowType,
+    FlowTypeSetting,
+    Maybe,
+    Scalars,
+    TranslationConnection
+} from "@code0-tech/sagittarius-graphql-types";
 
 
 export class FlowTypeView {
 
-    private readonly _id: Scalars['TypesFlowTypeID']['output']
-    private readonly _identifier: Scalars['String']['output']
-    private readonly _createdAt:  Scalars['Time']['output']
-    private readonly _updatedAt:  Scalars['Time']['output']
-    private readonly _descriptions:  Maybe<TranslationConnection> | undefined
-    private readonly _names:  Maybe<TranslationConnection> | undefined
-    private readonly _editable: Scalars['Boolean']['output']
-    private readonly _inputType: Maybe<DataType> | undefined
-    private readonly _returnType: Maybe<DataType> | undefined
-    //TODO: settings but their is a problem with type in settings
+    /** Time when this FlowType was created */
+    private readonly _createdAt?: Maybe<Scalars['Time']['output']>;
+    /** Descriptions of the flow type */
+    private readonly _descriptions?: Maybe<TranslationConnection>;
+    /** Editable status of the flow type */
+    private readonly _editable?: Maybe<Scalars['Boolean']['output']>;
+    /** Flow type settings of the flow type */
+    private readonly _flowTypeSettings?: Maybe<Array<FlowTypeSetting>>;
+    /** Global ID of this FlowType */
+    private readonly _id?: Maybe<Scalars['TypesFlowTypeID']['output']>;
+    /** Identifier of the flow type */
+    private readonly _identifier?: Maybe<Scalars['String']['output']>;
+    /** Input type of the flow type */
+    private readonly _inputType?: Maybe<DataType>;
+    /** Names of the flow type */
+    private readonly _names?: Maybe<TranslationConnection>;
+    /** Return type of the flow type */
+    private readonly _returnType?: Maybe<DataType>;
+    /** Time when this FlowType was last updated */
+    private readonly _updatedAt?: Maybe<Scalars['Time']['output']>;
 
 
     constructor(flowType: FlowType) {
-        this._id = flowType.id
-        this._identifier = flowType.identifier
-        this._createdAt = flowType.createdAt
-        this._updatedAt = flowType.updatedAt
-        this._descriptions = flowType.descriptions ?? undefined
-        this._names = flowType.names ?? undefined
-        this._editable = flowType.editable
-        this._inputType = flowType.inputType ?? undefined
-        this._returnType = flowType.returnType ?? undefined
+        this._createdAt = flowType.createdAt;
+        this._descriptions = flowType.descriptions;
+        this._editable = flowType.editable;
+        this._flowTypeSettings = flowType.flowTypeSettings;
+        this._id = flowType.id;
+        this._identifier = flowType.identifier;
+        this._inputType = flowType.inputType;
+        this._names = flowType.names;
+        this._returnType = flowType.returnType;
+        this._updatedAt = flowType.updatedAt;
     }
 
 
-    get id(): Scalars["TypesFlowTypeID"]["output"] {
-        return this._id;
-    }
-
-    get identifier(): Scalars["String"]["output"] {
-        return this._identifier;
-    }
-
-    get createdAt(): Scalars["Time"]["output"] {
+    get createdAt(): Maybe<Scalars["Time"]["output"]> | undefined {
         return this._createdAt;
-    }
-
-    get updatedAt(): Scalars["Time"]["output"] {
-        return this._updatedAt;
     }
 
     get descriptions(): Maybe<TranslationConnection> | undefined {
         return this._descriptions;
     }
 
-    get names(): Maybe<TranslationConnection> | undefined {
-        return this._names;
+    get editable(): Maybe<Scalars["Boolean"]["output"]> | undefined {
+        return this._editable;
     }
 
-    get editable(): Scalars["Boolean"]["output"] {
-        return this._editable;
+    get flowTypeSettings(): Maybe<Array<FlowTypeSetting>> | undefined {
+        return this._flowTypeSettings;
+    }
+
+    get id(): Maybe<Scalars["TypesFlowTypeID"]["output"]> | undefined {
+        return this._id;
+    }
+
+    get identifier(): Maybe<Scalars["String"]["output"]> | undefined {
+        return this._identifier;
     }
 
     get inputType(): Maybe<DataType> | undefined {
         return this._inputType;
     }
 
+    get names(): Maybe<TranslationConnection> | undefined {
+        return this._names;
+    }
+
     get returnType(): Maybe<DataType> | undefined {
         return this._returnType;
+    }
+
+    get updatedAt(): Maybe<Scalars["Time"]["output"]> | undefined {
+        return this._updatedAt;
     }
 }

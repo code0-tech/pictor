97d9085b9d5c4878b25841436bdeca35d0d19ac9
nicosammito <nico@sammito.de>
2025-10-23 15:31:07 +0200

    feat: refactor DNamespaceProject service for improved structure and abstraction

--- src/components/d-namespace/project/DNamespaceProject.service.ts
+++ src/components/d-namespace/project/DNamespaceProject.service.ts
@@ -1,29 +1,37 @@
 import {
     NamespacesProjectsAssignRuntimesInput,
-    NamespacesProjectsCreateInput, NamespacesProjectsDeleteInput, Scalars
+    NamespacesProjectsCreateInput,
+    NamespacesProjectsDeleteInput,
+    Scalars
 } from "@code0-tech/sagittarius-graphql-types";
 import {DNamespaceProjectView} from "./DNamespaceProject.view";
 import {ReactiveArrayService, ReactiveArrayStore} from "../../../utils/reactiveArrayService";
 
-export interface DNamespaceProjectService {
-    projectAssignRuntimes(payload: NamespacesProjectsAssignRuntimesInput): DNamespaceProjectView | undefined
-    projectsCreate(payload: NamespacesProjectsCreateInput): DNamespaceProjectView | undefined
-    projectsDelete(payload: NamespacesProjectsDeleteInput): void
-    findById(id: Scalars['NamespaceProjectID']['output']): DNamespaceProjectView | undefined
-}
-
-export abstract class DNamespaceProjectReactiveService extends ReactiveArrayService<DNamespaceProjectView> implements DNamespaceProjectService{
+export abstract class DNamespaceProjectService extends ReactiveArrayService<DNamespaceProjectView> {
 
     constructor(payload: ReactiveArrayStore<DNamespaceProjectView>) {
         super(payload);
     }
+
+    abstract projectAssignRuntimes(payload: NamespacesProjectsAssignRuntimesInput): DNamespaceProjectView | undefined
+
+    abstract projectsCreate(payload: NamespacesProjectsCreateInput): DNamespaceProjectView | undefined
+
+    abstract projectsDelete(payload: NamespacesProjectsDeleteInput): void
+
+    abstract findById(id: Scalars['NamespaceProjectID']['output']): DNamespaceProjectView | undefined
+}
+
+export abstract class DNamespaceProjectReactiveService extends DNamespaceProjectService {
 
     findById(id: Scalars["NamespaceProjectID"]["output"]): DNamespaceProjectView | undefined {
         return this.values().find(project => project.id === id)
     }
 
     abstract projectAssignRuntimes(payload: NamespacesProjectsAssignRuntimesInput): DNamespaceProjectView | undefined
+
     abstract projectsCreate(payload: NamespacesProjectsCreateInput): DNamespaceProjectView | undefined
+
     abstract projectsDelete(payload: NamespacesProjectsDeleteInput): void
 
 }

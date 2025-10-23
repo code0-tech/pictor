35904a2ce72a39bc783fb66e7f44e6170e74dd06
nicosammito <nico@sammito.de>
2025-10-23 12:49:12 +0200

    feat: extend validate method to include FlowView for enhanced data validation

--- src/components/d-flow/data-type/rules/DFlowDataTypeRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeRule.ts
@@ -1,8 +1,9 @@
 import {DFlowDataTypeService} from "../DFlowDataType.service";
 import {GenericMapper, NodeParameterValue} from "@code0-tech/sagittarius-graphql-types";
+import {FlowView} from "../../DFlow.view";
 
 export interface DFlowDataTypeRule {
-    validate(value: NodeParameterValue, config: object, generics?: Map<string, GenericMapper>, service?: DFlowDataTypeService): boolean
+    validate(value: NodeParameterValue, config: object, generics?: Map<string, GenericMapper>, service?: DFlowDataTypeService, flow?: FlowView): boolean
 }
 
 export const staticImplements = <T>() => {

1da6d6c389750f1647edd2aa817ca7137419faa3
nicosammito <nico@sammito.de>
2025-10-24 18:15:52 +0200

    feat: refactor flow setting handling in DFlowViewportTriggerTabContent for improved clarity and consistency

--- src/components/d-flow/viewport/file-tabs/DFlowViewportTriggerTabContent.tsx
+++ src/components/d-flow/viewport/file-tabs/DFlowViewportTriggerTabContent.tsx
@@ -31,28 +31,27 @@ export const DFlowViewportTriggerTabContent: React.FC<DFlowViewportTriggerTabCon
     }, [definition?.flowTypeSettings])
 
     const suggestionsById: Record<string, DFlowSuggestion[]> = {}
-    instance?.settings?.forEach(setting => {
-        const settingDefinition = flowTypeSettingsDefinition[setting.flowSettingId!!]
-        suggestionsById[setting.flowSettingId!!] = useSuggestions({dataType: settingDefinition.dataType}, [], "some_database_id", 0, [0], 0)
+    definition?.flowTypeSettings?.forEach(settingDefinition => {
+        suggestionsById[settingDefinition.identifier!!] = useSuggestions({dataType: settingDefinition.dataType}, [], "some_database_id", 0, [0], 0)
     })
 
     return <Flex style={{gap: ".7rem", flexDirection: "column"}}>
-        {instance.settings?.map(setting => {
-
-            const settingsDefinition = flowTypeSettingsDefinition[setting.flowSettingId!!]
-            const title = settingsDefinition?.names ? settingsDefinition.names?.nodes!![0]?.content : setting.flowSettingId
-            const description = settingsDefinition?.descriptions ? settingsDefinition?.descriptions?.nodes!![0]?.content : JSON.stringify(settingsDefinition!!.dataType)
-            const result = suggestionsById[setting.flowSettingId!!]
-            const defaultValue = typeof setting.value == "object" ? JSON.stringify(setting.value) : setting.value
+        {definition?.flowTypeSettings?.map(settingDefinition => {
+            const setting = instance.settings?.find(s => s.flowSettingIdentifier == settingDefinition.identifier)
+            const title = settingDefinition.names?.nodes!![0]?.content ?? ""
+            const description = settingDefinition?.descriptions?.nodes!![0]?.content ?? ""
+            const result = suggestionsById[settingDefinition.identifier!!]
+            const defaultValue = typeof setting?.value == "object" ? JSON.stringify(setting?.value) : setting?.value
 
             return <div>
                 <TextInput title={title}
                            description={description}
                            clearable
-                           key={JSON.stringify(setting.value)}
+                           key={JSON.stringify(setting?.value)}
                            defaultValue={defaultValue}
                            suggestionsFooter={<DFlowSuggestionMenuFooter/>}
-                           suggestions={toInputSuggestions(result)}/>
+                           suggestions={toInputSuggestions(result)}
+                           />
             </div>
         })}
     </Flex>

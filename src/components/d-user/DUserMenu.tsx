fff6cda26b38d5c8b68f7514cf72916346afed01
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-10-24 21:13:43 +0200

    feat: menu implementation for project/organization/user

--- src/components/d-user/DUserMenu.tsx
+++ src/components/d-user/DUserMenu.tsx
@@ -2,18 +2,36 @@
 
 import {Code0Component} from "../../utils/types"
 import React from "react"
-import {Menu} from "../menu/Menu"
+import {Menu, MenuContent, MenuPortal, MenuTrigger} from "../menu/Menu"
+import {DUserReactiveService} from "./DUser.service"
+import {useService} from "../../utils/contextStore"
+import {Scalars} from "@code0-tech/sagittarius-graphql-types"
+import Avatar from "../avatar/Avatar"
 
 export interface DUserMenuProps extends Code0Component<HTMLDivElement> {
+    userId: Scalars['UserID']['output']
 }
 
 const DUserMenu: React.FC<DUserMenuProps> = props => {
+    const userService = useService(DUserReactiveService)
+    const userStore = useService(DUserReactiveService)
+
+    const currentUser = userService.getUserSession()?.user
 
     return React.useMemo(() => {
         return (
             <Menu>
-
+                <MenuTrigger asChild>
+                    <Avatar src={currentUser?.avatarPath ?? ""}/>
+                </MenuTrigger>
+                <MenuPortal>
+                    <MenuContent side={"bottom"} align={"end"} sideOffset={4}>
+                        {props.children}
+                    </MenuContent>
+                </MenuPortal>
             </Menu>
         )
-    }, [])
+    }, [userStore])
 }
+
+export default DUserMenu

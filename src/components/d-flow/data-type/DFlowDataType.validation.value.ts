e6df001d943d4515ad456c0c5425b4c886febe33
nicosammito <nico@sammito.de>
2025-10-26 15:40:58 +0100

    feat: simplify value validation logic in DFlowDataType.validation.value.ts

--- src/components/d-flow/data-type/DFlowDataType.validation.value.ts
+++ src/components/d-flow/data-type/DFlowDataType.validation.value.ts
@@ -1,9 +1,8 @@
-import {GenericMapper, NodeParameterValue, Scalars} from "@code0-tech/sagittarius-graphql-types";
+import {GenericMapper, NodeParameterValue} from "@code0-tech/sagittarius-graphql-types";
 import {RuleMap} from "./rules/DFlowDataTypeRules";
 import {DataTypeView} from "./DFlowDataType.view";
 import {useService} from "../../../utils/contextStore";
 import {DFlowDataTypeReactiveService} from "./DFlowDataType.service";
-import {DFlowReactiveService} from "../DFlow.service";
 import {FlowView} from "../DFlow.view";
 
 export const useValidateValue = (
@@ -14,12 +13,11 @@ export const useValidateValue = (
 ): boolean => {
 
     const dataTypeService = useService(DFlowDataTypeReactiveService)
-    const flowService = useService(DFlowReactiveService)
-
     const map = new Map<string, GenericMapper>(generics?.map(generic => [generic.target!!, generic]))
 
     return dataType.rules?.nodes?.every(rule => {
         if (!rule || !rule.variant || !rule.config) return false
+        if (!RuleMap.get(rule.variant)) return true
         return RuleMap.get(rule.variant)?.validate(value, rule.config, map, dataTypeService, flow)
-    }) ?? false
+    }) ?? true
 }

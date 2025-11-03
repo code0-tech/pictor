36bee0fc75e3f50b739c935a2eac03dfd34a57b4
nicosammito <nico@sammito.de>
2025-11-03 16:16:55 +0100

    feat: add DLayout component with responsive layout structure and styling

---
+++ src/components/d-layout/DLayout.stories.tsx
@@ -0,0 +1,30 @@
+import {Meta} from "@storybook/react";
+import {DLayout} from "./DLayout";
+import React from "react";
+import Text from "../text/Text";
+import DFullScreen from "../d-fullscreen/DFullScreen";
+
+const meta: Meta = {
+    title: "Dashboard Layout",
+    component: DLayout,
+    parameters: {
+        visualTest: {
+            selector: 'body'
+        },
+        layout: "fullscreen"
+    },
+}
+
+export default meta
+
+
+export const DashboardLayoutExample = () => {
+    return <DFullScreen>
+        <DLayout leftContent={<Text>Left</Text>}
+                 rightContent={<Text>Right</Text>}
+                 topContent={<Text>Top</Text>}
+                 bottomContent={<Text>Bottom</Text>}>
+            <Text>Text</Text>
+        </DLayout>
+    </DFullScreen>
+}

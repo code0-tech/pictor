7380d16935cd9dfa4f05c452f57a70449a7755a7
nicosammito <nico@sammito.de>
2025-11-05 14:24:13 +0100

    feat: refactor DNamespaceRole service to implement getById method and update role methods to return promises

--- src/components/d-namespace/role/DNamespaceRole.service.ts
+++ src/components/d-namespace/role/DNamespaceRole.service.ts
@@ -1,51 +1,31 @@
 import {ReactiveArrayService, ReactiveArrayStore} from "../../../utils/reactiveArrayService";
 import {DNamespaceRoleView} from "./DNamespaceRole.view";
 import {
-    NamespacesRolesAssignAbilitiesInput,
-    NamespacesRolesAssignProjectsInput,
-    NamespacesRolesCreateInput,
-    NamespacesRolesDeleteInput,
-    NamespacesRolesUpdateInput,
+    NamespaceRole,
+    NamespacesRolesAssignAbilitiesInput, NamespacesRolesAssignAbilitiesPayload,
+    NamespacesRolesAssignProjectsInput, NamespacesRolesAssignProjectsPayload,
+    NamespacesRolesCreateInput, NamespacesRolesCreatePayload,
+    NamespacesRolesDeleteInput, NamespacesRolesDeletePayload,
+    NamespacesRolesUpdateInput, NamespacesRolesUpdatePayload,
     Scalars
 } from "@code0-tech/sagittarius-graphql-types";
 
-export abstract class DNamespaceRoleService extends ReactiveArrayService<DNamespaceRoleView> {
+export abstract class DNamespaceRoleReactiveService extends ReactiveArrayService<DNamespaceRoleView> {
 
     //TODO: inject UI error handler for toasts
     //inject: namespaceId because the runtimes query needs it
 
-    constructor(payload: ReactiveArrayStore<DNamespaceRoleView>) {
-        super(payload);
-
-    }
-
-    abstract findById(id: Scalars['NamespaceRoleID']['output']): DNamespaceRoleView | undefined
-
-    abstract roleAssignAbilities(payload: NamespacesRolesAssignAbilitiesInput): DNamespaceRoleView | undefined
-
-    abstract roleAssignProjects(payload: NamespacesRolesAssignProjectsInput): DNamespaceRoleView | undefined
-
-    abstract roleCreate(payload: NamespacesRolesCreateInput): DNamespaceRoleView | undefined
-
-    abstract roleDelete(payload: NamespacesRolesDeleteInput): DNamespaceRoleView | undefined
-
-    abstract roleUpdate(payload: NamespacesRolesUpdateInput): DNamespaceRoleView | undefined
-}
-
-export abstract class DNamespaceRoleReactiveService extends DNamespaceRoleService {
-
-    findById(id: Scalars["NamespaceRoleID"]["output"]): DNamespaceRoleView | undefined {
+    getById(id: NamespaceRole['id']): DNamespaceRoleView | undefined {
         return this.values().find(role => role.id === id);
     }
 
-    abstract override roleAssignAbilities(payload: NamespacesRolesAssignAbilitiesInput): DNamespaceRoleView | undefined
+    abstract roleAssignAbilities(payload: NamespacesRolesAssignAbilitiesInput): Promise<NamespacesRolesAssignAbilitiesPayload | undefined>
 
-    abstract override roleAssignProjects(payload: NamespacesRolesAssignProjectsInput): DNamespaceRoleView | undefined
+    abstract roleAssignProjects(payload: NamespacesRolesAssignProjectsInput): Promise<NamespacesRolesAssignProjectsPayload | undefined>
 
-    abstract override roleCreate(payload: NamespacesRolesCreateInput): DNamespaceRoleView | undefined
+    abstract roleCreate(payload: NamespacesRolesCreateInput): Promise<NamespacesRolesCreatePayload | undefined>
 
-    abstract override roleDelete(payload: NamespacesRolesDeleteInput): DNamespaceRoleView | undefined
+    abstract roleDelete(payload: NamespacesRolesDeleteInput): Promise<NamespacesRolesDeletePayload | undefined>
 
-    abstract override roleUpdate(payload: NamespacesRolesUpdateInput): DNamespaceRoleView | undefined
-
+    abstract roleUpdate(payload: NamespacesRolesUpdateInput): Promise<NamespacesRolesUpdatePayload | undefined>
 }

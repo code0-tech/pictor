9c1d4e58b6e60fb9a999c9404957c452aa360a23
nicosammito <nico@sammito.de>
2025-11-11 16:26:52 +0100

    feat: update useUserSession hook to handle undefined session state

--- src/components/d-user/DUser.session.hook.tsx
+++ src/components/d-user/DUser.session.hook.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import type {UserSession} from "@code0-tech/sagittarius-graphql-types";
 
 export const useUserSession = () => {
-    const [session, setSession] = React.useState<UserSession | null>(null)
+    const [session, setSession] = React.useState<UserSession | undefined | null>(undefined)
 
     React.useEffect(() => {
         const userSession = JSON.parse(localStorage.getItem("ide_code-zero_session")!!) as UserSession
         if (userSession && userSession.token) setSession(userSession)
+        else setSession(null)
     }, [])
 
     return session

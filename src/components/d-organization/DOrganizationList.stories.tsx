059059fa390aa1f043b6979d60a674af82783256
nicosammito <nico@sammito.de>
2025-11-15 21:13:30 +0100

    feat: add DOrganizationList story with mock services for testing

---
+++ src/components/d-organization/DOrganizationList.stories.tsx
@@ -0,0 +1,253 @@
+import {Meta, StoryObj} from "@storybook/react"
+import React from "react"
+import DOrganizationCard from "./DOrganizationCard"
+import {ContextStoreProvider} from "../../utils"
+import {useReactiveArrayService} from "../../utils"
+import {DOrganizationView} from "./DOrganization.view"
+import {DOrganizationReactiveService} from "./DOrganization.service"
+import {
+    NamespacesLicensesCreateInput,
+    NamespacesLicensesCreatePayload,
+    NamespacesLicensesDeleteInput,
+    NamespacesLicensesDeletePayload,
+    NamespacesProjectsAssignRuntimesInput,
+    NamespacesProjectsCreateInput,
+    NamespacesProjectsDeleteInput,
+    OrganizationsCreateInput,
+    OrganizationsCreatePayload,
+    OrganizationsDeleteInput,
+    OrganizationsDeletePayload,
+    OrganizationsUpdateInput,
+    OrganizationsUpdatePayload,
+    UsersEmailVerificationInput,
+    UsersEmailVerificationPayload,
+    UsersIdentityLinkInput,
+    UsersIdentityLinkPayload,
+    UsersIdentityLoginInput,
+    UsersIdentityLoginPayload,
+    UsersIdentityRegisterInput,
+    UsersIdentityRegisterPayload,
+    UsersIdentityUnlinkInput,
+    UsersIdentityUnlinkPayload,
+    UsersLoginInput,
+    UsersLoginPayload,
+    UsersLogoutInput,
+    UsersLogoutPayload,
+    UsersMfaBackupCodesRotateInput,
+    UsersMfaBackupCodesRotatePayload,
+    UsersMfaTotpGenerateSecretInput,
+    UsersMfaTotpGenerateSecretPayload,
+    UsersMfaTotpValidateSecretInput,
+    UsersMfaTotpValidateSecretPayload,
+    UsersPasswordResetInput,
+    UsersPasswordResetPayload,
+    UsersPasswordResetRequestInput,
+    UsersPasswordResetRequestPayload,
+    UsersRegisterInput,
+    UsersRegisterPayload
+} from "@code0-tech/sagittarius-graphql-types"
+import {DNamespaceProjectView} from "../d-namespace"
+import {DNamespaceLicenseReactiveService} from "../d-namespace"
+import {DNamespaceLicenseView} from "../d-namespace"
+import {DNamespaceView} from "../d-namespace"
+import {DNamespaceReactiveService} from "../d-namespace"
+import {Container} from "../container/Container";
+import {DOrganizationList} from "./DOrganizationList";
+import {DUserReactiveService, DUserView, setUserSession} from "../d-user";
+
+const meta: Meta = {
+    title: "DOrganizationList",
+    component: DOrganizationList
+}
+
+export default meta
+
+type DOrganizationCardStory = StoryObj<typeof DOrganizationCard>;
+
+class DOrganizationReactiveServiceExtended extends DOrganizationReactiveService {
+    organizationCreate(payload: OrganizationsCreateInput): Promise<OrganizationsCreatePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    organizationDelete(payload: OrganizationsDeleteInput): Promise<OrganizationsDeletePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    organizationUpdate(payload: OrganizationsUpdateInput): Promise<OrganizationsUpdatePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+}
+
+class DNamespaceReactiveServiceExtended extends DNamespaceReactiveService {
+    projectAssignRuntimes(payload: NamespacesProjectsAssignRuntimesInput): DNamespaceProjectView | undefined {
+        return undefined
+    }
+
+    projectsCreate(payload: NamespacesProjectsCreateInput): DNamespaceProjectView | undefined {
+        return undefined
+    }
+
+    projectsDelete(payload: NamespacesProjectsDeleteInput): void {
+    }
+}
+
+class DNamespaceLicenseReactiveServiceExtended extends DNamespaceLicenseReactiveService {
+    licenseCreate(payload: NamespacesLicensesCreateInput): Promise<NamespacesLicensesCreatePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    licenseDelete(payload: NamespacesLicensesDeleteInput): Promise<NamespacesLicensesDeletePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+}
+
+class DUserReactiveServiceExtended extends DUserReactiveService {
+    usersEmailVerification(payload: UsersEmailVerificationInput): Promise<UsersEmailVerificationPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersIdentityLink(payload: UsersIdentityLinkInput): Promise<UsersIdentityLinkPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersIdentityLogin(payload: UsersIdentityLoginInput): Promise<UsersIdentityLoginPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersIdentityRegister(payload: UsersIdentityRegisterInput): Promise<UsersIdentityRegisterPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersIdentityUnlink(payload: UsersIdentityUnlinkInput): Promise<UsersIdentityUnlinkPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersLogin(payload: UsersLoginInput): Promise<UsersLoginPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersLogout(payload: UsersLogoutInput): Promise<UsersLogoutPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersMfaBackupCodesRotate(payload: UsersMfaBackupCodesRotateInput): Promise<UsersMfaBackupCodesRotatePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersMfaTotpGenerateSecret(payload: UsersMfaTotpGenerateSecretInput): Promise<UsersMfaTotpGenerateSecretPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersMfaTotpValidateSecret(payload: UsersMfaTotpValidateSecretInput): Promise<UsersMfaTotpValidateSecretPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersPasswordReset(payload: UsersPasswordResetInput): Promise<UsersPasswordResetPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersPasswordResetRequest(payload: UsersPasswordResetRequestInput): Promise<UsersPasswordResetRequestPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    usersRegister(payload: UsersRegisterInput): Promise<UsersRegisterPayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+}
+
+export const DOrganizationCardExample: DOrganizationCardStory = {
+    render: (props) => {
+
+        const [organizationStore, organizationService] = useReactiveArrayService<DOrganizationView, DOrganizationReactiveServiceExtended>(DOrganizationReactiveServiceExtended, [
+            new DOrganizationView({
+                id: "gid://sagittarius/Organization/1",
+                name: "Example Organization",
+                namespace: {
+                    id: "gid://sagittarius/Namespace/1",
+                },
+                createdAt: new Date().toString(),
+                updatedAt: new Date().toString()
+            })
+        ])
+
+        const [namespaceStore, namespaceService] = useReactiveArrayService<DNamespaceView, DNamespaceReactiveServiceExtended>(DNamespaceReactiveServiceExtended, [
+            new DNamespaceView({
+                id: "gid://sagittarius/Namespace/1",
+                members: {
+                    count: 12
+                },
+                runtimes: {
+                    count: 3
+                },
+                projects: {
+                    count: 5
+                },
+                createdAt: new Date().toString(),
+                updatedAt: new Date().toString(),
+                namespaceLicenses: {
+                    count: 1,
+                    nodes: [{
+                        id: "gid://sagittarius/NamespaceLicense/1",
+                    }]
+                },
+                parent: undefined,
+                roles: undefined
+            })
+        ])
+
+        const [licenseStore, licenseService] = useReactiveArrayService<DNamespaceLicenseView, DNamespaceLicenseReactiveServiceExtended>(DNamespaceLicenseReactiveServiceExtended, [
+            new DNamespaceLicenseView({
+                id: "gid://sagittarius/NamespaceLicense/1",
+                createdAt: undefined,
+                endDate: undefined,
+                licensee: undefined,
+                namespace: undefined,
+                startDate: undefined,
+                updatedAt: undefined
+            })
+        ])
+
+        const [userStore, userService] = useReactiveArrayService<DUserView, DUserReactiveServiceExtended>(DUserReactiveServiceExtended, [
+            new DUserView({
+                id: "gid://sagittarius/User/1",
+                username: "exampleuser",
+                email: "test@gmail.com",
+                admin: undefined,
+                avatarPath: "",
+                firstname: undefined,
+                lastname: undefined,
+                namespace: undefined,
+                namespaceMemberships: undefined,
+                createdAt: new Date().toString(),
+                updatedAt: new Date().toString(),
+            })
+        ])
+
+        setUserSession({
+            user: {
+                id: "gid://sagittarius/User/1",
+                username: "exampleuser",
+                email: "test@gmail.com",
+                admin: undefined,
+                avatarPath: "",
+                firstname: undefined,
+                lastname: undefined,
+                namespace: undefined,
+                namespaceMemberships: undefined,
+                createdAt: new Date().toString(),
+                updatedAt: new Date().toString(),
+            },
+            active: true,
+            token: "test"
+        })
+
+        return (
+            <ContextStoreProvider
+                services={[[organizationStore, organizationService], [namespaceStore, namespaceService], [licenseStore, licenseService], [userStore, userService]]}>
+                <Container>
+                    <DOrganizationList/>
+                </Container>
+            </ContextStoreProvider>
+        )
+    }
+}

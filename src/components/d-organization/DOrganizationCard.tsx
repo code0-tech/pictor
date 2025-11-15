17e6b73e10e02975d19d180eb801647ed438e93c
nicosammito <nico@sammito.de>
2025-11-15 22:49:16 +0100

    feat: refactor DOrganizationCard to simplify props and enhance content integration

--- src/components/d-organization/DOrganizationCard.tsx
+++ src/components/d-organization/DOrganizationCard.tsx
@@ -1,30 +1,24 @@
 "use client"
 
 import React from "react"
-import {Code0Component, useService, useStore} from "../../utils"
+import {Code0Component} from "../../utils"
 import {Scalars} from "@code0-tech/sagittarius-graphql-types"
 import {Card} from "../card/Card"
-import {DOrganizationReactiveService} from "./DOrganization.service"
 import {DOrganizationContent} from "./DOrganizatonContent";
 
 export interface DOrganizationCardProps extends Code0Component<HTMLDivElement> {
     organizationId: Scalars['OrganizationID']['output']
-    onSettingsClick?: (organizationId: Scalars['OrganizationID']['output']) => void
-    onLeaveClick?: (organizationId: Scalars['OrganizationID']['output']) => void
+    onSetting?: (organizationId: Scalars['OrganizationID']['output']) => void
+    onLeave?: (organizationId: Scalars['OrganizationID']['output']) => void
 }
 
 const DOrganizationCard: React.FC<DOrganizationCardProps> = props => {
-    const {organizationId} = props
-    const organizationStore = useStore(DOrganizationReactiveService)
-    const organizationService = useService(DOrganizationReactiveService)
-    const organization = organizationService.getById(organizationId)
+    const {organizationId, onLeave, onSetting} = props
 
-    return React.useMemo(() => {
-        return <Card>
-            <DOrganizationContent organizationId={organization?.id}/>
-        </Card>
+    return <Card>
+        <DOrganizationContent onLeave={onLeave} onSetting={onSetting} organizationId={organizationId}/>
+    </Card>
 
-    }, [organizationStore])
 }
 
 export default DOrganizationCard

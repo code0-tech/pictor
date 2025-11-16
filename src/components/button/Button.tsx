bba0fb441d3f52cea4576dcb9be3659ac6b86317
nicosammito <nico@sammito.de>
2025-11-16 17:56:39 +0100

    feat: add dynamic padding sizes to Button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,6 @@
 import "./Button.style.scss"
-import React, {
-    ReactNode
-} from "react";
-import {mergeCode0Props} from "../../utils/utils";
-import {Code0Component, Color} from "../../utils/types"
+import React, {ReactNode} from "react";
+import {Code0Component, Code0Sizes, Color, mergeCode0Props} from "../../utils";
 
 export interface ButtonProps extends Code0Component<HTMLButtonElement> {
     children: ReactNode | ReactNode[]
@@ -15,14 +12,24 @@ export interface ButtonProps extends Code0Component<HTMLButtonElement> {
     active?: boolean
     //defaults to false
     disabled?: boolean
+    paddingSize?: Code0Sizes
 }
 
 export const Button: React.FC<ButtonProps> = React.forwardRef((props, ref) => {
 
-    const {children, variant = "normal", color = "secondary", active = false, disabled = false, ...args} = props
+    const {
+        children,
+        variant = "normal",
+        color = "secondary",
+        paddingSize = "xs",
+        active = false,
+        disabled = false,
+        ...args
+    } = props
 
-    return <button ref={ref} {...mergeCode0Props(`button button--${color} ${active ? "button--active" : ""} ${disabled ? "button--disabled" : ""} button--${variant}`, args)}
-              aria-disabled={disabled ? "true" : "false"}>
+    return <button
+        ref={ref} {...mergeCode0Props(`button button--${color} ${active ? "button--active" : ""} ${disabled ? "button--disabled" : ""} button--${variant} button--${paddingSize}`, args)}
+        aria-disabled={disabled ? "true" : "false"}>
         {children}
     </button>
 })

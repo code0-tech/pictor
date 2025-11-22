ad7fddc0e6d041a0fff1323ade305cb24a46b25e
nicosammito <nico@sammito.de>
2025-11-22 16:04:58 +0100

    feat: update DNamespaceMemberReactiveService to support dependency-based retrieval in getById method

--- src/components/d-member/DNamespaceMember.service.ts
+++ src/components/d-member/DNamespaceMember.service.ts
@@ -1,12 +1,16 @@
 import {
-    Maybe, Namespace, NamespaceMember,
-    NamespacesMembersAssignRolesInput, NamespacesMembersAssignRolesPayload,
-    NamespacesMembersDeleteInput, NamespacesMembersDeletePayload,
-    NamespacesMembersInviteInput, NamespacesMembersInvitePayload,
-    Scalars, User
+    Namespace,
+    NamespaceMember,
+    NamespacesMembersAssignRolesInput,
+    NamespacesMembersAssignRolesPayload,
+    NamespacesMembersDeleteInput,
+    NamespacesMembersDeletePayload,
+    NamespacesMembersInviteInput,
+    NamespacesMembersInvitePayload,
+    User
 } from "@code0-tech/sagittarius-graphql-types";
 import {DNamespaceMemberView} from "./DNamespaceMember.view";
-import {ReactiveArrayService, ReactiveArrayStore} from "../../utils/reactiveArrayService";
+import {ReactiveArrayService} from "../../utils";
 
 export type DMemberDependencies = {
     namespaceId: Namespace['id']
@@ -14,8 +18,8 @@ export type DMemberDependencies = {
 
 export abstract class DNamespaceMemberReactiveService extends ReactiveArrayService<DNamespaceMemberView, DMemberDependencies> {
 
-    getById(id: NamespaceMember['id']): DNamespaceMemberView | undefined {
-        return this.values().find(member => member.id === id);
+    getById(id: NamespaceMember['id'], dependencies?: DMemberDependencies): DNamespaceMemberView | undefined {
+        return this.values(dependencies).find(member => member.id === id);
     }
 
     getByNamespaceIdAndUserId(namespaceId: Namespace['id'], userId: User['id']): DNamespaceMemberView | undefined {

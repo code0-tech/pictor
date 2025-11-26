f9c1cc9fa1be90b15c0742a065db62ed5d760480
nicosammito <nico@sammito.de>
2025-11-26 13:51:31 +0100

    feat: add active state to CardSection component for improved interactivity

--- src/components/card/CardSection.tsx
+++ src/components/card/CardSection.tsx
@@ -9,6 +9,7 @@ export interface SectionType extends Code0Component<HTMLDivElement> {
     //defaults to false
     border?: boolean
     hover?: boolean
+    active?: boolean
 }
 
 const CardSection: React.FC<SectionType> = (props) => {
@@ -17,6 +18,7 @@ const CardSection: React.FC<SectionType> = (props) => {
         image = false,
         border = false,
         hover = false,
+        active = false,
         children,
         ...args
     } = props;
@@ -27,6 +29,7 @@ const CardSection: React.FC<SectionType> = (props) => {
                                 ${border ? "card__section--border" : ""} 
                                 ${image ? "card__section--image" : ""}
                                 ${hover ? "card__section--hover" : ""}
+                                ${active ? "card__section--active" : ""}
         `, args)}>
             {children}
         </div>

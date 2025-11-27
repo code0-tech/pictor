f66e7d0eaa57d043c30545517eb4e85aaa5b1c0f
nicosammito <nico@sammito.de>
2025-11-27 22:42:21 +0100

    feat: enhance DFlowValidation component with improved validation display and styling

--- src/components/d-flow/validation/DFlowValidation.tsx
+++ src/components/d-flow/validation/DFlowValidation.tsx
@@ -1,12 +1,13 @@
 import {Panel} from "@xyflow/react";
 import React from "react";
-import {useDFlowValidations} from "./DFlowValidation.hook";
 import type {Scalars} from "@code0-tech/sagittarius-graphql-types";
 import {Flex} from "../../flex/Flex";
-import {InspectionSeverity} from "../../../utils/inspection";
+import {InspectionSeverity, ValidationResult} from "../../../utils/inspection";
 import {Badge} from "../../badge/Badge";
 import {IconAlertTriangle, IconExclamationCircle, IconMessageExclamation} from "@tabler/icons-react";
 import "./DFlowValidation.style.scss"
+import {Text} from "../../text/Text";
+import {useDFlowValidations} from "./DFlowValidation.hook";
 
 export interface DFlowValidationProps {
     flowId: Scalars['FlowID']['output']
@@ -15,34 +16,34 @@ export interface DFlowValidationProps {
 export const DFlowValidation: React.FC<DFlowValidationProps> = (props) => {
 
     const {flowId} = props
-    const validations = useDFlowValidations(flowId)
+    const validations: ValidationResult[] = useDFlowValidations(flowId)
 
     return <Panel position="top-right">
         {(validations?.length ?? 0) > 0 ? (
             <div className={"d-flow-viewport-validations"}>
-                <Flex style={{gap: "0.35rem"}}>
+                <Flex align={"center"} style={{gap: "0.7rem"}}>
                     {(validations?.filter(v => v.type === InspectionSeverity.ERROR)?.length ?? 0) > 0 ? (
-                        <Badge color={"error"}>
+                        <Badge border color={"error"}>
                             <Flex align={"center"} style={{gap: "0.35rem"}}>
-                                <IconExclamationCircle size={12}/>
-                                {validations?.filter(v => v.type === InspectionSeverity.ERROR)?.length}
+                                <IconExclamationCircle size={16}/>
+                                <Text style={{color: "inherit"}}>{validations?.filter(v => v.type === InspectionSeverity.ERROR)?.length}</Text>
                             </Flex>
                         </Badge>
                     ) : null}
 
                     {(validations?.filter(v => v.type === InspectionSeverity.WARNING)?.length ?? 0) > 0 ? (
-                        <Badge color={"warning"}>
+                        <Badge border color={"warning"}>
                             <Flex align={"center"} style={{gap: "0.35rem"}}>
-                                <IconAlertTriangle size={12}/>
+                                <IconAlertTriangle size={16}/>
                                 {validations?.filter(v => v.type === InspectionSeverity.WARNING)?.length}
                             </Flex>
                         </Badge>
                     ) : null}
 
                     {(validations?.filter(v => v.type === InspectionSeverity.GRAMMAR)?.length ?? 0) > 0 ? (
-                        <Badge>
+                        <Badge border>
                             <Flex align={"center"} style={{gap: "0.35rem"}}>
-                                <IconMessageExclamation size={12}/>
+                                <IconMessageExclamation size={16}/>
                                 {validations?.filter(v => v.type === InspectionSeverity.GRAMMAR)?.length}
                             </Flex>
                         </Badge>

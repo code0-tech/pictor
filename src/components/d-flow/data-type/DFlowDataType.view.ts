a86a1113d7d370034452add4e5f1c66937f1655a
nicosammito <nico@sammito.de>
2025-11-27 18:44:05 +0100

    feat: add aliases and displayMessages properties to DataTypeView for enhanced functionality

--- src/components/d-flow/data-type/DFlowDataType.view.ts
+++ src/components/d-flow/data-type/DFlowDataType.view.ts
@@ -10,8 +10,12 @@ import type {
  */
 export class DataTypeView {
 
+    /** Name of the function */
+    private readonly _aliases?: Maybe<TranslationConnection>;
     /** Time when this DataType was created */
     private readonly _createdAt?: Maybe<Scalars['Time']['output']>;
+    /** Display message of the function */
+    private readonly _displayMessages?: Maybe<TranslationConnection>;
     /** Generic keys of the datatype */
     private readonly _genericKeys?: Maybe<Array<Scalars['String']['output']>>;
     /** Global ID of this DataType */
@@ -20,30 +24,39 @@ export class DataTypeView {
     private readonly _identifier?: Maybe<Scalars['String']['output']>;
     /** Names of the flow type setting */
     private readonly _name?: Maybe<TranslationConnection>;
-    /** The namespace where this datatype belongs to */
-    private readonly _runtime?: Maybe<Runtime>;
     /** Rules of the datatype */
     private readonly _rules?: Maybe<DataTypeRuleConnection>;
+    /** The namespace where this datatype belongs to */
+    private readonly _runtime?: Maybe<Runtime>;
     /** Time when this DataType was last updated */
     private readonly _updatedAt?: Maybe<Scalars['Time']['output']>;
     /** The type of the datatype */
     private readonly _variant?: Maybe<DataTypeVariant>;
 
     constructor(dataType: DataType) {
-        this._id = dataType.id
-        this._createdAt = dataType.createdAt
-        this._updatedAt = dataType.updatedAt
-        this._identifier = dataType.identifier
-        this._name = dataType.name ?? undefined
-        this._runtime = dataType.runtime ?? undefined
-        this._variant = dataType.variant
-        this._genericKeys = dataType.genericKeys ?? undefined
-        this._rules = dataType.rules ?? undefined
+        this._aliases = dataType.aliases;
+        this._createdAt = dataType.createdAt;
+        this._displayMessages = dataType.displayMessages;
+        this._genericKeys = dataType.genericKeys;
+        this._id = dataType.id;
+        this._identifier = dataType.identifier;
+        this._name = dataType.name;
+        this._runtime = dataType.runtime;
+        this._rules = dataType.rules;
+        this._updatedAt = dataType.updatedAt;
+        this._variant = dataType.variant;
+    }
 
+    get aliases(): Maybe<TranslationConnection> | undefined {
+        return this._aliases;
     }
 
     get createdAt(): Maybe<Scalars["Time"]["output"]> | undefined {
         return this._createdAt;
+    }
+
+    get displayMessages(): Maybe<TranslationConnection> | undefined {
+        return this._displayMessages;
     }
 
     get genericKeys(): Maybe<Array<Scalars["String"]["output"]>> | undefined {

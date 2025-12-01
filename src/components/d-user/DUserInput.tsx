3f480a84ff7fcb9505b456e9f8de7bcc4b431b5f
nicosammito <nico@sammito.de>
2025-12-01 17:35:57 +0100

    feat: remove unused InputSuggestionStory component from stories

---
+++ src/components/d-user/DUserInput.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {InputSuggestion, TextInput, TextInputProps} from "../form";
+import {InputSyntaxSegment} from "../form/Input.syntax.hook";
+import {Badge} from "../badge/Badge";
+import {Text} from "../text/Text";
+import {useService, useStore} from "../../utils";
+import {DUserReactiveService} from "./DUser.service";
+import {MenuItem} from "../menu/Menu";
+
+export interface DUserInputProps extends TextInputProps {
+
+}
+
+export const DUserInput: React.FC<DUserInputProps> = (props) => {
+
+    const {...rest} = props
+
+    const userService = useService(DUserReactiveService)
+    const userStore = useStore(DUserReactiveService)
+    const suggestions: InputSuggestion[] = React.useMemo(() => {
+        return userService.values().map(user => ({
+            value: user.username || "",
+            children: user.username,
+            insertMode: "insert",
+            valueData: user
+        }))
+    }, [userStore])
+
+    const transformSyntax = (
+        _?: string | null,
+        appliedParts: (InputSuggestion | any)[] = [],
+    ): InputSyntaxSegment[] => {
+
+        let cursor = 0
+
+        return appliedParts.map((part: string | InputSuggestion, index) => {
+            if (typeof part === "object") {
+                const segment = {
+                    type: "block",
+                    start: cursor,
+                    end: cursor + part.value.length,
+                    visualLength: 1,
+                    content: <Badge color={"info"} border>
+                        <Text style={{color: "inherit"}}>
+                            @{part.value}
+                        </Text>
+                    </Badge>,
+                }
+                cursor += part.value.length
+                return segment
+            }
+            const textString = part ?? ""
+            if (!textString.length) return
+
+            if (index == appliedParts.length - 1) {
+                const segment = {
+                    type: "text",
+                    start: cursor,
+                    end: cursor + textString.length,
+                    visualLength: textString.length,
+                    content: textString,
+                }
+                cursor += textString.length
+                return segment
+            }
+            cursor += textString.length
+            return {}
+        }) as InputSyntaxSegment[]
+    }
+
+    return <TextInput placeholder={"Enter users"} suggestionsEmptyState={<MenuItem><Text>No user found</Text></MenuItem>} filterSuggestionsByLastToken enforceUniqueSuggestions transformSyntax={transformSyntax} {...rest} suggestions={suggestions}/>
+}

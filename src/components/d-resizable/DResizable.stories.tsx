e96b27e4542ba63e2f7a261f9b4fcc409abdeb52
nicosammito <nico@sammito.de>
2025-12-07 22:11:21 +0100

    feat: update flow type ID in DResizable stories for consistency

--- src/components/d-resizable/DResizable.stories.tsx
+++ src/components/d-resizable/DResizable.stories.tsx
@@ -85,7 +85,7 @@ export const Dashboard = () => {
     const [flowStore, flowService] = useReactiveArrayService<FlowView, DFlowReactiveService>(DFlowReactiveServiceExtend, [new FlowView({
         id: "gid://sagittarius/Flow/1",
         type: {
-            id: "gid://sagittarius/TypesFlowType/842",
+            id: "gid://sagittarius/FlowType/867",
         },
         name: "de/codezero/examples/REST Flow",
         settings: {

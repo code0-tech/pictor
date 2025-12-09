efe5bb54a226c041e21b84a15f018301d2fd71d6
nicosammito <nico@sammito.de>
2025-12-09 17:04:06 +0100

    feat: add ContextMenu component with styles and storybook integration

---
+++ src/components/context-menu/ContextMenu.stories.tsx
@@ -0,0 +1,9 @@
+import {ContextMenu} from "./ContextMenu";
+import {Meta} from "@storybook/react-vite";
+
+const meta: Meta<typeof ContextMenu> = {
+    title: 'Components/ContextMenu',
+    component: ContextMenu,
+}
+
+export default meta

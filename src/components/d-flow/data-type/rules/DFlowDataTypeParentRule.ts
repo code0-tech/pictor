88d92bbe75c2bc2848877f075d8ccd6ff69d5aed
nicosammito <nico@sammito.de>
2025-12-11 16:38:02 +0100

    feat: update DFlowDataTypeParentRule to use Flow type instead of FlowView

--- src/components/d-flow/data-type/rules/DFlowDataTypeParentRule.ts
+++ src/components/d-flow/data-type/rules/DFlowDataTypeParentRule.ts
@@ -1,9 +1,8 @@
 import {DFlowDataTypeRule, staticImplements} from "./DFlowDataTypeRule";
 import {DFlowDataTypeReactiveService} from "../DFlowDataType.service";
 import {replaceGenericKeysInType} from "../../../../utils/generics";
-import type {DataTypeIdentifier, GenericMapper, NodeParameterValue} from "@code0-tech/sagittarius-graphql-types";
+import type {DataTypeIdentifier, Flow, GenericMapper, NodeParameterValue} from "@code0-tech/sagittarius-graphql-types";
 import {useValidateValue} from "../DFlowDataType.validation.value";
-import {FlowView} from "../../DFlow.view";
 
 export interface DFlowDataTypeParentRuleConfig {
     type: DataTypeIdentifier
@@ -11,7 +10,7 @@ export interface DFlowDataTypeParentRuleConfig {
 
 @staticImplements<DFlowDataTypeRule>()
 export class DFlowDataTypeParentRule {
-    public static validate(value: NodeParameterValue, config: DFlowDataTypeParentRuleConfig, generics?: Map<string, GenericMapper>, service?: DFlowDataTypeReactiveService, flow?: FlowView): boolean {
+    public static validate(value: NodeParameterValue, config: DFlowDataTypeParentRuleConfig, generics?: Map<string, GenericMapper>, service?: DFlowDataTypeReactiveService, flow?: Flow): boolean {
 
         const replacedType = generics ? replaceGenericKeysInType(config.type, generics) : config.type
 

e5a6770bef3d4f284b0ad4fd0d030d679ad0050f
nicosammito <nico@sammito.de>
2025-12-19 22:33:12 +0100

    feat: implement hooks for data type, function, reference, and value suggestions

---
+++ src/components/d-flow-suggestion/DFlowValueSuggestions.hook.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {useService, useStore} from "../../utils";
+import {DFlowDataTypeReactiveService} from "../d-flow-data-type";
+import {DFlowSuggestion, DFlowSuggestionType} from "./DFlowSuggestion.view";
+import type {
+    DataTypeIdentifier,
+    DataTypeRulesItemOfCollectionConfig,
+    DataTypeRulesNumberRangeConfig
+} from "@code0-tech/sagittarius-graphql-types";
+
+export const useValueSuggestions = (dataTypeIdentifier?: DataTypeIdentifier): DFlowSuggestion[] => {
+    const dataTypeService = useService(DFlowDataTypeReactiveService)
+    const dataTypeStore = useStore(DFlowDataTypeReactiveService)
+
+    const dataType = React.useMemo(() => (
+        dataTypeIdentifier ? dataTypeService?.getDataType(dataTypeIdentifier) : undefined
+    ), [dataTypeIdentifier, dataTypeService, dataTypeStore])
+
+    return React.useMemo(() => {
+        if (!dataType) return []
+
+        const suggestions: DFlowSuggestion[] = []
+        dataType.rules?.nodes?.forEach(rule => {
+            if (rule?.variant === "ITEM_OF_COLLECTION") {
+                (rule.config as DataTypeRulesItemOfCollectionConfig)!!.items?.forEach(value => {
+                    suggestions.push({
+                        path: [],
+                        type: DFlowSuggestionType.VALUE,
+                        displayText: [value.toString()],
+                        value: {
+                            __typename: "LiteralValue",
+                            value: value
+                        },
+                    })
+                })
+            } else if (rule?.variant === "NUMBER_RANGE") {
+                const config: DataTypeRulesNumberRangeConfig = rule.config as DataTypeRulesNumberRangeConfig
+                if (config.from === null || config.from === undefined) return
+                if (config.to === null || config.to === undefined) return
+
+                for (let i = config.from; i <= config.to; i += (config.steps ?? 1)) {
+                    suggestions.push({
+                        path: [],
+                        type: DFlowSuggestionType.VALUE,
+                        displayText: [i.toString() ?? ""],
+                        value: {
+                            __typename: "LiteralValue",
+                            value: i
+                        },
+                    })
+                }
+            }
+        })
+
+        return suggestions
+    }, [dataType])
+}

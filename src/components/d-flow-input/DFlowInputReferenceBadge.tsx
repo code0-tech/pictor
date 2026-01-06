bb0159d3a1e287f01450be9c38ee2fab128e4471
nicosammito <nico@sammito.de>
2026-01-06 17:50:15 +0100

    feat: update DFlowInputReferenceBadge to format display text with braces for improved clarity

--- src/components/d-flow-input/DFlowInputReferenceBadge.tsx
+++ src/components/d-flow-input/DFlowInputReferenceBadge.tsx
@@ -2,7 +2,6 @@ import {Badge, BadgeType} from "../badge/Badge";
 import {ReferenceValue} from "@code0-tech/sagittarius-graphql-types";
 import React from "react";
 import {Text} from "../text/Text";
-import {IconCirclesRelation} from "@tabler/icons-react";
 import {md5} from "js-md5";
 
 export interface DFlowInputReferenceBadge extends Omit<BadgeType, 'value' | 'children'> {
@@ -23,9 +22,8 @@ export const DFlowInputReferenceBadge: React.FC<DFlowInputReferenceBadge> = (pro
                   color={`hsl(${hashToHue(hashRef)}, 100%, 72%)`}
                   border
                   {...rest}>
-        <IconCirclesRelation size={12}/>
         <Text size={"sm"} style={{color: "inherit"}}>
-            {String(value.depth)}-{String(value.scope)}-{String(value.node)}-{value.referencePath?.map(path => path.path).join(".") ?? ""}
+            {`{{`} {String(value.depth)}-{String(value.scope)}-{String(value.node)}-{value.referencePath?.map(path => path.path).join(".") ?? ""} {`}}`}
         </Text>
     </Badge>
 }

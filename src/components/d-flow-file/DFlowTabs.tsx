f3450529a59fb4860a47c7c7250abbcb6c8563c2
nicosammito <nico@sammito.de>
2026-01-06 14:28:14 +0100

    feat: simplify tab activation logic in DFlowTabs by removing unnecessary update call

--- src/components/d-flow-file/DFlowTabs.tsx
+++ src/components/d-flow-file/DFlowTabs.tsx
@@ -75,8 +75,7 @@ export const DFlowTabs: React.FC<DFlowTabsProps> = (props) => {
             data-id={id}
             value={activeTabId}
             onValueChange={(value) => {
-                fileTabsService.activateTab(value); // mutieren reicht; kein .update() nötig, wenn setState benutzt wird
-                fileTabsService.update()
+                fileTabsService.activateTab(value);
             }}
         >
             <DLayout topContent={<FileTabsList

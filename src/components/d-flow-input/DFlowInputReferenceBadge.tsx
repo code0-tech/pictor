1949007549ef3daf25454b0ec9f75fc69149dc51
nicosammito <nico@sammito.de>
2026-01-08 00:02:02 +0100

    feat: enhance DFlowInputReferenceBadge to include IconVariable and improve rendering logic

--- src/components/d-flow-input/DFlowInputReferenceBadge.tsx
+++ src/components/d-flow-input/DFlowInputReferenceBadge.tsx
@@ -6,6 +6,7 @@ import {Flex} from "../flex/Flex";
 import {DFlowInputNodeBadge} from "./DFlowInputNodeBadge";
 import {FunctionDefinitionView} from "../d-flow-function";
 import {FlowTypeView} from "../d-flow-type";
+import {IconVariable} from "@tabler/icons-react";
 
 export interface DFlowInputReferenceBadge extends Omit<BadgeType, 'value' | 'children'> {
     value: ReferenceValue
@@ -17,26 +18,28 @@ export const DFlowInputReferenceBadge: React.FC<DFlowInputReferenceBadge> = (pro
 
     const {value, flowId, definition, ...rest} = props
     const content = React.useMemo(() => {
+        console.log(value)
         if (value.nodeFunctionId && flowId) {
             return <Flex align={"center"} display={"inline-flex"}>
                 <DFlowInputNodeBadge definition={definition} value={{
                     id: value.nodeFunctionId,
+                    __typename: "NodeFunctionIdWrapper"
                 }} flowId={flowId}/>
-                .{value.referencePath?.map(path => path.path).join(".") ?? ""}
+                {"inputTypeIdentifier" in value && value.inputTypeIdentifier ? "." + value.inputTypeIdentifier : ""}
+                {value.referencePath ? "." + (value.referencePath?.map(path => path.path).join(".") ?? "") : ""}
             </Flex>
         }
         return `{{ ${String(value.depth)}-${String(value.scope)}-${String(value.node)}-${value.referencePath?.map(path => path.path).join(".") ?? ""} }}`
     }, [value])
 
-    return <Badge style={{verticalAlign: "bottom"}}
+    return <Badge style={{verticalAlign: "middle"}}
                   color={"warning"}
                   py={"0"}
                   border
                   {...rest}>
+        <IconVariable size={12}/>
         <Text size={"sm"} style={{color: "inherit"}}>
-            {"{{ "}
             {content}
-            {" }}"}
         </Text>
     </Badge>
 }

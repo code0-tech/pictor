9977346f8aa80107dc5f3e465a3582e1be0ad61c
nicosammito <nico@sammito.de>
2026-01-07 22:03:20 +0100

    feat: enhance DFlowInputReferenceBadge to include flowId and definition for improved rendering

--- src/components/d-flow-input/DFlowInputReferenceBadge.tsx
+++ src/components/d-flow-input/DFlowInputReferenceBadge.tsx
@@ -1,29 +1,42 @@
 import {Badge, BadgeType} from "../badge/Badge";
-import {ReferenceValue} from "@code0-tech/sagittarius-graphql-types";
+import {Flow, ReferenceValue} from "@code0-tech/sagittarius-graphql-types";
 import React from "react";
 import {Text} from "../text/Text";
-import {md5} from "js-md5";
+import {Flex} from "../flex/Flex";
+import {DFlowInputNodeBadge} from "./DFlowInputNodeBadge";
+import {FunctionDefinitionView} from "../d-flow-function";
+import {FlowTypeView} from "../d-flow-type";
 
 export interface DFlowInputReferenceBadge extends Omit<BadgeType, 'value' | 'children'> {
     value: ReferenceValue
+    flowId: Flow['id']
+    definition?: FunctionDefinitionView | FlowTypeView
 }
 
 export const DFlowInputReferenceBadge: React.FC<DFlowInputReferenceBadge> = (props) => {
 
-    const {value, ...rest} = props
-    const hashRef = md5(md5(value.nodeFunctionId || ""))
+    const {value, flowId, definition, ...rest} = props
+    const content = React.useMemo(() => {
+        if (value.nodeFunctionId && flowId) {
+            return <Flex align={"center"} display={"inline-flex"}>
+                <DFlowInputNodeBadge definition={definition} value={{
+                    id: value.nodeFunctionId,
+                }} flowId={flowId}/>
+                .{value.referencePath?.map(path => path.path).join(".") ?? ""}
+            </Flex>
+        }
+        return `{{ ${String(value.depth)}-${String(value.scope)}-${String(value.node)}-${value.referencePath?.map(path => path.path).join(".") ?? ""} }}`
+    }, [value])
 
-    const hashToHue = (md5: string): number => {
-        const int = parseInt(md5.slice(0, 8), 16)
-        return int % 360
-    }
-
-    return <Badge style={{verticalAlign: "middle"}}
-                  color={`hsl(${hashToHue(hashRef)}, 100%, 72%)`}
+    return <Badge style={{verticalAlign: "bottom"}}
+                  color={"warning"}
+                  py={"0"}
                   border
                   {...rest}>
         <Text size={"sm"} style={{color: "inherit"}}>
-            {`{{`} {String(value.depth)}-{String(value.scope)}-{String(value.node)}-{value.referencePath?.map(path => path.path).join(".") ?? ""} {`}}`}
+            {"{{ "}
+            {content}
+            {" }}"}
         </Text>
     </Badge>
 }

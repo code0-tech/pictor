c1be3e030a2f71365f5ecf6043395de4a681f779
nicosammito <nico@sammito.de>
2026-01-13 14:45:32 +0100

    refactor: add color property to DFlowNodeProps for enhanced visualization

--- src/components/d-flow-node/DFlowNode.ts
+++ src/components/d-flow-node/DFlowNode.ts
@@ -4,6 +4,7 @@ import {Flow, NodeFunction} from "@code0-tech/sagittarius-graphql-types";
 export interface DFlowNodeProps extends Record<string, unknown>, Code0Component<HTMLDivElement> {
     nodeId: NodeFunction['id']
     flowId: Flow['id']
+    color: string
     parentNodeId?: NodeFunction['id']
     isParameter?: boolean
     index?: number

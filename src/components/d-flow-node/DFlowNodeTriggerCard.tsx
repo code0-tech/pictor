c7ba30a801eec7763d1cdb50a68fb826d86e37ed
nicosammito <nico@sammito.de>
2026-01-13 14:07:07 +0100

    refactor: update DFlowNodeTriggerCard to use DFlowNodeProps for improved type consistency

--- src/components/d-flow-node/DFlowNodeTriggerCard.tsx
+++ src/components/d-flow-node/DFlowNodeTriggerCard.tsx
@@ -1,24 +1,20 @@
 import React, {memo} from "react";
-import {Code0Component} from "../../utils";
 import {Handle, Node, NodeProps, Position, useReactFlow, useStore} from "@xyflow/react";
 import {Text} from "../text/Text";
-import {useService} from "../../utils";
+import {useService, useStore as usePictorStore} from "../../utils/contextStore";
 import {FileTabsService} from "../file-tabs/FileTabs.service";
 import {Card} from "../card/Card";
 import {Flex} from "../flex/Flex";
-import {IconBolt, IconChevronDown, IconFile} from "@tabler/icons-react";
+import {IconBolt, IconChevronDown} from "@tabler/icons-react";
 import {Button} from "../button/Button";
 import {DFlowTabTrigger} from "../d-flow-file/DFlowTabTrigger";
 import {DFlowTypeReactiveService} from "../d-flow-type";
 import {Badge} from "../badge/Badge";
-import {Flow} from "@code0-tech/sagittarius-graphql-types";
-
-export interface DFlowNodeTriggerCardDataProps extends Omit<Code0Component<HTMLDivElement>, "scope"> {
-    instance: Flow
-}
+import {DFlowNodeProps} from "./DFlowNode";
+import {DFlowReactiveService} from "../d-flow";
 
 // @ts-ignore
-export type DFlowNodeTriggerCardProps = NodeProps<Node<DFlowNodeTriggerCardDataProps>>
+export type DFlowNodeTriggerCardProps = NodeProps<Node<DFlowNodeProps>>
 
 export const DFlowNodeTriggerCard: React.FC<DFlowNodeTriggerCardProps> = memo((props) => {
 
@@ -26,14 +22,20 @@ export const DFlowNodeTriggerCard: React.FC<DFlowNodeTriggerCardProps> = memo((p
     const fileTabsService = useService(FileTabsService)
     const flowInstance = useReactFlow()
     const flowTypeService = useService(DFlowTypeReactiveService)
-    const definition = flowTypeService.getById(data.instance.type?.id!!)
+    const flowTypeStore = usePictorStore(DFlowTypeReactiveService)
+    const flowService = useService(DFlowReactiveService)
+    const flowStore = usePictorStore(DFlowReactiveService)
+
+    const flow = React.useMemo(() => flowService.getById(data.flowId), [flowStore, data])
+    const definition = React.useMemo(() => flow ? flowTypeService.getById(flow.type?.id) : undefined, [flowTypeStore, flow])
+
     const width = props.width ?? 0
     const height = props.height ?? 0
     const viewportWidth = useStore(s => s.width)
     const viewportHeight = useStore(s => s.height)
 
     React.useEffect(() => {
-        if (!definition?.id) return
+        if (!definition?.id || !flow) return
         fileTabsService.registerTab({
             id: definition?.id!!,
             active: true,
@@ -42,17 +44,22 @@ export const DFlowNodeTriggerCard: React.FC<DFlowNodeTriggerCardProps> = memo((p
                 <IconBolt size={12}/>
                 <Text size={"sm"}>{definition?.names!![0]?.content}</Text>
             </>,
-            content: <DFlowTabTrigger instance={data.instance}/>,
+            content: <DFlowTabTrigger instance={flow}/>,
             show: true
         })
-    }, [definition, data.instance, fileTabsService])
+    }, [definition, data.instance, fileTabsService, flow])
 
     return <Flex align={"center"} style={{flexDirection: "column"}} key={id} data-flow-refernce={id}>
-        <Badge color={"info"} style={{borderTopRightRadius: "0.35rem", borderTopLeftRadius: "0.35rem", borderBottomLeftRadius: 0, borderBottomRightRadius: 0}}>Starting node</Badge>
+        <Badge color={"info"} style={{
+            borderTopRightRadius: "0.35rem",
+            borderTopLeftRadius: "0.35rem",
+            borderBottomLeftRadius: 0,
+            borderBottomRightRadius: 0
+        }}>Starting node</Badge>
         <Card variant={"normal"}
               color={"info"}
               paddingSize={"xs"}
-              className={fileTabsService.getActiveTab()?.id == definition?.id ? "d-flow-viewport-default-card--active" : undefined}
+              className={fileTabsService.getActiveTab()?.id == definition?.id ? "d-flow-node--active" : undefined}
               onClick={() => {
                   flowInstance.setViewport({
                       x: (viewportWidth / 2) + (props.positionAbsoluteX * -1) - (width / 2),
@@ -83,7 +90,7 @@ export const DFlowNodeTriggerCard: React.FC<DFlowNodeTriggerCardProps> = memo((p
                 isConnectable={false}
                 type="source"
                 style={{bottom: "2px"}}
-                className={"d-flow-viewport-default-card__handle d-flow-viewport-default-card__handle--source"}
+                className={"d-flow-node__handle d-flow-node__handle--source"}
                 position={Position.Bottom}
             />
         </Card>

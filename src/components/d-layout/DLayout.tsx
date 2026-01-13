a2d26a6b59b224d7c1c8089df0b3008e0c188712
nicosammito <nico@sammito.de>
2026-01-13 11:01:08 +0100

    feat

--- src/components/d-layout/DLayout.tsx
+++ src/components/d-layout/DLayout.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import {Code0Component} from "../../utils/types";
-import {mergeCode0Props} from "../../utils/utils";
+import React, {CSSProperties} from "react";
+import {Code0Component, Code0Sizes} from "../../utils/types";
+import {getDOMSizeFromCodeZeroSize, mergeCode0Props} from "../../utils/utils";
 import {Flex} from "../flex/Flex";
 import "./DLayout.style.scss"
 
@@ -10,30 +10,45 @@ export interface DLayoutProps extends Code0Component<HTMLDivElement> {
     bottomContent?: React.ReactElement
     leftContent?: React.ReactElement
     rightContent?: React.ReactElement
+    layoutGap?: Code0Sizes | CSSProperties['gap']
+    showLayoutSplitter?: boolean
 }
 
 export const DLayout: React.FC<DLayoutProps> = (props) => {
-    const {children, topContent, bottomContent, leftContent, rightContent, ...rest} = props
+    const {children, topContent, bottomContent, leftContent, rightContent, showLayoutSplitter = true, layoutGap = "xl", ...rest} = props
 
     return (
         <div {...mergeCode0Props("d-layout", rest)}>
-            <Flex className="d-layout__inner" style={{ flexDirection: "column" }}>
+            <Flex className="d-layout__inner" style={{ flexDirection: "column", gap: getDOMSizeFromCodeZeroSize(layoutGap)}}>
                 {topContent && (
                     <div className="d-layout__top">
                         {topContent}
                     </div>
                 )}
 
-                <Flex className="d-layout__middle">
+                {topContent && showLayoutSplitter && (
+                    <div className="d-layout__splitter" aria-orientation={"horizontal"}/>
+                )}
+
+                <Flex className="d-layout__middle" style={{gap: getDOMSizeFromCodeZeroSize(layoutGap)}}>
                     {leftContent && (
                         <div className="d-layout__left">
                             {leftContent}
                         </div>
                     )}
 
+                    {leftContent && showLayoutSplitter && (
+                        <div className="d-layout__splitter" aria-orientation={"vertical"}/>
+                    )}
+
                     <div className="d-layout__content">
                         {children}
                     </div>
+
+
+                    {rightContent && showLayoutSplitter && (
+                        <div className="d-layout__splitter" aria-orientation={"vertical"}/>
+                    )}
 
                     {rightContent && (
                         <div className="d-layout__right">
@@ -41,6 +56,10 @@ export const DLayout: React.FC<DLayoutProps> = (props) => {
                         </div>
                     )}
                 </Flex>
+
+                {bottomContent && showLayoutSplitter && (
+                    <div className="d-layout__splitter" aria-orientation={"horizontal"}/>
+                )}
 
                 {bottomContent && (
                     <div className="d-layout__bottom">

f6bd7a26d69c928ff46170ca318b8d2f8429ee02
nicosammito <nico@sammito.de>
2026-01-27 13:20:24 +0100

    feat: add flow type selection to DFlowFolderCreateDialog for enhanced user experience

--- src/components/d-flow-folder/DFlowFolderCreateDialog.tsx
+++ src/components/d-flow-folder/DFlowFolderCreateDialog.tsx
@@ -1,26 +1,43 @@
-import {DFlowFolderProps} from "./DFlowFolder";
 import React from "react";
-import {TextInput, useForm} from "../form";
+import {InputDescription, InputLabel, InputMessage, useForm} from "../form";
 import {Dialog, DialogClose, DialogContent, DialogPortal} from "../dialog/Dialog";
 import {Flex} from "../flex/Flex";
 import {Button} from "../button/Button";
 import {FlowType} from "@code0-tech/sagittarius-graphql-types";
 import {DFlowFolderItemPathInput} from "./DFlowFolderItemPathInput";
+import {useService, useStore} from "../../utils";
+import {DFlowTypeReactiveService} from "../d-flow-type";
+import {Menu, MenuContent, MenuItem, MenuLabel, MenuPortal, MenuTrigger} from "../menu/Menu";
+import {Spacing} from "../spacing/Spacing";
+import {Badge} from "../badge/Badge";
+import {IconArrowDown, IconArrowUp, IconCornerDownLeft, IconFile} from "@tabler/icons-react";
+import {Card} from "../card/Card";
+import {hashToColor} from "../d-flow/DFlow.util";
 
 export interface DFlowFolderCreateDialogProps {
     open?: boolean
     onOpenChange?: (open: boolean) => void
-    flowTypeId: FlowType['id']
+    flowTypeId?: FlowType['id']
     onCreate?: (name: string, type: FlowType['id']) => void
 }
 
 export const DFlowFolderCreateDialog: React.FC<DFlowFolderCreateDialogProps> = (props) => {
+
     const {open} = props
 
+    const flowTypeService = useService(DFlowTypeReactiveService)
+    const flowTypeStore = useStore(DFlowTypeReactiveService)
+
     const [createDialogOpen, setCreateDialogOpen] = React.useState(open)
-    const initialValues = React.useMemo(() => ({
-        name: ""
-    }), [])
+    const [selectedFlowTypeId, setSelectedFlowTypeId] = React.useState<FlowType['id'] | undefined>(props.flowTypeId)
+    const selectedFlowType = React.useMemo(() => {
+        return flowTypeService.getById(selectedFlowTypeId)
+    }, [selectedFlowTypeId, flowTypeStore])
+
+    const initialValues = React.useMemo(
+        () => ({name: "", flowTypeId: props.flowTypeId ?? null}),
+        []
+    )
 
     React.useEffect(() => {
         setCreateDialogOpen(open)
@@ -32,10 +49,14 @@ export const DFlowFolderCreateDialog: React.FC<DFlowFolderCreateDialogProps> = (
             name: (value) => {
                 if (!value) return "Name is required"
                 return null
+            },
+            flowTypeId: (value) => {
+                if (!value) return "Flow type is required"
+                return null
             }
         },
         onSubmit: (values) => {
-            props.onCreate?.(values.name, props.flowTypeId)
+            props.onCreate?.(values.name, values.flowTypeId)
             props.onOpenChange?.(false)
         }
     })
@@ -47,6 +68,54 @@ export const DFlowFolderCreateDialog: React.FC<DFlowFolderCreateDialogProps> = (
             <DialogContent autoFocus showCloseButton
                            title={"Create new flow"}>
                 <div>
+                    <InputLabel>Type of flow</InputLabel>
+                    <InputDescription>You can choose a flow type here</InputDescription>
+                    <Menu>
+                        <MenuTrigger asChild>
+                            <Button w={"100%"}
+                                    color={inputs.getInputProps("flowTypeId").formValidation?.valid ? "secondary" : "error"}
+                                    style={{justifyContent: "start"}}>
+                                {selectedFlowTypeId ?
+                                    <IconFile size={13} color={hashToColor(selectedFlowTypeId)}/> : null}
+                                {selectedFlowType ? selectedFlowType?.names?.[0].content : "Select flow type"}
+                            </Button>
+                        </MenuTrigger>
+                        <MenuPortal>
+                            <MenuContent align={"start"}
+                                         color={"secondary"}
+                                         sideOffset={8}>
+                                <Card paddingSize={"xxs"} mt={-0.35} mx={-0.35} style={{borderWidth: "2px"}}>
+                                    {flowTypeService.values().map((flowType) => (
+                                        <MenuItem key={flowType.id} onSelect={() => {
+                                            inputs.getInputProps("flowTypeId").formValidation?.setValue(flowType.id)
+                                            setSelectedFlowTypeId(flowType.id)
+                                        }}>
+                                            {flowType.names?.[0].content}
+                                        </MenuItem>
+                                    ))}
+                                </Card>
+                                <MenuLabel>
+                                    <Flex style={{gap: ".35rem"}}>
+                                        <Flex align={"center"} style={{gap: "0.35rem"}}>
+                                            <Flex>
+                                                <Badge border><IconArrowUp size={12}/></Badge>
+                                                <Badge border><IconArrowDown size={12}/></Badge>
+                                            </Flex>
+                                            move
+                                        </Flex>
+                                        <Spacing spacing={"xxs"}/>
+                                        <Flex align={"center"} style={{gap: ".35rem"}}>
+                                            <Badge border><IconCornerDownLeft size={12}/></Badge>
+                                            select
+                                        </Flex>
+                                    </Flex>
+                                </MenuLabel>
+                            </MenuContent>
+                        </MenuPortal>
+                    </Menu>
+                    {!inputs.getInputProps("flowTypeId").formValidation?.valid ?
+                        <InputMessage>{inputs.getInputProps("flowTypeId").formValidation?.notValidMessage!}</InputMessage> : null}
+                    <Spacing spacing={"md"}/>
                     <DFlowFolderItemPathInput
                         description={"You can choose a name here and only use alphanumeric names."}
                         title={"Name of the flow"}

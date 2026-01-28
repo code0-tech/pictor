7aafd2dc78fde7919450844ab6c4e12dc5311085
nicosammito <nico@sammito.de>
2026-01-28 13:48:11 +0100

    feat: enhance DFlowTabTrigger with input type definition and improved UI components

--- src/components/d-flow-file/DFlowTabTrigger.tsx
+++ src/components/d-flow-file/DFlowTabTrigger.tsx
@@ -7,9 +7,16 @@ import {DFlowSuggestion} from "../d-flow-suggestion";
 import {useValueSuggestions} from "../d-flow-suggestion/DFlowValueSuggestions.hook";
 import {useDataTypeSuggestions} from "../d-flow-suggestion/DFlowDataTypeSuggestions.hook";
 import {toInputSuggestions} from "../d-flow-suggestion/DFlowSuggestionMenu.util";
-import type {DataType, Flow, NodeParameterValue, Scalars} from "@code0-tech/sagittarius-graphql-types";
-import {DFlowInputDataType} from "../d-flow-input/DFlowInputDataType";
+import type {Flow, NodeParameterValue, Scalars} from "@code0-tech/sagittarius-graphql-types";
 import {DFlowInputDefault} from "../d-flow-input/DFlowInputDefault";
+import {Text} from "../text/Text";
+import {Badge} from "../badge/Badge";
+import {Button} from "../button/Button";
+import {IconEdit, IconLiveView} from "@tabler/icons-react";
+import {InputDescription, InputLabel} from "../form";
+import {Card} from "../card/Card";
+import CardSection from "../card/CardSection";
+import {ButtonGroup} from "../button-group/ButtonGroup";
 
 export interface DFlowTabTriggerProps {
     instance: Flow
@@ -38,10 +45,60 @@ export const DFlowTabTrigger: React.FC<DFlowTabTriggerProps> = (props) => {
 
 
     return <Flex style={{gap: ".7rem", flexDirection: "column"}}>
-        {definition?.inputType ? <DFlowInputDataType onDataTypeChange={value => {
-            instance.inputType = value as DataType
-            flowService.update()
-        }} initialValue={instance.inputType || definition.inputType} blockingDataType={definition.inputType}/> : null}
+        <div>
+            <InputLabel>Input type</InputLabel>
+            <InputDescription>Here you can define the expected pattern which the trigger needs to follow on
+                trigger.</InputDescription>
+            <Card color={"secondary"} paddingSize={"xs"}>
+                <Flex style={{gap: ".7rem"}} align={"center"} justify={"space-between"}>
+                    <Text>Rest Adapter Input Type</Text>
+                    <Badge color={"secondary"} border>2 rules</Badge>
+                </Flex>
+                <Card paddingSize={"xs"} mt={0.7} mb={-0.7} mx={-0.7} style={{borderWidth: "2px"}}>
+                    <CardSection border>
+                        <Flex style={{gap: ".7rem"}} justify={"space-between"}>
+                            <Flex align={"center"} style={{gap: ".35rem"}}>
+                                <Text size={"md"}>Contains key</Text>
+                                <Badge border color={"info"} style={{verticalAlign: "middle"}}>
+                                    <Text style={{color: "inherit"}}>body</Text>
+                                </Badge>
+                                <Text size={"md"}>of datatype</Text>
+                                <Badge border color={"info"} style={{verticalAlign: "middle"}}>
+                                    <Text style={{color: "inherit"}}>Object</Text>
+                                </Badge>
+                            </Flex>
+                            <ButtonGroup>
+                                <Button paddingSize={"xxs"} variant={"filled"}><IconLiveView size={13}/> </Button>
+                                <Button paddingSize={"xxs"} variant={"filled"}><IconEdit size={13}/> </Button>
+                            </ButtonGroup>
+
+                        </Flex>
+                    </CardSection>
+                    <CardSection border>
+                        <Flex style={{gap: ".7rem"}} justify={"space-between"}>
+                            <Flex align={"center"} style={{gap: ".35rem"}}>
+                                <Text size={"md"}>Contains key</Text>
+                                <Badge border color={"info"}>
+                                    <Text style={{color: "inherit"}}>headers</Text>
+                                </Badge>
+                                <Text size={"md"}>of datatype</Text>
+                                <Badge border color={"info"}>
+                                    <Text style={{color: "inherit"}}>Object</Text>
+                                </Badge>
+                            </Flex>
+                            <ButtonGroup>
+                                <Button paddingSize={"xxs"} variant={"filled"}><IconLiveView size={13}/> </Button>
+                                <Button paddingSize={"xxs"} variant={"filled"}><IconEdit size={13}/> </Button>
+                            </ButtonGroup>
+                        </Flex>
+                    </CardSection>
+                </Card>
+            </Card>
+        </div>
+        {/*{definition?.inputType ? <DFlowInputDataType onDataTypeChange={value => {*/}
+        {/*    instance.inputType = value as DataType*/}
+        {/*    flowService.update()*/}
+        {/*}} initialValue={instance.inputType || definition.inputType} blockingDataType={definition.inputType}/> : null}*/}
         {definition?.flowTypeSettings?.map(settingDefinition => {
             const setting = instance.settings?.nodes?.find(s => s?.flowSettingIdentifier == settingDefinition.identifier)
             const title = settingDefinition.names!![0]?.content ?? ""
@@ -55,7 +112,7 @@ export const DFlowTabTrigger: React.FC<DFlowTabTriggerProps> = (props) => {
                 startTransition(async () => {
                     if (value?.__typename == "LiteralValue" && settingDefinition.identifier) {
                         await flowService.setSettingValue(props.instance.id, String(settingDefinition.identifier), value.value)
-                    } else if (settingDefinition.identifier)  {
+                    } else if (settingDefinition.identifier) {
                         await flowService.setSettingValue(props.instance.id, String(settingDefinition.identifier), value)
                     }
                 })

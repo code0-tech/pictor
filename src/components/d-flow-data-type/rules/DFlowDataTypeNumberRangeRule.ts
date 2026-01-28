d51331324bed3c2decde1e97a0d86c35b0ce3ddc
nicosammito <nico@sammito.de>
2026-01-28 14:43:54 +0100

    feat: remove redundant type check for number in DFlowDataTypeRangeRule validation

--- src/components/d-flow-data-type/rules/DFlowDataTypeNumberRangeRule.ts
+++ src/components/d-flow-data-type/rules/DFlowDataTypeNumberRangeRule.ts
@@ -5,7 +5,6 @@ import type {DataTypeRulesNumberRangeConfig, NodeParameterValue} from "@code0-te
 export class DFlowDataTypeRangeRule {
     public static validate(value: NodeParameterValue, config: DataTypeRulesNumberRangeConfig): boolean {
         if (value.__typename !== 'LiteralValue') return false
-        if (!(typeof value.value === "number")) return false
         if (!config.from || !config.to) return false
         return value.value >= config.from && value.value <= config.to
     }

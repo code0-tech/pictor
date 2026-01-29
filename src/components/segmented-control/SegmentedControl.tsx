8102e728f8d6ff61b3cef92f516373fe84b491ab
nicosammito <nico@sammito.de>
2026-01-29 23:58:27 +0100

    feat: enhance SegmentedControl to support color variations

--- src/components/segmented-control/SegmentedControl.tsx
+++ src/components/segmented-control/SegmentedControl.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import {Code0ComponentProps} from "../../utils";
+import {Code0ComponentProps, Color} from "../../utils";
 import {ToggleGroupSingleProps, ToggleGroupItemProps, Root, Item} from "@radix-ui/react-toggle-group";
 import {mergeCode0Props} from "../../utils";
 import "./SegmentedControl.style.scss"
 
-type SegmentedControlProps = Code0ComponentProps & ToggleGroupSingleProps
+type SegmentedControlProps = Code0ComponentProps & ToggleGroupSingleProps & {color?: Color}
 type SegmentedControlItemProps = Code0ComponentProps & ToggleGroupItemProps
 
 export const SegmentedControl: React.FC<SegmentedControlProps> = (props) => {
-    return <Root {...mergeCode0Props("segmented-control", props) as SegmentedControlProps}/>
+    return <Root {...mergeCode0Props(`segmented-control segmented-control--${props.color ?? "secondary"}`, props) as SegmentedControlProps}/>
 }
 
 export const SegmentedControlItem: React.FC<SegmentedControlItemProps> = (props) => {

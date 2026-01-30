ec60d4ecfde1848a3e1d69bc55e203b6ba3ea5aa
nicosammito <nico@sammito.de>
2026-01-30 23:48:14 +0100

    feat: enhance value validation logic in useValueValidation for improved variant handling

--- src/components/d-flow-validation/DValueValidation.hook.ts
+++ src/components/d-flow-validation/DValueValidation.hook.ts
@@ -2,6 +2,7 @@ import type {Flow, GenericMapper, NodeParameterValue} from "@code0-tech/sagittar
 import {RuleMap} from "../d-flow-data-type/rules/DFlowDataTypeRules";
 import {DataTypeView} from "../d-flow-data-type";
 import {DFlowDataTypeReactiveService} from "../d-flow-data-type";
+import {VariantsMap} from "../d-flow-data-type/variants/DFlowDataTypeVariants";
 
 export const useValueValidation = (
     value: NodeParameterValue,
@@ -13,9 +14,13 @@ export const useValueValidation = (
 
     const map = new Map<string, GenericMapper>(generics?.map(generic => [generic.target!!, generic]))
 
-    return dataType.rules?.nodes?.every(rule => {
+    const isRulesValid = dataType.rules?.nodes?.every(rule => {
         if (!rule || !rule.variant || !rule.config) return false
         if (!RuleMap.get(rule.variant)) return true //TODO; missing parent type rule
         return RuleMap.get(rule.variant)?.validate(value, rule.config, map, dataTypeService, flow)
     }) ?? true
+
+    const isVariantValid = VariantsMap.get(dataType.variant!!)?.validate(value) ?? true
+
+    return isRulesValid && isVariantValid
 }

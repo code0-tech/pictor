ac0039eac599fd2b26f345026c4f286aa4d9ed13
nicosammito <nico@sammito.de>
2026-02-07 21:55:45 +0100

    feat: add DataTable component with filter input and styles

---
+++ src/components/data-table/DataTable.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {DataTableFilterInput} from "./DataTableFilterInput";
+
+export const Default = () => {
+    return <DataTableFilterInput/>
+}
+
+
+export default {
+    title: "DataTable",
+    component: Default,
+}

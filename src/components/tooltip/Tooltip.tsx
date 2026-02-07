b4b1c46748b8c34a06b376faae40407ae4ef6257
nicosammito <nico@sammito.de>
2026-02-07 12:41:24 +0100

    feat: enhance tooltip component with color customization for improved styling

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Code0ComponentProps} from "../../utils/types";
+import {Code0ComponentProps, Color} from "../../utils/types";
 import * as RadixTooltip from "@radix-ui/react-tooltip";
 import {mergeCode0Props} from "../../utils/utils";
 import "./Tooltip.style.scss"
@@ -7,12 +7,12 @@ import "./Tooltip.style.scss"
 export type TooltipProps = Code0ComponentProps & RadixTooltip.TooltipProps
 export type TooltipTriggerProps = Code0ComponentProps & RadixTooltip.TooltipTriggerProps
 export type TooltipPortalProps = Code0ComponentProps & RadixTooltip.TooltipPortalProps
-export type TooltipContentProps = Code0ComponentProps & RadixTooltip.TooltipContentProps
-export type TooltipArrowProps = Code0ComponentProps & RadixTooltip.TooltipArrowProps
+export type TooltipContentProps = Code0ComponentProps & RadixTooltip.TooltipContentProps & {color?: Color}
+export type TooltipArrowProps = Code0ComponentProps & RadixTooltip.TooltipArrowProps & {color?: Color}
 
 export const Tooltip: React.FC<TooltipProps> = (props) => {
     return <RadixTooltip.TooltipProvider delayDuration={0}>
-        <RadixTooltip.Tooltip {...mergeCode0Props("tooltip", props) as TooltipProps}/>
+        <RadixTooltip.Tooltip {...mergeCode0Props(`tooltip`, props) as TooltipProps}/>
     </RadixTooltip.TooltipProvider>
 }
 
@@ -26,9 +26,9 @@ export const TooltipPortal: React.FC<TooltipPortalProps> = (props) => {
 
 export const TooltipContent: React.FC<TooltipContentProps> = (props) => {
     return <RadixTooltip.TooltipContent
-        align={props.align} {...mergeCode0Props("tooltip__content", props) as TooltipContentProps}/>
+        align={props.align} {...mergeCode0Props(`tooltip__content tooltip__content--${props.color ?? "tertiary"}`, props) as TooltipContentProps}/>
 }
 
 export const TooltipArrow: React.FC<TooltipArrowProps> = (props) => {
-    return <RadixTooltip.TooltipArrow {...mergeCode0Props("tooltip__arrow", props) as TooltipArrowProps}/>
+    return <RadixTooltip.TooltipArrow {...mergeCode0Props(`tooltip__arrow tooltip__arrow--${props.color ?? "tertiary"}}`, props) as TooltipArrowProps}/>
 }

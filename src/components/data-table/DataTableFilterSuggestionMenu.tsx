d2177b0323a361faa227b9c0f5f92a534f9fcd7d
nicosammito <nico@sammito.de>
2026-02-09 17:31:02 +0100

    feat: update DataTableFilterSuggestionMenu to manage open state and prevent interaction outside

--- src/components/data-table/DataTableFilterSuggestionMenu.tsx
+++ src/components/data-table/DataTableFilterSuggestionMenu.tsx
@@ -11,13 +11,18 @@ export const DataTableFilterSuggestionMenu: React.FC<DataTableFilterSuggestionMe
 
     const {children, context} = props
 
+    const [open, setOpen] = React.useState(true)
+
     const LocalMenuContent = MenuContent as any
 
-    return <Menu open={true} modal={false}>
+    return <Menu open={open} onOpenChange={(open) => setOpen(open)} modal={false}>
         <MenuTrigger asChild>
             <div style={{position: 'absolute', top: 0, left: 0, width: 0, height: 0}}/>
         </MenuTrigger>
-        <LocalMenuContent w={"200px"} onOpenAutoFocus={(e: Event) => {
+        <LocalMenuContent w={"200px"} onInteractOutside={(e: Event) => {
+            const prevent = Array.from((e.currentTarget as HTMLDivElement).classList).map(className => className).some(className => className.startsWith("cm-"))
+            if (prevent) e.preventDefault()
+        }} onOpenAutoFocus={(e: Event) => {
             e.preventDefault();
             ((e.currentTarget as HTMLElement).querySelector('[role="menuitem"]') as HTMLElement)?.focus();
         }} onKeyDown={(event: any) => {

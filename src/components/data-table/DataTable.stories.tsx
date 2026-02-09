111c89943d62c7396c13afed5b5408b8dd12d5e4
nicosammito <nico@sammito.de>
2026-02-09 13:21:42 +0100

    feat: add custom filter suggestions to DataTableFilterInput with context-aware menus

--- src/components/data-table/DataTable.stories.tsx
+++ src/components/data-table/DataTable.stories.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import {DataTableFilterInput} from "./DataTableFilterInput";
+import {DataTableFilterSuggestionMenu} from "./DataTableFilterSuggestionMenu";
+import {MenuItem} from "../menu/Menu";
 
 export const Default = () => {
-    return <DataTableFilterInput/>
+    return <DataTableFilterInput filterTokens={[
+        {
+            token: "name",
+            operators: ["isOneOf", "isNotOneOf"],
+            suggestion: (context, operator, applySuggestion) => {
+                return <DataTableFilterSuggestionMenu context={context}>
+                    <MenuItem onSelect={() => applySuggestion("value ")}>value</MenuItem>
+                </DataTableFilterSuggestionMenu>
+            }
+        },
+        {
+            token: "member",
+            operators: ["isOneOf", "isNotOneOf"],
+            suggestion: (context, operator, applySuggestion) => {
+                return <DataTableFilterSuggestionMenu context={context}>
+                    <MenuItem onSelect={() => applySuggestion("@nsammito ")}>@nsammito</MenuItem>
+                </DataTableFilterSuggestionMenu>
+            }
+        }
+    ]}/>
 }
 
 

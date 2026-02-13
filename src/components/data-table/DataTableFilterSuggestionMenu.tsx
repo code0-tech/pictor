c88c3aaf5e0f8c12dfe91f4e9cb1f2c1bd8d7102
nicosammito <nico@sammito.de>
2026-02-13 19:30:53 +0100

    fix: replace w prop with miw in LocalMenuContent for correct menu sizing

--- src/components/data-table/DataTableFilterSuggestionMenu.tsx
+++ src/components/data-table/DataTableFilterSuggestionMenu.tsx
@@ -19,7 +19,7 @@ export const DataTableFilterSuggestionMenu: React.FC<DataTableFilterSuggestionMe
         <MenuTrigger asChild>
             <div style={{position: 'absolute', top: 0, left: 0, width: 0, height: 0}}/>
         </MenuTrigger>
-        <LocalMenuContent w={"200px"} onInteractOutside={(e: Event) => {
+        <LocalMenuContent miw={"200px"} onInteractOutside={(e: Event) => {
             const prevent = Array.from((e.currentTarget as HTMLDivElement).classList).map(className => className).some(className => className.startsWith("cm-"))
             if (prevent) e.preventDefault()
         }} onOpenAutoFocus={(e: Event) => {

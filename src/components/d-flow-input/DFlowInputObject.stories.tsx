1dd46e1518812e6dd12ab088a29437a1512f995a
nicosammito <nico@sammito.de>
2026-02-22 14:30:52 +0100

    feat: update DFlowInputObject story to use simplified sample object data

--- src/components/d-flow-input/DFlowInputObject.stories.tsx
+++ src/components/d-flow-input/DFlowInputObject.stories.tsx
@@ -1,21 +1,7 @@
 import React from "react"
-import DFlowInputObject from "./DFlowInputObject";
+import {DFlowInputObject} from "./DFlowInputObject";
 
-const sampleObject = {
-    test: {
-        test: "test",
-        test2: "test",
-        nested: {
-            test: "test",
-            test2: "test"
-        }
-    },
-    list: [
-        {bla: "bla"},
-        {blub: [1, 2, 3]}
-    ],
-    value: "test"
-};
+const sampleObject =  [{blub: [1, 2, 3]}];
 
 export const Test = () => {
     return <DFlowInputObject data={sampleObject} label="Test Object"/>;
